a93fd5fd1feb1bcb860e4119e93a51f19d9bf03c
ZhangYuanhan-AI <yuanhan002@ntu.edu.sg>
2023-07-09 09:50:35 +0800

    add otter and llama-adapter

---
+++ configs/multimodal/llama_adapter_v2_multimodal/llama-adapter-v2-mm-7b-4-mmagibench.py
@@ -0,0 +1,42 @@
+# dataloader settings
+val_pipeline = [
+    dict(type='mmpretrain.torchvision/Resize',
+         size=(224, 224),
+         interpolation=3),
+    dict(type='mmpretrain.torchvision/ToTensor'),
+    dict(type='mmpretrain.torchvision/Normalize',
+         mean=(0.48145466, 0.4578275, 0.40821073),
+         std=(0.26862954, 0.26130258, 0.27577711)),
+    dict(type='mmpretrain.PackInputs',
+         algorithm_keys=[
+             'question', 'answer', 'options', 'category', 'l2-category',
+             'index', 'context', 'options_dict'
+         ])
+]
+
+dataset = dict(type='opencompass.OmniMMBenchDataset',
+               data_file='data/mm_benchmark/mmagi_v030_full_inferin.tsv',
+               pipeline=val_pipeline)
+
+dataloader = dict(batch_size=1,
+                  num_workers=4,
+                  dataset=dataset,
+                  collate_fn=dict(type='pseudo_collate'),
+                  sampler=dict(type='DefaultSampler', shuffle=False))
+
+# model settings
+model = dict(
+    type='LLaMA-adapter-v2-mm-benchmark',
+    llama_dir=  # noqa
+    '/mnt/petrelfs/share_data/zhangyuanhan/llama_adapter_v2_multimodal',
+)
+
+# evaluation settings
+evaluator = [
+    dict(
+        type='opencompass.DumpResults',
+        save_path='work_dirs/llama-adapter-v2-multimodal-mmagibench-v0.1.0.xlsx'
+    )
+]
+
+# load_from = '/mnt/cache/liuyuan/research/NLP/MiniGPT-4/minigpt4-7b/prerained_minigpt4_7b.pth'  # noqa
