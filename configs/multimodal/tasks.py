8c14fcc6f82bf5c6a377cf3fdbf9494e713af343
wangbo-zhao <wangbo.zhao96@gmail.com>
2023-07-10 13:59:25 +0800

    fix tasks.py

--- configs/multimodal/tasks.py
+++ configs/multimodal/tasks.py
@@ -1,7 +1,7 @@
 tasks = [
-    # 'configs/multimodal/minigpt-4/minigpt-4-7b-omnimmbench.py',
-    # 'configs/multimodal/instructblip/instructblip-omnimmbench.py',
-    # 'configs/multimodal/flamingo/flamingo-9b-omnimmbench.py',
+    'configs/multimodal/minigpt-4/minigpt-4-7b-omnimmbench.py',
+    'configs/multimodal/instructblip/instructblip-omnimmbench.py',
+    'configs/multimodal/flamingo/flamingo-9b-omnimmbench.py',
     'configs/multimodal/mPLUG-owl/mplug_owl-7b-omnimmbench.py'
 ]
 
