8812b1565a0738721ef0878a9ddf751a7e8fbca5
cky <cky3702@163.com>
2023-07-12 14:43:23 +0800

    support internLM

--- configs/eval_internLM.py
+++ configs/eval_internLM.py
@@ -11,16 +11,13 @@ datasets += nq_datasets
 models = [
     dict(
         type=intern_model,
-        path="/cpfs01/shared/public/users/chenkeyu1/models/linglongta_v4_2/116999",
-        tokenizer_path='/cpfs01/shared/public/tokenizers/V7.model',
-        tokenizer_type='v7',
-        model_config = "/cpfs01/user/chenkeyu1/pjeval/InternLM/configs/model_config.py",
+        path="model/",
+        tokenizer_path='V7.model',
+        model_config = "model_config.py",
         max_out_len=100,
         max_seq_len=2048,
         batch_size=16,
         run_cfg=dict(num_gpus=1, num_procs=1))
 ]
-
-
 
 
