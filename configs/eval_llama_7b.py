803a75a86f111a6e19f01f197b63cbe8cd611c40
Leymore <zfz-960727@163.com>
2023-07-17 14:34:16 +0800

    fix eval_llama_7b

--- configs/eval_llama_7b.py
+++ configs/eval_llama_7b.py
@@ -3,22 +3,7 @@ from mmengine.config import read_base
 with read_base():
     from .datasets.piqa.piqa_ppl import piqa_datasets
     from .datasets.siqa.siqa_gen import siqa_datasets
+    from .models.hf_llama_7b import models
 
 
 datasets = [*piqa_datasets, *siqa_datasets]
-
-from opencompass.models import HuggingFaceCausalLM
-
-models = [
-    dict(
-        type=HuggingFaceCausalLM,
-        path='huggyllama/llama-7b',
-        tokenizer_path='huggyllama/llama-7b',
-        tokenizer_kwargs=dict(padding_side='left', truncation_side='left'),
-        max_seq_len=2048,
-        abbr='llama-7b',            
-        max_out_len=100,            
-        batch_size=16,
-        run_cfg=dict(num_gpus=1),   
-    )
-]
