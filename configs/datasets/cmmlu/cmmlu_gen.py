23a799860bf0b4335978d69407604edaa4673dc2
李浩楠 <lihaonan@lihaonandeMacBook-Air.local>
2023-07-22 19:48:47 +0400

    add CMMLU

---
+++ configs/datasets/cmmlu/cmmlu_gen.py
@@ -0,0 +1,4 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .cmmlu_gen_3d57f6 import cmmlu_datasets  # noqa: F401, F403
---
+++ opencompass/datasets/cmmlu.py
@@ -0,0 +1,32 @@
+import csv
+import os.path as osp
+
+from datasets import DatasetDict, load_dataset
+
+from opencompass.registry import LOAD_DATASET
+
+from .base import BaseDataset
+
+
+@LOAD_DATASET.register_module()
+class CMMLUDataset(BaseDataset):
+
+    @staticmethod
+    def load(path: str, name: str):
+        for split in ['dev', 'test']:
+            raw_data = []
+            filename = osp.join(path, split, f'{name}.csv')
+            with open(filename, encoding='utf-8') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    assert len(row) == 7
+                    raw_data.append({
+                        'question': row[1],
+                        'A': row[2],
+                        'B': row[3],
+                        'C': row[4],
+                        'D': row[5],
+                        'answer': row[6],
+                    })
+            dataset[split] = Dataset.from_list(raw_data)
+        return dataset
