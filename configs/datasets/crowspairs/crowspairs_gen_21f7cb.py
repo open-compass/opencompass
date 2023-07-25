f2052485842093803649300ec48abcddd216ae9f
yingfhu <yingfhu@gmail.com>
2023-07-25 14:48:15 +0800

    [Refactor] Update crows-pairs evaluation

--- configs/datasets/crowspairs/crowspairs_gen_21f7cb.py
+++ configs/datasets/crowspairs/crowspairs_gen_21f7cb.py
@@ -1,7 +1,6 @@
 from opencompass.openicl.icl_prompt_template import PromptTemplate
 from opencompass.openicl.icl_retriever import ZeroRetriever
 from opencompass.openicl.icl_inferencer import GenInferencer
-# from opencompass.openicl.icl_evaluator import AccEvaluator
 from opencompass.datasets import (crowspairsDataset_V2, crowspairs_postprocess,
                                   CrowspairsEvaluator)
 
