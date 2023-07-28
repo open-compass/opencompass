80b6149c86cbfa9918c94660ebdbd49530cbbdbc
Abhijoy Sarkar <abhijoy.sar@gmail.com>
2023-07-27 20:23:46 -0400

    packaging properly

--- opencompass/__init__.py
+++ opencompass/__init__.py
@@ -1,10 +1,1 @@
-import sys
-import os
 __version__ = '0.1.0'
-
-# Get the path of the parent package 'opencompass'
-current_dir = os.path.dirname(os.path.realpath(__file__))
-parent_dir = os.path.abspath(os.path.join(current_dir, '..'))
-
-# Add the parent directory to sys.path to resolve imports correctly
-sys.path.append(parent_dir)
