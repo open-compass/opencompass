29e34f908d790093764f7e84f445ab1efe74d1ce
fangyixiao18 <fangyx18@hotmail.com>
2023-08-17 16:35:31 +0800

    update

--- configs/multimodal/minigpt_4/minigpt_4_7b_seedbench.py
+++ configs/multimodal/minigpt_4/minigpt_4_7b_seedbench.py
@@ -2,34 +2,30 @@ from opencompass.multimodal.models.minigpt_4 import MiniGPT4SEEDBenchPromptConst
 
 # dataloader settings
 image_pipeline = [
-    dict(
-        type='mmpretrain.torchvision/Resize', size=(224, 224),
-        interpolation=3),
+    dict(type='mmpretrain.torchvision/Resize',
+         size=(224, 224),
+         interpolation=3),
     dict(type='mmpretrain.torchvision/ToTensor'),
-    dict(
-        type='mmpretrain.torchvision/Normalize',
-        mean=(0.48145466, 0.4578275, 0.40821073),
-        std=(0.26862954, 0.26130258, 0.27577711)),
-    dict(
-        type='mmpretrain.PackInputs',
-        algorithm_keys=[
-            'question', 'answer', 'choices', 'data_type', 'question_type_id',
-            'index', 'data_path', 'question_id'
-        ])
+    dict(type='mmpretrain.torchvision/Normalize',
+         mean=(0.48145466, 0.4578275, 0.40821073),
+         std=(0.26862954, 0.26130258, 0.27577711)),
+    dict(type='mmpretrain.PackInputs',
+         algorithm_keys=[
+             'question', 'answer', 'choices', 'data_type', 'question_type_id',
+             'index', 'data_path', 'question_id'
+         ])
 ]
 video_pipeline = [
     dict(type='mmaction.Resize', scale=(224, 224), interpolation='bicubic'),
     dict(type='mmaction.CenterCrop', crop_size=224),
-    dict(
-        type='Normalize',
-        mean=(0.48145466, 0.4578275, 0.40821073),
-        std=(0.26862954, 0.26130258, 0.27577711)),
-    dict(
-        type='mmpretrain.PackInputs',
-        algorithm_keys=[
-            'question', 'answer', 'choices', 'data_type', 'question_type_id',
-            'index', 'data_path', 'question_id'
-        ])
+    dict(type='Normalize',
+         mean=(0.48145466, 0.4578275, 0.40821073),
+         std=(0.26862954, 0.26130258, 0.27577711)),
+    dict(type='mmpretrain.PackInputs',
+         algorithm_keys=[
+             'question', 'answer', 'choices', 'data_type', 'question_type_id',
+             'index', 'data_path', 'question_id'
+         ])
 ]
 
 dataset = dict(
@@ -43,22 +39,21 @@ dataset = dict(
     video_pipeline=video_pipeline,
     only_image=True)
 
-minigpt_4_seedbench_dataloader = dict(
-    batch_size=1,
-    num_workers=4,
-    dataset=dataset,
-    collate_fn=dict(type='pseudo_collate'),
-    sampler=dict(type='DefaultSampler', shuffle=False))
+minigpt_4_seedbench_dataloader = dict(batch_size=1,
+                                      num_workers=4,
+                                      dataset=dataset,
+                                      collate_fn=dict(type='pseudo_collate'),
+                                      sampler=dict(type='DefaultSampler',
+                                                   shuffle=False))
 
 # model settings
 minigpt_4_seedbench_model = dict(
     type='minigpt-4',
     low_resource=False,
     llama_model='/path/to/vicuna/',
-    prompt_constructor=dict(
-        type=MiniGPT4SEEDBenchPromptConstructor,
-        image_prompt='###Human: <Img><ImageHere></Img>',
-        reply_prompt='###Assistant:'),
+    prompt_constructor=dict(type=MiniGPT4SEEDBenchPromptConstructor,
+                            image_prompt='###Human: <Img><ImageHere></Img>',
+                            reply_prompt='###Assistant:'),
     post_processor=None,
     mode='loss')
 
