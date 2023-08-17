36e8de7a83f319390b805fee1e254ff2f5a699ba
liuyuan <3463423099@qq.com>
2023-08-17 18:28:42 +0800

    [Fix]: Fix name

--- configs/multimodal/tasks.py
+++ configs/multimodal/tasks.py
@@ -1,15 +1,15 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .minigpt_4.minigpt_4_7b_mmbench import (minigpt_4_dataloader,
-                                                 minigpt_4_evaluator,
-                                                 minigpt_4_load_from,
-                                                 minigpt_4_model)
+    from .minigpt_4.minigpt_4_7b_mmbench import (minigpt_4_mmbench_dataloader,
+                                                 minigpt_4_mmbench_evaluator,
+                                                 minigpt_4_mmbench_load_from,
+                                                 minigpt_4_mmbench_model)
 
-models = [minigpt_4_model]
-datasets = [minigpt_4_dataloader]
-evaluators = [minigpt_4_evaluator]
-load_froms = [minigpt_4_load_from]
+models = [minigpt_4_mmbench_model]
+datasets = [minigpt_4_mmbench_dataloader]
+evaluators = [minigpt_4_mmbench_evaluator]
+load_froms = [minigpt_4_mmbench_load_from]
 num_gpus = 8
 num_procs = 8
 launcher = 'pytorch'
