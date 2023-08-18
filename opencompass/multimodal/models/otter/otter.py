057a92cdb357672d25bf96e9b1204ee6d518f99b
Li Bo <drluodian@gmail.com>
2023-08-18 04:54:20 +0000

    add docs

--- opencompass/multimodal/models/otter/otter.py
+++ opencompass/multimodal/models/otter/otter.py
@@ -13,6 +13,15 @@ from .modeling_otter import OtterForConditionalGeneration
 
 @MODELS.register_module("otter-9b")
 class Otter(nn.Module):
+    """Inference code of OTTER.
+    Model details:
+        OTTER: a multi-modal model based on OpenFlamingo (open-sourced version of DeepMind's Flamingo)
+        https://github.com/Luodian/Otter
+    Args:
+        model_path (str): The path of OTTER model, in Huggingface model hub format.
+        load_bit (str): The bit of OTTER model, can be "fp32" or "bf16".
+    """
+    
     def __init__(self, model_path, load_bit) -> None:
         super().__init__()
         torch_dtype = torch.bfloat16 if load_bit == "bf16" else torch.float32
@@ -20,7 +29,6 @@ class Otter(nn.Module):
         self.tokenizer = self.model.text_tokenizer
         self.tokenizer.padding_side = "left"
         self.model_dtype = next(self.model.parameters()).dtype
-        # self.model = self.model.to(device)
 
     def post_process(self, output_text):
         pattern = re.compile(r"([A-Z]\.)")
@@ -58,10 +66,6 @@ class Otter(nn.Module):
 
         data_sample = data_samples[0]
         vision_x = image.unsqueeze(1).unsqueeze(0).to(dtype=self.model_dtype)
-        # B,N,T,C,H,W = vision_x.shape
-        # vision_x = vision_x.expand(1,2,1,C,H,W)
-        # vision_x = torch.zeros_like(vision_x)
-        # system_message = "<image>User: what is the capitcal of China? There are several options:\nA. Beijing\nB. Shanghai\nC. Guangzhou\nD. Shenzhen\n.GPT:<answer>: A\n<|endofchunk|>"
         lang_x = self.model.text_tokenizer([f"<image>User: {prompt} GPT:<answer>"], return_tensors="pt")
 
         bad_words_id = self.model.text_tokenizer(["User:", "GPT:"]).input_ids
