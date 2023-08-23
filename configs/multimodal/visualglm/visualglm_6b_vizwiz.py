aa55a84ff46802353237c814f36867a0b58f14d5
yuanyike <yuanyike@pjlab.org.cn>
2023-08-23 16:50:45 +0800

    [Feat] Add public dataset support of VisualGLM.

--- configs/multimodal/visualglm/visualglm_6b_vizwiz.py
+++ configs/multimodal/visualglm/visualglm_6b_vizwiz.py
@@ -1,7 +1,9 @@
-from opencompass.multimodal.models.visualglm import (VisualGLMBasePostProcessor, VisualGLMMMBenchPromptConstructor)
+from opencompass.multimodal.models.visualglm import (VisualGLMBasePostProcessor, VisualGLMVQAPromptConstructor)
 
 # dataloader settings
 val_pipeline = [
+    dict(type='mmpretrain.LoadImageFromFile'),
+    dict(type='mmpretrain.ToPIL', to_rgb=True),
     dict(type='mmpretrain.torchvision/Resize',
          size=(224, 224),
          interpolation=3),
@@ -9,18 +11,20 @@ val_pipeline = [
     dict(type='mmpretrain.torchvision/Normalize',
          mean=(0.48145466, 0.4578275, 0.40821073),
          std=(0.26862954, 0.26130258, 0.27577711)),
-    dict(type='mmpretrain.PackInputs',
-         algorithm_keys=[
-             'question', 'options', 'category', 'l2-category', 'context',
-             'index', 'options_dict'
-         ])
+    dict(
+        type='mmpretrain.PackInputs',
+        algorithm_keys=['question', 'gt_answer', 'gt_answer_weight'],
+        meta_keys=['question_id', 'image_id'],
+    )
 ]
 
-dataset = dict(type='opencompass.MMBenchDataset',
-               data_file='data/mmbench/mmbench_test_20230712.tsv',
+dataset = dict(type='mmpretrain.VizWiz',
+               data_root='data/vizwiz/',
+               data_prefix='Images/val',
+               ann_file='Annotations/val.json',
                pipeline=val_pipeline)
 
-mmbench_dataloader = dict(batch_size=1,
+visualglm_vizwiz_dataloader = dict(batch_size=1,
                   num_workers=4,
                   dataset=dataset,
                   collate_fn=dict(type='pseudo_collate'),
@@ -30,12 +34,9 @@ mmbench_dataloader = dict(batch_size=1,
 visualglm_model = dict(
     type='visualglm',
     pretrained_path='/path/to/visualglm',  # or Huggingface repo id
-    prompt_constructor=dict(type=VisualGLMMMBenchPromptConstructor),
+    prompt_constructor=dict(type=VisualGLMVQAPromptConstructor),
     post_processor=dict(type=VisualGLMBasePostProcessor)
 )
 
 # evaluation settings
-mmbench_evaluator = [
-    dict(type='opencompass.DumpResults',
-         save_path='work_dirs/visualglm-6b-mmbench.xlsx')
-]
+visualglm_vizwiz_evaluator = [dict(type='mmpretrain.VQAAcc')]
--- opencompass/multimodal/models/visualglm/prompt_constructor.py
+++ opencompass/multimodal/models/visualglm/prompt_constructor.py
@@ -1,8 +1,8 @@
 import torch
 
 
-class VisualGLMPromptConstructor:
-    """Prompt constructor for VisualGLM.
+class VisualGLMMMBenchPromptConstructor:
+    """MMBench prompt constructor for VisualGLM.
 
     The overall prompt will be formulated as
     "system_prompt"+"human_prompt"+"image_prompt"+question+"assistant+prompt".
@@ -30,7 +30,7 @@ class VisualGLMPromptConstructor:
             batch (dict): Input data containing image and data_samples.
 
         Returns:
-            tuple: A tuple containing prompt, images and data_samples.
+            A tuple containing images, prompt, data_samples and image_position.
         """
 
         images = batch.pop('inputs')
@@ -50,6 +50,171 @@ class VisualGLMPromptConstructor:
             for context, question, option in zip(contexts, questions, options)
         ]
 
+        image_position = 5
+
+        return images, prompt, data_samples, image_position
+
+
+class VisualGLMBasePromptConstructor:
+    """Base prompt constructor for VisualGLM.
+
+    The prompt will concat <img> and the given system prompt.
+    Args:
+        system_prompt (str): System prompt. (Default: '')
+    """
+
+    def __init__(self, system_prompt='') -> None:
+        self.prompt = system_prompt
+
+    def __call__(self, batch: dict) -> tuple:
+        """Construct prompt.
+
+        Args:
+            batch (dict): Input data containing image and data_samples.
+
+        Returns:
+            A tuple containing images, prompt, data_samples and image_position.
+        """
+
+        images = batch.pop('inputs')
+        images = torch.stack(images, dim=0)
+        data_samples = batch.pop('data_samples')
+
+        # generate text prompt
+        img_prompt = '<img></img>'
+        prompt = img_prompt + self.prompt
+        image_position = prompt.rfind('<img>') + 5
+
+        image_position = 5
+
+        return images, prompt, data_samples, image_position
+
+
+class VisualGLMVQAPromptConstructor(VisualGLMBasePromptConstructor):
+    """VQA prompt constructor for VisualGLM.
+
+    The prompt will concat <img>, the question and the system prompt.
+    Args:
+        system_prompt (str): System prompt. (Default: '')
+    """
+
+    def __init__(self, system_prompt='') -> None:
+        super().__init__(system_prompt)
+
+    def __call__(self, batch: dict) -> tuple:
+        """Construct prompt.
+
+        Args:
+            batch (dict): Input data containing image and data_samples.
+
+        Returns:
+            A tuple containing images, prompt, data_samples and image_position.
+        """
+
+        images = batch.pop('inputs')
+        images = torch.stack(images, dim=0)
+        data_samples = batch.pop('data_samples')
+        questions = [sample.get('question') for sample in data_samples]
+
+        # generate text prompt
+        prompt = [
+            '<img></img>Q:{} {}\nA:'.format(question, self.prompt)
+            for question in questions
+        ]
+        image_position = 5
+
+        return images, prompt, data_samples, image_position
+
+
+class VisualGLMScienceQAPromptConstructor(VisualGLMBasePromptConstructor):
+    """ScienceQA prompt constructor for VisualGLM.
+
+    The prompt will concat image and all terms in a question.
+    Args:
+        system_prompt (str): System prompt. (Default: '')
+    """
+
+    choice_mapping = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F'}
+
+    def __init__(self, system_prompt='') -> None:
+        super().__init__(system_prompt)
+
+    def __call__(self, batch: dict) -> tuple:
+        """Construct prompt.
+
+        Args:
+            batch (dict): Input data containing image and data_samples.
+
+        Returns:
+            A tuple containing images, prompt, data_samples and image_position.
+        """
+
+        images = batch.pop('inputs')
+        images = torch.stack(images, dim=0)
+        data_samples = batch.pop('data_samples')
+        questions = [
+            'Q: ' + sample.get('question') + '\n' for sample in data_samples
+        ]
+        choices = [sample.get('choices') for sample in data_samples]
+        choices = [[
+            f'({self.choice_mapping[i]}) ' + item
+            for i, item in enumerate(choice)
+        ] for choice in choices]
+        choices = [
+            'Choices: ' + ' '.join(choice) + '\n' for choice in choices
+        ]  # noqa
+        contexts = [
+            'Context: ' + data_sample.get('hint') + '\n'
+            for data_sample in data_samples
+        ]  # noqa
+
+        # generate text prompt
+        prompt = [
+            '<img></img>' + context + question + choice + self.prompt
+            for context, question, choice in zip(contexts, questions, choices)
+        ]
+        image_position = 5
+
+        return images, prompt, data_samples, image_position
+
+
+class VisualGLMIconQAPromptConstructor(VisualGLMBasePromptConstructor):
+    """IconQA prompt constructor for VisualGLM.
+
+    The prompt will concat <img>, the question and the system prompt.
+    Args:
+        system_prompt (str): System prompt. (Default: '')
+    """
+
+    def __init__(self, system_prompt='') -> None:
+        super().__init__(system_prompt)
+
+    def __call__(self, batch: dict) -> tuple:
+        """Construct prompt.
+
+        Args:
+            batch (dict): Input data containing image and data_samples.
+
+        Returns:
+            A tuple containing images, prompt, data_samples and image_position.
+        """
+
+        images = batch.pop('inputs')
+        images = torch.stack(images, dim=0)
+        data_samples = batch.pop('data_samples')
+        questions = [
+            'Q: ' + sample.get('question') + '\n' for sample in data_samples
+        ]
+        choices = [sample.get('choices') for sample in data_samples]
+        choices = [
+            'Options: ' + ', '.join(choice) + '.\n' for choice in choices
+        ]  # noqa
+
+        # generate text prompt
+        prompt = [
+            '<img></img>' + question + choice + self.prompt
+            for question, choice in zip(questions, choices)
+        ]
         image_position = 5
 
         return images, prompt, data_samples, image_position
