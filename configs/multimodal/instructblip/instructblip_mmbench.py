1034c487efe3cc0854791c7d6115418f71714fb7
Yixiao Fang <36138628+fangyixiao18@users.noreply.github.com>
2023-08-23 15:33:59 +0800

    [Refactor] Refactor instructblip (#227)
    * refactor instructblip
    
    * add post processor
    
    * add forward
    
    * fix lint
    
    * update
    
    * update

--- configs/multimodal/instructblip/instructblip_mmbench.py
+++ configs/multimodal/instructblip/instructblip_mmbench.py
@@ -1,3 +1,6 @@
+from opencompass.multimodal.models.instructblip import (
+    InstructBlipMMBenchPromptConstructor, InstructBlipMMBenchPostProcessor)
+
 # dataloader settings
 val_pipeline = [
     dict(type='mmpretrain.torchvision/Resize',
@@ -9,24 +12,27 @@ val_pipeline = [
          std=(0.26862954, 0.26130258, 0.27577711)),
     dict(type='mmpretrain.PackInputs',
          algorithm_keys=[
-             'question', 'category', 'l2-category', 'context',
-             'index', 'options_dict', 'options', 'split'
+             'question', 'category', 'l2-category', 'context', 'index',
+             'options_dict', 'options', 'split'
          ])
 ]
 
-dataset = dict(type='opencompass.MMBench',
+dataset = dict(type='opencompass.MMBenchDataset',
                data_file='data/mmbench/mmbench_test_20230712.tsv',
                pipeline=val_pipeline)
 
-dataloader = dict(batch_size=1,
-                  num_workers=4,
-                  dataset=dataset,
-                  collate_fn=dict(type='pseudo_collate'),
-                  sampler=dict(type='DefaultSampler', shuffle=False))
+instruct_blip_dataloader = dict(batch_size=1,
+                                num_workers=4,
+                                dataset=dataset,
+                                collate_fn=dict(type='pseudo_collate'),
+                                sampler=dict(type='DefaultSampler',
+                                             shuffle=False))
 
 # model settings
-model = dict(
-    type='blip2-vicuna-instruct-mmbench',
+instruct_blip_model = dict(
+    type='blip2-vicuna-instruct',
+    prompt_constructor=dict(type=InstructBlipMMBenchPromptConstructor),
+    post_processor=dict(type=InstructBlipMMBenchPostProcessor),
     freeze_vit=True,
     low_resource=False,
     llm_model='/path/to/vicuna-7b/',
@@ -35,11 +41,11 @@ model = dict(
 )
 
 # evaluation settings
-evaluator = [
+instruct_blip_evaluator = [
     dict(
         type='opencompass.DumpResults',
         save_path=  # noqa: E251
         'work_dirs/instructblip_vicuna7b/instructblipvicuna_mmbench.xlsx')
 ]
 
-load_from = '/path/to/instruct_blip_vicuna7b_trimmed.pth'  # noqa
+instruct_blip_load_from = '/path/to/instruct_blip_vicuna7b_trimmed'
