343f785b07a90c4ef4df5621544b210641f56f81
Yuan Liu <30762564+YuanLiuuuuuu@users.noreply.github.com>
2023-08-24 14:11:29 +0800

    [Feature]: Add Flamingo (#258)
    * [Feature]: Add Openflamingo MMBench
    
    * [Fix]: Fix import error
    
    * [Fix]: Revert task config
    
    * [Fix]: Fix path bug

--- configs/multimodal/openflamingo/README.md
+++ configs/multimodal/openflamingo/README.md
@@ -1,13 +1,8 @@
-# MiniGPT-4
+# OpenFlamingo
 
 ### Prepare the environment
 
-```sh
-cd opencompass/multimodal/models/minigpt_4
-git clone https://github.com/Vision-CAIR/MiniGPT-4.git
-```
-
-Then prepare the environement according to this [doc](https://github.com/Vision-CAIR/MiniGPT-4)
+Install [MMPretrain](https://github.com/open-mmlab/mmpretrain) according to this [doc](https://mmpretrain.readthedocs.io/en/latest/get_started.html#installation)
 
 ### Start evaluation
 
--- opencompass/multimodal/models/__init__.py
+++ opencompass/multimodal/models/__init__.py
@@ -1,8 +1,13 @@
+import os.path as osp
+
 from opencompass.utils import satisfy_requirement
 
 if satisfy_requirement('salesforce-lavis'):
     from .instructblip import *  # noqa: F401, F403
 
+if osp.exists('opencompass/multimodal/models/minigpt_4/MiniGPT-4'):
+    from .minigpt_4 import *  # noqa: F401, F403
+
 from .llava import *  # noqa: F401, F403
-from .minigpt_4 import *  # noqa: F401, F403
+from .openflamingo import *  # noqa: F401, F403
 from .visualglm import *  # noqa: F401, F403
