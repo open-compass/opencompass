d3e7fa25b9c26912d2ce459f523db2118f1198e2
mzr1996 <mzr1996@163.com>
2023-08-28 02:01:02 +0000

    Support GSM8k evaluation with tools by Lagent and LangChain

---
+++ opencompass/models/langchain.py
@@ -0,0 +1,49 @@
+from typing import Tuple
+
+from mmengine.registry import Registry
+
+REGISTRY = Registry('helper')
+
+
+class LangchainAgent:
+
+    def __init__(self, agent_type, llm, tools) -> None:
+        from langchain.agents import initialize_agent, load_tools
+
+        llm = REGISTRY.build(llm)
+        tools = load_tools(tools, llm=llm)
+        self.agent = initialize_agent(tools,
+                                      llm,
+                                      agent=agent_type,
+                                      return_intermediate_steps=True)
+
+    def chat(self, user_input, ice=None) -> Tuple[str, dict]:
+        from langchain.schema import AgentAction
+        try:
+            generation = self.agent(user_input)
+            answer = generation['output']
+            steps = []
+            for step in generation['intermediate_steps']:
+                action: AgentAction = step[0]
+                steps.append(
+                    dict(
+                        type=action.tool,
+                        args=action.tool_input,
+                        result=step[1],
+                        thought=action.log,
+                        state=0,
+                        errmsg=None,
+                    ))
+        except Exception as e:
+            answer = None
+            steps = [
+                dict(
+                    type='InvalidAction',
+                    args={},
+                    result=None,
+                    thought=None,
+                    state=-1002,
+                    errmsg=str(e),
+                )
+            ]
+        return answer, steps
