f7d1ebc7a749cc3b9640d2bf72cd946281b6ab46
Li Bo <drluodian@gmail.com>
2023-08-30 11:19:03 +0000

    remove unused redefinition

--- opencompass/multimodal/models/__init__.py
+++ opencompass/multimodal/models/__init__.py
@@ -9,7 +9,6 @@ if osp.exists('opencompass/multimodal/models/minigpt_4/MiniGPT-4'):
     from .minigpt_4 import *  # noqa: F401, F403
 
 from .llava import *  # noqa: F401, F403
-from .minigpt_4 import *  # noqa: F401, F403
-from .otter import *  # noqa: F401, F403
 from .openflamingo import *  # noqa: F401, F403
+from .otter import *  # noqa: F401, F403
 from .visualglm import *  # noqa: F401, F403
