f2dd98ca7a7d20bec36b44919f63bd9a81196a63
Yuanhan Zhang <704464079@qq.com>
2023-09-01 23:32:05 +0800

    [Feat] Support LLaVA and mPLUG-Owl (#331)
    * refine gitignore
    
    * [Feature]: Add minigpt-4
    
    * [Feature]: Add mm local runner
    
    * [Feature]: Add instructblip
    
    * add otter and llama-adapter
    
    * add owl
    
    * add llama2-adapter and owl
    
    * lint
    
    * [Feature]: Add minigpt-4
    
    * [Feature]: Add instructblip
    
    * add otter and llama-adapter
    
    * add owl
    
    * add llama2-adapter and owl
    
    * lint
    
    * lint
    
    * update
    
    * lint
    
    * lint
    
    * add __init__.py
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: liuyuan <3463423099@qq.com>

---
+++ opencompass/multimodal/models/mplug_owl/__init__.py
@@ -0,0 +1,8 @@
+from .mplug_owl import MplugOwl
+from .post_processor import MplugOwlMMBenchPostProcessor
+from .prompt_constructor import MplugOwlMMBenchPromptConstructor  # noqa
+
+__all__ = [
+    'MplugOwl', 'MplugOwlMMBenchPostProcessor',
+    'MplugOwlMMBenchPromptConstructor'
+]
