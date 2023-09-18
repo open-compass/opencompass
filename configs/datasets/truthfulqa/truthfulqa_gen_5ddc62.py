f6c79e3b82c2c63dcc9e671419f7a706d6db6b06
yingfhu <yingfhu@gmail.com>
2023-09-18 11:50:47 +0800

    [Feat] refine docs and codes for more user guides

--- configs/datasets/truthfulqa/truthfulqa_gen_5ddc62.py
+++ configs/datasets/truthfulqa/truthfulqa_gen_5ddc62.py
@@ -20,8 +20,11 @@ truthfulqa_infer_cfg = dict(
 # Metrics such as 'truth' and 'info' needs
 # OPENAI_API_KEY with finetuned models in it.
 # Please use your own finetuned openai model with keys and refers to
-# the source code for more details
-# Metrics such as 'bleurt', 'rouge', 'bleu' are free to test
+# the source code of `TruthfulQAEvaluator` for more details.
+#
+# If you cannot provide available models for 'truth' and 'info',
+# and want to perform basic metric eval, please set
+# `metrics=('bleurt', 'rouge', 'bleu')`
 
 # When key is set to "ENV", the key will be fetched from the environment
 # variable $OPENAI_API_KEY. Otherwise, set key in here directly.
