2ea19fcd0e9d1b3ddd37e68444d2a74ced4b6def
gaotongxiao <gaotongxiao@gmail.com>
2023-09-22 18:44:45 +0800

    update

--- configs/datasets/kaoshi/kaoshi_gen.py
+++ configs/datasets/kaoshi/kaoshi_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .Kaoshi_gen_86aca2 import kaoshi_datasets  # noqa: F401, F403
+    from .kaoshi_gen_86aca2 import kaoshi_datasets  # noqa: F401, F403
