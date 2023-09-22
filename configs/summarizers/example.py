2a62bea1a4b7dc6545cf9590cf0651afa3aef999
TTTTTiam <45125943+CuteyThyme@users.noreply.github.com>
2023-09-22 17:42:08 +0800

    add evaluation of scibench (#393)
    * add evaluation of scibench
    
    * add evaluation of scibench
    
    * update scibench
    
    * remove scibench evaluator
    
    ---------
    
    Co-authored-by: Leymore <zfz-960727@163.com>

--- configs/summarizers/example.py
+++ configs/summarizers/example.py
@@ -10,6 +10,7 @@ with read_base():
     from .groups.flores import flores_summary_groups
     from .groups.tydiqa import tydiqa_summary_groups
     from .groups.xiezhi import xiezhi_summary_groups
+    from .groups.scibench import scibench_summary_groups
 
 summarizer = dict(
     summary_groups=sum([v for k, v in locals().items() if k.endswith("_summary_groups")], []),
