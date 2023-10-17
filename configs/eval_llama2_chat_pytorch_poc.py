abfbeff757532e4bb8a19a969d196e104d311e90
HIT-cwh <2892770585@qq.com>
2023-10-17 22:03:15 +0800

    WIP: add pytorch-poc

---
+++ configs/eval_llama2_chat_pytorch_poc.py
@@ -0,0 +1,118 @@
+from mmengine.config import read_base
+from opencompass.models import PytorchModel
+
+
+with read_base():
+    # choose a list of datasets
+    from .datasets.mmlu.mmlu_gen_a484b3 import mmlu_datasets
+    # from .datasets.ceval.ceval_gen_5f30c7 import ceval_datasets
+    # from .datasets.SuperGLUE_WiC.SuperGLUE_WiC_gen_d06864 import WiC_datasets
+    # from .datasets.SuperGLUE_WSC.SuperGLUE_WSC_gen_6dc406 import WSC_datasets
+    # from .datasets.triviaqa.triviaqa_gen_2121ce import triviaqa_datasets
+    # from .datasets.gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
+    # from .datasets.humaneval.humaneval_gen_8e312c import humaneval_datasets
+    # from .datasets.race.race_gen_69ee4f import race_datasets
+    # from .datasets.crowspairs.crowspairs_gen_381af0 import crowspairs_datasets
+    # and output the results in a choosen format
+    from .summarizers.medium import summarizer
+
+datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
+
+
+meta_template = dict(
+    round=[
+        dict(role="HUMAN", api_role="HUMAN"),
+        dict(role="BOT", api_role="BOT", generate=True),
+    ],
+)
+
+# config for internlm-chat-7b
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='internlm-chat-7b-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=32,
+#         concurrency=32,
+#         meta_template=meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
+
+# config for internlm-chat-7b-w4 model
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='internlm-chat-7b-w4-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=32,
+#         concurrency=32,
+#         meta_template=meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
+
+# config for internlm-chat-7b-w4kv8 model
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='internlm-chat-7b-w4kv8-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=32,
+#         concurrency=32,
+#         meta_template=meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
+
+# config for internlm-chat-20b
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='internlm-chat-20b-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=8,
+#         concurrency=8,
+#         meta_template=meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
+
+# config for internlm-chat-20b-w4 model
+models = [
+    dict(
+        type=PytorchModel,
+        abbr='llama2-chat-7b-pytorch-poc',
+        path="/mnt/142/gaojianfei/quantization/smooth_llama_chat_absmax",
+        # path='/mnt/140/InternLM/20B/internlm-20b-chat',
+        max_out_len=100,
+        max_seq_len=2048,
+        batch_size=1,
+        concurrency=1,
+        meta_template=meta_template,
+        run_cfg=dict(num_gpus=1, num_procs=1),
+    )
+]
+
+# config for internlm-chat-20b-w4kv8 model
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='internlm-chat-20b-w4kv8-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=16,
+#         concurrency=16,
+#         meta_template=meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
---
+++ opencompass/models/pytorch_poc.py
@@ -0,0 +1,171 @@
+import os.path as osp
+from concurrent.futures import ThreadPoolExecutor
+from typing import Dict, List, Optional, Union
+
+from lmdeploy.pytorch_poc import engine as tm
+from lmdeploy.tokenizer import Tokenizer
+from transformers import AutoTokenizer
+
+from opencompass.models.base import BaseModel
+from opencompass.utils.logging import get_logger
+from opencompass.utils.prompt import PromptList
+from lmdeploy.pytorch_poc.messages import SamplingParam
+import random
+
+PromptType = Union[PromptList, str]
+
+
+def valid_str(string, coding='utf-8'):
+    """decode text according to its encoding type."""
+    invalid_chars = [b'\xef\xbf\xbd']
+    bstr = bytes(string, coding)
+    for invalid_char in invalid_chars:
+        bstr = bstr.replace(invalid_char, b'')
+    ret = bstr.decode(encoding=coding, errors='ignore')
+    return ret
+
+
+class PytorchModel(BaseModel):
+    """Model wrapper for TurboMind Python API.
+
+    Args:
+        path (str): path of the turbomind model
+        max_seq_len (int): The maximum allowed sequence length of a model.
+            Note that the length of prompt + generated tokens shall not exceed
+            this value. Defaults to 2048.
+        meta_template (Dict, optional): The model's meta prompt
+            template if needed, in case the requirement of injecting or
+            wrapping of any meta instructions.
+    """
+
+    def __init__(
+        self,
+        path: str,
+        concurrency: int = 8,
+        max_seq_len: int = 2048,
+        meta_template: Optional[Dict] = None,
+    ):
+
+        super().__init__(path=path,
+                         max_seq_len=max_seq_len,
+                         meta_template=meta_template)
+        self.logger = get_logger()
+        self.tokenizer = AutoTokenizer.from_pretrained(path)
+        tm_model = tm.Engine(path)
+        self.generators = [
+            tm_model.create_instance() for i in range(concurrency)
+        ]
+        self.generator_ids = [i + 1 for i in range(concurrency)]
+
+    def generate(
+        self,
+        inputs: List[str],
+        max_out_len: int = 512,
+        temperature: float = 1.0,
+    ) -> List[str]:
+        """Generate results given a list of inputs.
+
+        Args:
+            inputs (List[str]): A list of prompts
+            max_out_len (int): The maximum length of the output.
+            temperature (float): What sampling temperature to use,
+                between 0 and 2. Higher values like 0.8 will make the output
+                more random, while lower values like 0.2 will make it more
+                focused and deterministic. Defaults to 1.0.
+
+        Returns:
+            List[str]: A list of generated strings.
+        """
+        assert isinstance(
+            inputs, List), f'List(str) is expected, but got {type(inputs)}'
+
+        # split inputs into batches
+        batch_size = len(self.generators)
+        batch_inputs = [
+            inputs[i:i + batch_size] for i in range(0, len(inputs), batch_size)
+        ]
+
+        results = []
+        for batch_input in batch_inputs:
+            with ThreadPoolExecutor() as executor:
+                _results = list(
+                    executor.map(self._generate,
+                                 self.generators[:len(batch_input)],
+                                 self.generator_ids[:len(batch_input)],
+                                 batch_input, [max_out_len] * len(batch_input),
+                                 [temperature] * len(batch_input)))
+                results += _results
+        return results
+
+    def get_token_len(self, prompt: str) -> int:
+        input_ids = self.tokenizer.encode(prompt)
+        return len(input_ids)
+
+    def wait(self):
+        """Wait till the next query can be sent.
+
+        Applicable in both single-thread and multi-thread environments.
+        """
+        return self.token_bucket.get_token()
+
+    def _generate(self, generator, session_id, prompt: str or PromptList,
+                  max_out_len: int, temperature: float) -> str:
+        """Generate results given a list of inputs.
+
+        Args:
+            prompt (str or PromptList): A string or PromptDict.
+                The PromptDict should be organized in OpenCompass'
+                API format.
+            max_out_len (int): The maximum length of the output.
+            temperature (float): What sampling temperature to use,
+                between 0 and 2. Higher values like 0.8 will make the output
+                more random, while lower values like 0.2 will make it more
+                focused and deterministic.
+
+        Returns:
+            str: The generated string.
+        """
+        assert type(
+            prompt) is str, 'We only support string for TurboMind Python API'
+        b_inst='[INST]'
+        e_inst='[/INST]'
+        b_sys='<<SYS>>\n'
+        e_sys='\n<</SYS>>\n\n'
+        system="""\
+You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
+
+If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
+        prompt = f'{b_inst} {b_sys} {system} {e_sys} {prompt} '
+        # print(prompt)
+        # print('*' * 40)
+
+        # prompt = '<BOS>' + prompt
+        # input_ids = self.tokenizer.encode(prompt)
+        input_ids = self.tokenizer.encode(prompt)
+        sampling_param = SamplingParam(
+            top_k=40,
+            top_p=0.8,
+            temperature=0.8,
+            repetition_penalty=1.0,
+            ignore_eos=False,
+            random_seed=random.getrandbits(64),
+        )
+        response_size = 0
+
+        for outputs in generator.stream_infer(session_id=session_id,
+                                            #   input_ids=input_ids,
+                                              prompt_token_ids=input_ids,
+                                              request_output_len=max_out_len,
+                                              step=0,
+                                              sampling_param=sampling_param):
+            status, res, tokens = outputs
+            # decode res
+            response_all = self.tokenizer.decode(res)
+            response_cur = response_all[response_size:]
+            response_all = valid_str(response_all)
+            response_size += len(response_cur)
+        if hasattr(generator, 'end'):
+            generator.end(session_id)
+        # print(f'response_all = {response_all}' + '\n end.')
+        # assert False
+        return response_all
