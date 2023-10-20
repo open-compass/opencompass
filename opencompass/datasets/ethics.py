383329e08ad2ce54a09a4c46d75deb7b10ddc8cf
KevinNuNu <34083603+KevinNuNu@users.noreply.github.com>
2023-10-21 01:41:22 +0800

    fix lint

--- opencompass/datasets/ethics.py
+++ opencompass/datasets/ethics.py
@@ -1,6 +1,6 @@
 import copy
 
-from datasets import load_dataset, DatasetDict, Dataset
+from datasets import Dataset, DatasetDict, load_dataset
 
 from opencompass.registry import LOAD_DATASET
 
@@ -14,19 +14,22 @@ class EthicsUtilitarianismDataset(BaseDataset):
     def load(path: str, name: str):
         dataset = load_dataset(path=path, name=name)
         new_dataset = DatasetDict()
-        splits = ["train", "validation", "test"]
+        splits = ['train', 'validation', 'test']
 
         for split in splits:
             examples = []
             for example in dataset[split]:
                 example1 = copy.deepcopy(example)
-                example1['scenario_A'], example1['scenario_B'] = example['baseline'], example['less_pleasant']
+                example1['scenario_A'], example1['scenario_B'] = \
+                    example['baseline'], example['less_pleasant']
                 example1['label'] = 1
                 examples.append(example1)
                 example2 = copy.deepcopy(example)
-                example2['scenario_A'], example2['scenario_B'] = example['less_pleasant'], example['baseline']
+                example2['scenario_A'], example2['scenario_B'] = \
+                    example['less_pleasant'], example['baseline']
                 example2['label'] = 0
                 examples.append(example2)
-            new_dataset[split] = Dataset.from_list(examples).remove_columns(['baseline', 'less_pleasant'])
+            new_dataset[split] = Dataset.from_list(examples).remove_columns(
+                ['baseline', 'less_pleasant'])
 
         return new_dataset
