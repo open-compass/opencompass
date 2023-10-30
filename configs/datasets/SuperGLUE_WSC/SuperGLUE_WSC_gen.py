a5f6fd8652e2c736c6f4ca1899e7b57c16e5812d
wq.chu <wq.chu@tianrang-inc.com>
2023-10-30 11:03:10 +0800

    fix typo in WSC prompt

--- configs/datasets/SuperGLUE_WSC/SuperGLUE_WSC_gen.py
+++ configs/datasets/SuperGLUE_WSC/SuperGLUE_WSC_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .SuperGLUE_WSC_gen_8a881c import WSC_datasets  # noqa: F401, F403
+    from .SuperGLUE_WSC_gen_fe4bf3 import WSC_datasets  # noqa: F401, F403
--- configs/datasets/SuperGLUE_WSC/SuperGLUE_WSC_ppl.py
+++ configs/datasets/SuperGLUE_WSC/SuperGLUE_WSC_ppl.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .SuperGLUE_WSC_ppl_cbf31c import WSC_datasets  # noqa: F401, F403
+    from .SuperGLUE_WSC_ppl_1c4a90 import WSC_datasets  # noqa: F401, F403
