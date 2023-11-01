32492ab2be11a3783b57386a72a01c02b8c8c28e
KevinNuNu <34083603+KevinNuNu@users.noreply.github.com>
2023-11-01 22:43:30 +0800

    [Feature] download from modelscope

--- opencompass/models/__init__.py
+++ opencompass/models/__init__.py
@@ -2,9 +2,9 @@ from .base import BaseModel, LMTemplateParser  # noqa
 from .base_api import APITemplateParser, BaseAPIModel  # noqa
 from .claude_api import Claude  # noqa: F401
 from .glm import GLM130B  # noqa: F401, F403
-from .huggingface import HuggingFace  # noqa: F401, F403
-from .huggingface import HuggingFaceCausalLM  # noqa: F401, F403
+from .huggingface import HuggingFace, HuggingFaceCausalLM  # noqa: F401, F403
 from .intern_model import InternLM  # noqa: F401, F403
 from .llama2 import Llama2, Llama2Chat  # noqa: F401, F403
+from .modelscope import ModelScope, ModelScopeCausalLM  # noqa: F401, F403
 from .openai_api import OpenAI  # noqa: F401
 from .zhipuai import ZhiPuAI  # noqa: F401
