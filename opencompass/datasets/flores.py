3b775d89cee424f62254a3b4f6d3b6153d2094fd
Leymore <zfz-960727@163.com>
2023-11-10 15:53:56 +0800

    update flores_first100

--- opencompass/datasets/flores.py
+++ opencompass/datasets/flores.py
@@ -1,6 +1,7 @@
+import os
 import re
 
-from datasets import DatasetDict, load_dataset
+from datasets import Dataset, DatasetDict
 
 from opencompass.registry import LOAD_DATASET, TEXT_POSTPROCESSORS
 
@@ -11,15 +12,30 @@ from .base import BaseDataset
 class FloresFirst100Dataset(BaseDataset):
 
     @staticmethod
-    def load(name):
-        return DatasetDict({
-            'dev':
-            load_dataset(path='facebook/flores', name=name, split='dev'),
-            'devtest':
-            load_dataset(path='facebook/flores',
-                         name=name,
-                         split='devtest[:100]')
-        })
+    def load_single(src_path, tgt_path, src_lang, tgt_lang):
+
+        with open(src_path, 'r', encoding='utf-8') as f:
+            src_lines = f.readlines()
+        with open(tgt_path, 'r', encoding='utf-8') as f:
+            tgt_lines = f.readlines()
+        assert len(src_lines) == len(tgt_lines)
+        dataset_list = [{
+            f'sentence_{src_lang}': src_lines[i].strip(),
+            f'sentence_{tgt_lang}': tgt_lines[i].strip(),
+        } for i in range(len(src_lines))]
+        return Dataset.from_list(dataset_list)
+
+    @staticmethod
+    def load(path, name):
+        src_lang, tgt_lang = name.split('-')
+        dev_dataset = FloresFirst100Dataset.load_single(
+            os.path.join(path, 'dev', f'{src_lang}.dev'),
+            os.path.join(path, 'dev', f'{tgt_lang}.dev'), src_lang, tgt_lang)
+        devtest_dataset = FloresFirst100Dataset.load_single(
+            os.path.join(path, 'devtest', f'{src_lang}.devtest'),
+            os.path.join(path, 'devtest', f'{tgt_lang}.devtest'), src_lang,
+            tgt_lang)
+        return DatasetDict({'dev': dev_dataset, 'devtest': devtest_dataset})
 
 
 @TEXT_POSTPROCESSORS.register_module('flores')
