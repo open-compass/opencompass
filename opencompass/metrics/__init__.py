7a1d604dd769f0b51c35e8d66e51629fad576fd5
teowu <realtimothyhwu@gmail.com>
2023-11-14 02:42:08 +0800

    add init

--- opencompass/metrics/__init__.py
+++ opencompass/metrics/__init__.py
@@ -1,5 +1,6 @@
 from .dump_results import DumpResults
+from .qbench import DumpResultsforQBench
 from .mme_score import MMEMetric
 from .seedbench import SEEDBenchAcc
 
-__all__ = ['DumpResults', 'SEEDBenchAcc', 'MMEMetric']
+__all__ = ['DumpResults', 'DumpResultsforQBench', 'SEEDBenchAcc', 'MMEMetric']
--- opencompass/multimodal/datasets/__init__.py
+++ opencompass/multimodal/datasets/__init__.py
@@ -1,6 +1,7 @@
 from .mmbench import MMBenchDataset  # noqa: F401, F403
 from .mme import MMEDataset  # noqa: F401, F403
 from .seedbench import SEEDBenchDataset  # noqa: F401, F403
+from .qbench import QBenchDataset
 
-__all__ = ['MMBenchDataset'
+__all__ = ['MMBenchDataset', 'QBenchDataset',
            'SEEDBenchDataset', 'MMEDataset']
