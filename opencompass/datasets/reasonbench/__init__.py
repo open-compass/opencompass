01401c289a6d30f687fb5e6d4f11388625b54750
Skyfall-xzz <17307130166@fudan.edu.cn>
2023-11-14 22:31:03 +0800

    lint the code to meet pre-commit requirements

--- opencompass/datasets/reasonbench/__init__.py
+++ opencompass/datasets/reasonbench/__init__.py
@@ -1,3 +1,3 @@
-from .FourOptionDataset import FourOptionDataset  # noqa: F401, F403
-from .ThreeOptionDataset import ThreeOptionDataset  # noqa: F401, F403
-from .TwoOptionDataset import TwoOptionDataset  # noqa: F401, F403
+from .FourOptionDataset import *  # noqa: F401, F403
+from .ThreeOptionDataset import *  # noqa: F401, F403
+from .TwoOptionDataset import *  # noqa: F401, F403
