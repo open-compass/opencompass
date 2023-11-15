cd4f37f64683146443226e1db54dae8e53f2f2e4
mk2x15 <mk2x15@gmail.com>
2023-11-16 00:01:24 +0800

    append dataset to init

--- opencompass/datasets/__init__.py
+++ opencompass/datasets/__init__.py
@@ -87,3 +87,8 @@ from .xcopa import *  # noqa: F401, F403
 from .xiezhi import XiezhiDataset, XiezhiRetriever  # noqa: F401, F403
 from .xlsum import *  # noqa: F401, F403
 from .xsum import *  # noqa: F401, F403
+from. xquad import *
+from. wisesight_senti import *
+from. limesoda import *
+from. wikilingua_doc import *
+from. massive import *
