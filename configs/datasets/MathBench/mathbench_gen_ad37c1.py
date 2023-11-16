0eec2b15e4e26ea19e567a428e4d3f2df4f19fd4
liushz <qq1791167085@163.com>
2023-11-16 14:10:46 +0800

    Update mathbench

--- configs/datasets/MathBench/mathbench_gen_ad37c1.py
+++ configs/datasets/MathBench/mathbench_gen_ad37c1.py
@@ -3,17 +3,17 @@ from opencompass.openicl.icl_retriever import ZeroRetriever
 from opencompass.openicl.icl_inferencer import GenInferencer
 from opencompass.openicl.icl_evaluator import CircularEvaluator, AccEvaluator
 from opencompass.datasets import MathBenchDataset, mathbench_postprocess
-from opencompass.utils.text_postprocessors import first_capital_postprocess
+from opencompass.utils.text_postprocessors import first_option_postprocess
 
 
 single_choice_prompts = {
     "single_choice_cn_with_reasoning": "以下是一道关于数学的单项选择题，请你一步一步推理并得到最终的答案选项。回答格式为如下：\n答案选项：A、B、C、D中你认为正确的一个选项\n计算过程：根据题目得到选项答案的一步步过程\n请严格按照上面的格式回答问题，下面是你要回答的题目：\n{question}\n答案选项：",
-    "single_choice_cn": "以下是一道关于数学的单项选择题，请你给出正确的答案选项。\n下面是你要回答的题目：\n{question}\n答案选项：",
+    "single_choice_cn": "以下是一道关于数学的单项选择题，请你直接回答正确答案的选项序号。\n下面是你要回答的题目：\n{question}\n答案选项：",
     "single_choice_en_with_reasoning": "Here is a multiple-choice question about mathematics. Please provide the final answer option by step-by-step reasoning. Please answer in the following format:\nAnswer option: A, B, C, or D (the option you believe is correct)\nCalculation process: Step-by-step process to derive the answer option based on the question\nPlease strictly follow the above format to answer the question. Here is the question you need to answer:\n{question}\nAnswer option:",
     "single_choice_en": "Here is a multiple-choice question about mathematics. Please provide the correct answer option directly.\nHere is the question you need to answer:\n{question}\nAnswer option:",
 }
 
-cloze_prompts={
+cloze_prompts ={
     "cloze_cn": [
                 dict(role='HUMAN', prompt='Q: 林中有15棵树。林务工人员今天将在林中种植树木。完成后，将有21棵树。林务工人员今天种植了多少棵树？'),
                 dict(role='BOT', prompt='A: 我们从15棵树开始。后来有21棵树。差值必定是他们种植的树木数量。所以，他们必须种植了21 - 15 = 6棵树。答案是 6\n'),
@@ -33,7 +33,7 @@ cloze_prompts={
                 dict(role='BOT', prompt='A: 她以每个3美元的价格买了5个百吉饼。这意味着她在百吉饼上花费了5 * 3 = 15美元。她一开始有23美元，所以现在她还剩下23 - 15 = 8美元。答案是 8\n'),
                 dict(role='HUMAN', prompt='Q: {question}'),
                 dict(role='BOT', prompt='A: {answer}'),
-                ],
+                ], 
     "cloze_en": [
                 dict(role='HUMAN', prompt='Q: There are 15 trees in the grove. Grove workers will plant trees in the grove today. After they are done, there will be 21 trees. How many trees did the grove workers plant today?'),
                 dict(role='BOT', prompt='A: We start with 15 trees. Later we have 21 trees. The difference must be the number of trees they planted. So, they must have planted 21 - 15 = 6 trees. The answer is 6.\n'),
@@ -53,16 +53,15 @@ cloze_prompts={
                 dict(role='BOT', prompt='A: She bought 5 bagels for $3 each. This means she spent 5 * $3 = $15 on the bagels. She had $23 in beginning, so now she has $23 - $15 = $8. The answer is 8.\n'),
                 dict(role='HUMAN', prompt='Q: {question}'),
                 dict(role='BOT', prompt='A: {answer}\n'),
-],
-}
-
+]}
 
 mathbench_sets = {
     'college': ['single_choice_cn', 'cloze_en'],
     'high': ['single_choice_cn', 'single_choice_en'],
     'middle': ['single_choice_cn'],
-    'primary': ['cloze_cn'],
+    'primary': ['cloze_cn']
 }
+
 
 # Generate reasoning path if set True or just generate the final answer
 with_reasoning = False
@@ -75,10 +74,9 @@ mathbench_datasets = []
 for _split in list(mathbench_sets.keys()):
     for _name in mathbench_sets[_split]:
         mathbench_infer_cfg = dict(
-            ice_template=dict(
+            prompt_template=dict(
                 type=PromptTemplate,
                 template=dict(
-                    begin="</E>",
                     round=[
                         dict(
                             role="HUMAN",
@@ -86,15 +84,14 @@ for _split in list(mathbench_sets.keys()):
                         ),
                         dict(role="BOT", prompt="{answer}")] if 'choice' in _name else cloze_prompts[_name],
                     ),
-                ice_token="</E>",
             ),
             retriever=dict(type=ZeroRetriever),
-            inferencer=dict(type=GenInferencer, max_out_len=512,),
+            inferencer=dict(type=GenInferencer, max_out_len=512),
         )
 
         mathbench_eval_cfg = dict(
             evaluator=dict(type=CircularEvaluator if 'choice' in _name else AccEvaluator),
-            pred_postprocessor=dict(type=first_capital_postprocess) if 'single_choice' in _name else dict(type=mathbench_postprocess, name=_name))
+            pred_postprocessor=dict(type=first_option_postprocess, options='ABCD') if 'single_choice' in _name else dict(type=mathbench_postprocess, name=_name))
 
         mathbench_datasets.append(
             dict(
@@ -110,5 +107,3 @@ for _split in list(mathbench_sets.keys()):
                 infer_cfg=mathbench_infer_cfg,
                 eval_cfg=mathbench_eval_cfg,
             ))
-
-del _split, _name
