55e012ec8c7da61e900bbd913136837263932c2f
helloyongyang <yongyang1030@163.com>
2023-11-20 03:22:41 +0800

    [Feat] support lightllm api

---
+++ configs/eval_lightllm.py
@@ -0,0 +1,31 @@
+from mmengine.config import read_base
+from opencompass.models import LightllmApi
+from opencompass.partitioners import NaivePartitioner
+from opencompass.runners import LocalRunner
+from opencompass.tasks import OpenICLInferTask
+
+with read_base():
+    from .datasets.humaneval.humaneval_gen import humaneval_datasets
+
+datasets = [*humaneval_datasets]
+
+models = [
+    dict(abbr='LightllmApi',
+        type=LightllmApi,
+        url='http://localhost:8080/generate',
+        max_out_len=1024,
+        batch_size=8,
+        generation_kwargs=dict(
+            do_sample=False,
+            ignore_eos=False,
+            ),
+        ),
+]
+
+infer = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=8,
+        task=dict(type=OpenICLInferTask)),
+)
