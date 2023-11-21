a22914ca797b723c706c64b9afa3e32c08da16e0
tonysy <sy.zhangbuaa@gmail.com>
2023-11-21 18:55:04 +0800

    Update API

---
+++ configs/eval_api_sensetime.py
@@ -0,0 +1,36 @@
+from mmengine.config import read_base
+from opencompass.models import SenseTime
+from opencompass.partitioners import NaivePartitioner
+from opencompass.runners import LocalRunner
+from opencompass.runners.local_api import LocalAPIRunner
+from opencompass.tasks import OpenICLInferTask
+
+with read_base():
+    # from .datasets.collections.chat_medium import datasets
+    from .summarizers.medium import summarizer
+    from .datasets.ceval.ceval_gen import ceval_datasets
+
+datasets = [
+    *ceval_datasets,
+]
+
+models = [
+    dict(
+        abbr='nova-ptc-xl-v1',
+        type=SenseTime,
+        path='nova-ptc-xl-v1',
+        key='xxxxxxxxxxxxxx',
+        query_per_second=1,
+        max_out_len=2048,
+        max_seq_len=2048,
+        batch_size=8),
+]
+
+infer = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalAPIRunner,
+        max_num_workers=2,
+        concurrent_users=2,
+        task=dict(type=OpenICLInferTask)),
+)
--- opencompass/models/__init__.py
+++ opencompass/models/__init__.py
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 from .alaya import AlayaLM  # noqa: F401
-=======
 from .ai360_api import AI360GPT  # noqa: F401
 from .baichuan_api import BaiChuan  # noqa: F401
->>>>>>> Update API
+from .baidu_api import ERNIEBot  # noqa: F401
 from .base import BaseModel, LMTemplateParser  # noqa
 from .base_api import APITemplateParser, BaseAPIModel  # noqa
+from .bytedance_api import ByteDance  # noqa: F401
 from .claude_api import Claude  # noqa: F401
 from .glm import GLM130B  # noqa: F401, F403
 from .huggingface import HuggingFace  # noqa: F401, F403
@@ -16,6 +15,6 @@ from .llama2 import Llama2, Llama2Chat  # noqa: F401, F403
 from .minimax_api import MiniMax  # noqa: F401
 from .openai_api import OpenAI  # noqa: F401
 from .pangu_api import PanGu  # noqa: F401
+from .sensetime_api import SenseTime  # noqa: F401
 from .xunfei_api import XunFei  # noqa: F401
 from .zhipuai_api import ZhiPuAI  # noqa: F401
-from .ai360_api import AI360GPT  # noqa: F401
