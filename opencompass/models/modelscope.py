e34aaac76f3e43284d2c75606d3ef5d48d075d86
yingfhu <yingfhu@gmail.com>
2023-11-22 15:29:03 +0800

    minor fix

--- opencompass/models/modelscope.py
+++ opencompass/models/modelscope.py
@@ -1,19 +1,15 @@
-import os
-from typing import Dict, List, Optional, Union
+from typing import Dict, Optional, Union
 
-import numpy as np
 import torch
 
-from opencompass.models.base import BaseModel
-from opencompass.registry import MODELS
-from opencompass.utils.logging import get_logger
 from opencompass.utils.prompt import PromptList
+
+from .huggingface import HuggingFace
 
 PromptType = Union[PromptList, str]
 
 
-@MODELS.register_module()
-class ModelScope(BaseModel):
+class ModelScope(HuggingFace):
     """Model wrapper around ModelScope models.
 
     Args:
@@ -67,27 +63,21 @@ class ModelScope(BaseModel):
                  batch_padding: bool = False,
                  pad_token_id: Optional[int] = None,
                  mode: str = 'none'):
-        super().__init__(path=path,
-                         max_seq_len=max_seq_len,
-                         tokenizer_only=tokenizer_only,
-                         meta_template=meta_template)
-        from opencompass.utils.fileio import patch_hf_auto_model
-        if ms_cache_dir is None:
-            ms_cache_dir = os.getenv('MS_MODEL_HUB', None)
-        patch_hf_auto_model(ms_cache_dir)
-        self.logger = get_logger()
-        self.pad_token_id = pad_token_id
-        assert mode in ['none', 'mid']
-        self.mode = mode
-        self._load_tokenizer(path=path,
-                             tokenizer_path=tokenizer_path,
-                             tokenizer_kwargs=tokenizer_kwargs)
-        self.batch_padding = batch_padding
-        self.extract_pred_after_decode = extract_pred_after_decode
-        if not tokenizer_only:
-            self._load_model(path=path,
-                             model_kwargs=model_kwargs,
-                             peft_path=peft_path)
+        super().__init__(
+            path=path,
+            hf_cache_dir=ms_cache_dir,
+            max_seq_len=max_seq_len,
+            tokenizer_path=tokenizer_path,
+            tokenizer_kwargs=tokenizer_kwargs,
+            peft_path=peft_path,
+            tokenizer_only=tokenizer_only,
+            model_kwargs=model_kwargs,
+            meta_template=meta_template,
+            extract_pred_after_decode=extract_pred_after_decode,
+            batch_padding=batch_padding,
+            pad_token_id=pad_token_id,
+            mode=mode,
+        )
 
     def _load_tokenizer(self, path: str, tokenizer_path: Optional[str],
                         tokenizer_kwargs: dict):
@@ -181,226 +171,7 @@ class ModelScope(BaseModel):
             self.model.config.eos_token_id = 2
             self.model.config.pad_token_id = self.tokenizer.pad_token_id
 
-    def generate(self, inputs: List[str], max_out_len: int,
-                 **kwargs) -> List[str]:
-        """Generate results given a list of inputs.
 
-        Args:
-            inputs (List[str]): A list of strings.
-            max_out_len (int): The maximum length of the output.
-
-        Returns:
-            List[str]: A list of generated strings.
-        """
-        if self.batch_padding and len(inputs) > 1:
-            return self._batch_generate(inputs=inputs,
-                                        max_out_len=max_out_len,
-                                        **kwargs)
-        else:
-            return sum((self._single_generate(
-                inputs=[input_], max_out_len=max_out_len, **kwargs)
-                        for input_ in inputs), [])
-
-    def _batch_generate(self, inputs: List[str], max_out_len: int,
-                        **kwargs) -> List[str]:
-        """Support for batch prompts inference.
-
-        Args:
-            inputs (List[str]): A list of strings.
-            max_out_len (int): The maximum length of the output.
-
-        Returns:
-            List[str]: A list of generated strings.
-        """
-        if self.extract_pred_after_decode:
-            prompt_lens = [len(input_) for input_ in inputs]
-
-        # step-1: tokenize the input with batch_encode_plus
-        tokens = self.tokenizer.batch_encode_plus(inputs,
-                                                  padding=True,
-                                                  truncation=True,
-                                                  max_length=self.max_seq_len -
-                                                  max_out_len)
-        tokens = {
-            k: torch.tensor(np.array(tokens[k]), device=self.model.device)
-            for k in tokens if k in ['input_ids', 'attention_mask']
-        }
-
-        # step-2: conduct model forward to generate output
-        outputs = self.model.generate(**tokens,
-                                      max_new_tokens=max_out_len,
-                                      **kwargs)
-
-        if not self.extract_pred_after_decode:
-            outputs = outputs[:, tokens['input_ids'].shape[1]:]
-
-        decodeds = self.tokenizer.batch_decode(outputs,
-                                               skip_special_tokens=True)
-
-        if self.extract_pred_after_decode:
-            decodeds = [
-                token[len_:] for token, len_ in zip(decodeds, prompt_lens)
-            ]
-
-        return decodeds
-
-    def _single_generate(self, inputs: List[str], max_out_len: int,
-                         **kwargs) -> List[str]:
-        """Support for single prompt inference.
-
-        Args:
-            inputs (List[str]): A list of strings.
-            max_out_len (int): The maximum length of the output.
-
-        Returns:
-            List[str]: A list of generated strings.
-        """
-        if self.extract_pred_after_decode:
-            prompt_lens = [len(input_) for input_ in inputs]
-
-        if self.mode == 'mid':
-            input_ids = self.tokenizer(inputs, truncation=False)['input_ids']
-            input_ids = torch.tensor(input_ids, device=self.model.device)
-            if len(input_ids[0]) > self.max_seq_len - max_out_len:
-                half = int((self.max_seq_len - max_out_len) / 2)
-                inputs = [
-                    self.tokenizer.decode(input_ids[0][:half],
-                                          skip_special_tokens=True) +
-                    self.tokenizer.decode(input_ids[0][-half:],
-                                          skip_special_tokens=True)
-                ]
-
-        input_ids = self.tokenizer(inputs,
-                                   truncation=True,
-                                   max_length=self.max_seq_len -
-                                   max_out_len)['input_ids']
-        input_ids = torch.tensor(input_ids, device=self.model.device)
-        # To accommodate the PeftModel, parameters should be passed in
-        # key-value format for generate.
-        outputs = self.model.generate(input_ids=input_ids,
-                                      max_new_tokens=max_out_len,
-                                      **kwargs)
-
-        if not self.extract_pred_after_decode:
-            outputs = outputs[:, input_ids.shape[1]:]
-
-        decodeds = self.tokenizer.batch_decode(outputs,
-                                               skip_special_tokens=True)
-
-        if self.extract_pred_after_decode:
-            decodeds = [
-                token[len_:] for token, len_ in zip(decodeds, prompt_lens)
-            ]
-
-        return decodeds
-
-    def get_logits(self, inputs: List[str]):
-
-        if self.batch_padding and len(inputs) > 1:
-            # batch inference
-            tokens = self.tokenizer(inputs,
-                                    padding=True,
-                                    truncation=True,
-                                    max_length=self.max_seq_len)
-
-            tokens = {
-                k: torch.tensor(np.array(tokens[k]), device=self.model.device)
-                for k in tokens if k in ['input_ids', 'attention_mask']
-            }
-            outputs = self.model(**tokens)
-
-        else:
-            input_ids = self.tokenizer(
-                inputs,
-                padding=False,
-                truncation=True,
-                max_length=self.max_seq_len)['input_ids']
-            input_ids = torch.tensor(input_ids, device=self.model.device)
-            tokens = {'input_ids': input_ids}
-
-            outputs = self.model(input_ids)
-        return outputs[0], {'tokens': tokens}
-
-    def get_ppl(self,
-                inputs: List[str],
-                mask_length: Optional[List[int]] = None) -> List[float]:
-        """Get perplexity scores given a list of inputs.
-
-        Args:
-            inputs (List[str]): A list of strings.
-            mask_length (Optional[List[int]]): A list of mask lengths. If
-                provided, the perplexity scores will be calculated with the
-                first mask_length[i] tokens masked out. It's okay to skip
-                its implementation if advanced features in PPLInfernecer is
-                not needed.
-
-        Returns:
-            List[float]: A list of perplexity scores.
-        """
-
-        if self.batch_padding and len(inputs) > 1:
-            assert self.tokenizer.pad_token
-            return self._get_ppl(inputs, mask_length=mask_length)
-        else:
-            return np.concatenate([
-                self._get_ppl(inputs=[text], mask_length=mask_length)
-                for text in inputs
-            ])
-
-    def _get_ppl(self,
-                 inputs: List[str],
-                 mask_length: Optional[List[int]] = None) -> List[float]:
-        """Get perplexity scores given a list of inputs.
-
-        Args:
-            inputs (List[str]): A list of strings.
-            mask_length (Optional[List[int]]): A list of mask lengths. If
-                provided, the perplexity scores will be calculated with the
-                first mask_length[i] tokens masked out. It's okay to skip
-                its implementation if advanced features in PPLInfernecer is
-                not needed.
-
-        Returns:
-            List[float]: A list of perplexity scores.
-        """
-
-        outputs, inputs = self.get_logits(inputs)
-        shift_logits = outputs[..., :-1, :].contiguous().float()
-
-        shift_labels = inputs['tokens']['input_ids'][..., 1:].contiguous()
-
-        loss_fct = torch.nn.CrossEntropyLoss(
-            reduction='none', ignore_index=self.tokenizer.pad_token_id)
-        loss = loss_fct(shift_logits.view(-1, shift_logits.size(-1)),
-                        shift_labels.view(-1)).view(shift_labels.size())
-
-        if mask_length is not None:
-            mask = torch.zeros_like(shift_labels)  # [batch,seqlen]
-            for i in range(len(mask)):
-                for j in range(mask_length[i] - 1, len(mask[i])):
-                    mask[i][j] = 1
-            loss = loss * mask
-
-        lens = (inputs['tokens']['input_ids'] !=
-                self.tokenizer.pad_token_id).sum(-1).cpu().numpy()
-        if mask_length is not None:
-            lens -= np.array(mask_length)
-        ce_loss = loss.sum(-1).cpu().detach().numpy() / lens
-        return ce_loss
-
-    def get_token_len(self, prompt: str) -> int:
-        """Get lengths of the tokenized strings.
-
-        Args:
-            prompt (str): Input string.
-
-        Returns:
-            int: Length of the input tokens
-        """
-        return len(self.tokenizer.encode(prompt))
-
-
-@MODELS.register_module()
 class ModelScopeCausalLM(ModelScope):
     """Model wrapper around ModelScope CausalLM.
 
