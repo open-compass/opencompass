b9b507f50c21e5f10e700bb254f5db829418712b
liuhongwei <liuhongwei@pjlab.org.cn>
2023-12-01 21:28:33 +0800

    Fix errors

--- configs/eval_code_mathbench.py
+++ configs/eval_code_mathbench.py
@@ -6,10 +6,7 @@ from opencompass.tasks import OpenICLInferTask
 from opencompass.models.lagent import LagentAgent
 from lagent import PythonInterpreter, ReAct
 from lagent.agents.react import ReActProtocol
-
-with read_base():
-    from .datasets.gsm8k.gsm8k_agent_gen_3ac57d import gsm8k_datasets as datasets
-
+    
 system_prompt = """You are a helpful assistant which use tools to solve mathematical reasoning questions. The code must be a function, and the function name must be 'solution'. For mathematics, please use code tool to calculate. The example format is as follows:
 ```
 def solution():
@@ -24,6 +21,10 @@ protocol = dict(
     finish=dict(role="FINISH", begin="FinalAnswer:", end="\n"),
     call_protocol=system_prompt,
 )
+
+with read_base():
+    from .datasets.MathBench.mathbench_code_gen_568903 import mathbench_code_datasets as datasets
+    from .summarizers.mathbench import summarizer
 
 models = [
     dict(
