4f536d2539331eaf611fd4655a6f0ea91804bf1c
bittersweet1999 <1487910649@qq.com>
2023-12-05 17:30:34 +0800

    add summarizer

--- opencompass/summarizers/__init__.py
+++ opencompass/summarizers/__init__.py
@@ -1,4 +1,5 @@
 from .default import DefaultSummarizer
+from .subject import SubjectSummarizer
 from .subjective import SubjectiveSummarizer
 
-__all__ = ['DefaultSummarizer', 'SubjectiveSummarizer']
+__all__ = ['DefaultSummarizer', 'SubjectiveSummarizer', 'SubjectSummarizer']
