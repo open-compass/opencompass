3a354bd1da9eabad19fa6ff1afe0132e46d55424
Fengzhe Zhou <zfz-960727@163.com>
2023-12-07 20:29:00 +0800

    add qwen and deepseek configs (#672)

---
+++ configs/models/deepseek/hf_deepseek_67b_chat.py
@@ -0,0 +1,32 @@
+from opencompass.models import HuggingFaceCausalLM
+
+_meta_template = dict(
+    round=[
+        dict(role="HUMAN", begin='User: ', end='\n\n'),
+        dict(role="BOT", begin="Assistant: ", end='<｜end▁of▁sentence｜>', generate=True),
+    ],
+)
+
+models = [
+    dict(
+        type=HuggingFaceCausalLM,
+        abbr='deepseek-67b-chat-hf',
+        path="deepseek-ai/deepseek-llm-67b-chat",
+        tokenizer_path='deepseek-ai/deepseek-llm-67b-chat',
+        model_kwargs=dict(
+            device_map='auto',
+            trust_remote_code=True,
+        ),
+        tokenizer_kwargs=dict(
+            padding_side='left',
+            truncation_side='left',
+            trust_remote_code=True,
+            use_fast=False,
+        ),
+        meta_template=_meta_template,
+        max_out_len=100,
+        max_seq_len=2048,
+        batch_size=8,
+        run_cfg=dict(num_gpus=4, num_procs=1),
+    )
+]
