b53e28ec8ba581916c1e5e416d1ec3a8ceca88dd
bittersweet1999 <1487910649@qq.com>
2023-12-13 18:36:56 +0800

    alignment all done

--- opencompass/summarizers/alignmentbench.py
+++ opencompass/summarizers/alignmentbench.py
@@ -7,6 +7,7 @@ from collections import defaultdict
 from datetime import datetime
 
 import mmengine
+import numpy as np
 from mmengine import ConfigDict
 
 try:
@@ -15,6 +16,16 @@ except ImportError:
     from_csv = None
 
 from opencompass.utils import dataset_abbr_from_cfg
+
+CATEGORIES = {
+    '中文推理': ['数学计算', '逻辑推理'],
+    '中文语言': ['基本任务', '中文理解', '综合问答', '文本写作', '角色扮演', '专业能力'],
+}
+
+all_dimensions = [
+    '事实正确性', '满足用户需求', '安全无害', '清晰度', '逻辑性', '完备性', '创造性', '可负责程度', '逻辑连贯性',
+    '公平与可负责程度', '丰富度', '综合得分'
+]
 
 
 def post_process(judgment: str):
@@ -43,8 +54,11 @@ def post_process(judgment: str):
 
     def check_rating(rating):
         for k, v in rating.items():
-            if isinstance(v, (int, float)) and ',' not in k:  # 确保值是数字
-                pass
+            if isinstance(v, (int, float)) and k in all_dimensions:  # 确保值是数字
+                if v >= 0 and v <= 10:
+                    pass
+                else:
+                    return None
             else:
                 return None
         return rating
@@ -56,6 +70,10 @@ def post_process(judgment: str):
         score = rating.get('综合得分', -1)
         if score == -1:
             score = extract_score(judgment)
+        if score >= 0 and score <= 10:
+            pass
+        else:
+            score = -1
         rating = check_rating(rating)
     else:
         score = -1
@@ -94,8 +112,9 @@ class AlignmentBenchSummarizer:
         output_dir = osp.join(osp.split(output_path)[0], f'{self.time_str}')
         mmengine.mkdir_or_exist(output_dir)
         results_folder = osp.join(work_dir, 'results')
-        fout = osp.join(output_dir, 'report.csv')
-        fout_flag = 0
+        fout = osp.join(output_dir, 'dimension.csv')
+        fout2 = osp.join(output_dir, 'capability.csv')
+        fout_flag, fout_flag2 = 0, 0
         for subdir in os.listdir(results_folder):
             subdir_path = os.path.join(results_folder, subdir)
             if os.path.isdir(subdir_path):
@@ -120,33 +139,88 @@ class AlignmentBenchSummarizer:
                     )
 
                     # 初始化一个嵌套字典用于存储模型和评分
-                    model_ratings = defaultdict(int)
-                    model_counts = defaultdict(int)
+                    dimension_ratings = defaultdict(int)
+                    dimension_counts = defaultdict(int)
+                    capability_ratings = defaultdict(int)
+                    capability_counts = defaultdict(int)
                     for ans, ref in zip(judged_answers, references):
                         for k, v in ans['rating'].items():
                             if k != '综合得分':
-                                model_ratings[k] += v
-                                model_counts[k] += 1
-                        model_ratings['综合得分'] += ans['score']
-                        model_counts['综合得分'] += 1
+                                dimension_ratings[k] += v
+                                dimension_counts[k] += 1
+                        dimension_ratings['综合得分'] += ans['score']
+                        dimension_counts['综合得分'] += 1
+                        capability_ratings[ref['capability']] += ans['score']
+                        capability_counts[ref['capability']] += 1
 
-                    model_avg_ratings = defaultdict(float)
-                    for dimension, total_score in model_ratings.items():
-                        model_avg_ratings[
-                            dimension] = total_score / model_counts[dimension]
+                    dimension_avg_ratings = defaultdict(float)
+                    capability_avg_ratings = defaultdict(float)
+                    for dimension, total_score in dimension_ratings.items():
+                        dimension_avg_ratings[
+                            dimension] = total_score / dimension_counts[
+                                dimension]
 
-                    scores = {model: model_avg_ratings}
+                    for capability, total_score in capability_ratings.items():
+                        capability_avg_ratings[
+                            capability] = total_score / capability_counts[
+                                capability]
+
+                    capability_avg_ratings['中文推理总分'] = np.mean([
+                        np.mean(capability_avg_ratings[cat])
+                        for cat in CATEGORIES['中文推理']
+                    ])
+                    capability_avg_ratings['中文语言总分'] = np.mean([
+                        np.mean(capability_avg_ratings[cat])
+                        for cat in CATEGORIES['中文语言']
+                    ])
+                    capability_avg_ratings['总分'] = (
+                        capability_avg_ratings['中文推理总分'] +
+                        capability_avg_ratings['中文语言总分']) / 2
+
+                    scores = {model: dimension_avg_ratings}
                     rows = list(scores.keys())
                     columns = list(scores[rows[0]].keys())
                     with open(fout, 'a+', newline='') as csvfile:
                         writer = csv.writer(csvfile)
                         if fout_flag == 0:
-                            writer.writerow([''] + columns)
+                            writer.writerow(['模型'] + columns)
                             fout_flag += 1
                         for row in rows:
                             writer.writerow(
                                 [row] +
                                 [scores[row][column] for column in columns])
+
+                    scores = {model: capability_avg_ratings}
+                    with open(fout2, 'a+', newline='') as csvfile:
+                        writer = csv.writer(csvfile)
+                        if fout_flag2 == 0:
+                            num_header = [str(i) for i in range(12)]
+                            writer.writerow(num_header)
+
+                            header = ['模型', '总分']
+                            for category, sub_categories in CATEGORIES.items():
+                                header.append(category)
+                                header.extend(
+                                    [None for _ in range(len(sub_categories))])
+                            writer.writerow(header)
+
+                            sub_header = ['模型', '总分']
+                            for category, sub_categories in CATEGORIES.items():
+                                sub_header.extend([category + '总分'])
+                                sub_header.extend(sub_categories)
+                            writer.writerow(sub_header)
+                            fout_flag2 += 1
+
+                        row = [model]
+                        row.append(scores[model]['总分'])
+                        for category, sub_categories in CATEGORIES.items():
+                            row.append(scores[model][category + '总分'])
+                            for sub_category in sub_categories:
+                                row.append(scores[model][sub_category])
+                        writer.writerow(row)
         with open(fout, 'r') as f:
+            x = from_csv(f)
+        print(x)
+        with open(fout2, 'r') as f:
             x = from_csv(f)
         print(x)
