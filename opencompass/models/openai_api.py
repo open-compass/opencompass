73eebc2d859590b0da44b216aea828ffc8818372
tonysy <sy.zhangbuaa@gmail.com>
2023-12-14 18:15:19 +0800

    update alignmentbench

--- opencompass/models/openai_api.py
+++ opencompass/models/openai_api.py
@@ -384,11 +384,8 @@ class OpenAIAllesAPIN(OpenAI):
                 elif item['role'] == 'SYSTEM':
                     msg['role'] = 'system'
                 messages.append(msg)
-<<<<<<< HEAD
+
             # model can be response with user and system
-=======
-            # model can be reponse with user and system
->>>>>>> update alignmentbench
             # when it comes with agent involved.
             assert msg['role'] in ['user', 'system']
         data = {
