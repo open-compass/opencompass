31e5f83fd11e99f41a01a7833ee4cdd2c0f6103b
RunningLeon <mnsheng@yeah.net>
2023-12-15 12:48:30 +0800

    test w4

--- configs/eval_internlm_chat_turbomind.py
+++ configs/eval_internlm_chat_turbomind.py
@@ -5,13 +5,13 @@ from opencompass.models.turbomind import TurboMindModel
 with read_base():
     # choose a list of datasets
     from .datasets.mmlu.mmlu_gen_a484b3 import mmlu_datasets
-    from .datasets.ceval.ceval_gen_5f30c7 import ceval_datasets
-    from .datasets.SuperGLUE_WiC.SuperGLUE_WiC_gen_d06864 import WiC_datasets
-    from .datasets.SuperGLUE_WSC.SuperGLUE_WSC_gen_7902a7 import WSC_datasets
-    from .datasets.triviaqa.triviaqa_gen_2121ce import triviaqa_datasets
+    # from .datasets.ceval.ceval_gen_5f30c7 import ceval_datasets
+    # from .datasets.SuperGLUE_WiC.SuperGLUE_WiC_gen_d06864 import WiC_datasets
+    # from .datasets.SuperGLUE_WSC.SuperGLUE_WSC_gen_7902a7 import WSC_datasets
+    # from .datasets.triviaqa.triviaqa_gen_2121ce import triviaqa_datasets
     from .datasets.gsm8k.gsm8k_gen_1d7fe4 import gsm8k_datasets
-    from .datasets.race.race_gen_69ee4f import race_datasets
-    from .datasets.crowspairs.crowspairs_gen_381af0 import crowspairs_datasets
+    # from .datasets.race.race_gen_69ee4f import race_datasets
+    # from .datasets.crowspairs.crowspairs_gen_381af0 import crowspairs_datasets
     # and output the results in a choosen format
     from .summarizers.medium import summarizer
 
@@ -38,6 +38,7 @@ qwen_meta_template = dict(
         dict(role="BOT", begin="\n<|im_start|>assistant\n", end='<|im_end|>', generate=True),
     ],
 )
+
 # config for internlm-chat-7b
 # models = [
 #     dict(
@@ -99,19 +100,19 @@ qwen_meta_template = dict(
 # ]
 
 # config for internlm-chat-20b-w4 model
-# models = [
-#     dict(
-#         type=TurboMindModel,
-#         abbr='internlm-chat-20b-w4-turbomind',
-#         path="./turbomind",
-#         max_out_len=100,
-#         max_seq_len=2048,
-#         batch_size=16,
-#         concurrency=16,
-#         meta_template=meta_template,
-#         run_cfg=dict(num_gpus=1, num_procs=1),
-#     )
-# ]
+models = [
+    dict(
+        type=TurboMindModel,
+        abbr='internlm-chat-20b-w4-turbomind',
+        path="./turbomind",
+        max_out_len=100,
+        max_seq_len=2048,
+        batch_size=16,
+        concurrency=16,
+        meta_template=meta_template,
+        run_cfg=dict(num_gpus=1, num_procs=1),
+    )
+]
 
 # config for internlm-chat-20b-w4kv8 model
 # models = [
@@ -144,16 +145,16 @@ qwen_meta_template = dict(
 # ]
 
 # config for qwen-chat-7b
-models = [
-    dict(
-        type=TurboMindModel,
-        abbr='qwen-chat-7b-turbomind',
-        path="./turbomind",
-        max_out_len=100,
-        max_seq_len=2048,
-        batch_size=16,
-        concurrency=32,
-        meta_template=llama2_meta_template,
-        run_cfg=dict(num_gpus=1, num_procs=1),
-    )
-]
+# models = [
+#     dict(
+#         type=TurboMindModel,
+#         abbr='qwen-chat-7b-turbomind',
+#         path="./turbomind",
+#         max_out_len=100,
+#         max_seq_len=2048,
+#         batch_size=16,
+#         concurrency=32,
+#         meta_template=qwen_meta_template,
+#         run_cfg=dict(num_gpus=1, num_procs=1),
+#     )
+# ]
