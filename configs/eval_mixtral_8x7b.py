d7e7a637a5a513c237f02b4cea8079b27a603427
Jingming <109033042+jingmingzhuo@users.noreply.github.com>
2023-12-15 14:15:32 +0800

    [Fix] fix a bug on configs/eval_mixtral_8x7b.py (#706)

--- configs/eval_mixtral_8x7b.py
+++ configs/eval_mixtral_8x7b.py
@@ -2,7 +2,7 @@ from mmengine.config import read_base
 
 with read_base():
     from .datasets.collections.base_medium_llama import piqa_datasets, siqa_datasets
-    from .models.mistral.mixtral_8x7b_32k import models
+    from .models.mixtral.mixtral_8x7b_32k import models
 
 
 datasets = [*piqa_datasets, *siqa_datasets]
