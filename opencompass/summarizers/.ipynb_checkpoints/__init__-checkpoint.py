f9be9e5194efd1cc9638cdc7273437e85394ee54
csatsurnh <cshan1995@126.com>
2023-12-19 17:44:13 +0800

    remove

--- opencompass/summarizers/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,7 +0,0 @@
-from .alignmentbench import AlignmentBenchSummarizer  # noqa: F401
-from .circular import CircularSummarizer  # noqa: F401
-from .corev2 import Corev2Summarizer  # noqa: F401
-from .creationbench import CreationBenchSummarizer  # noqa: F401
-from .creationv01 import Creationv01Summarizer  # noqa: F401
-from .default import DefaultSummarizer  # noqa: F401
-from .subjective import SubjectiveSummarizer  # noqa: F401
