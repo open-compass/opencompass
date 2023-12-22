f47a547af16ef4a396e084a9155809157542d100
Francis-llgg <102740968+Francis-llgg@users.noreply.github.com>
2023-12-22 10:41:33 +0800

    Add files via upload

---
+++ configs/datasets/gpqa/gpqa_extended.py
@@ -0,0 +1,4 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .gpqa_extended_gen_956c9f import gpqa_extended_datasets
---
+++ configs/datasets/gpqa/gpqa_main.py
@@ -0,0 +1,4 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .gpqa_main_gen_956c9f import gpqa_main_datasets
