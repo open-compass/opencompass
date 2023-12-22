a9ef62fcb80a260d478c9a63d4be03139543bd96
Francis-llgg <102740968+Francis-llgg@users.noreply.github.com>
2023-12-22 12:38:14 +0800

    Delete configs/datasets/gpqa directory

--- configs/datasets/gpqa/gpqa_extended.py
+++
@@ -1,4 +0,0 @@
-from mmengine.config import read_base
-
-with read_base():
-    from .gpqa_extended_gen_956c9f import gpqa_extended_datasets
--- configs/datasets/gpqa/gpqa_main.py
+++
@@ -1,4 +0,0 @@
-from mmengine.config import read_base
-
-with read_base():
-    from .gpqa_main_gen_956c9f import gpqa_main_datasets
