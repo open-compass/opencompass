dfd9ac0fd91eaf1ed652d152f1bfd5f0d94d5d46
bittersweet1999 <148421775+bittersweet1999@users.noreply.github.com>
2023-12-27 17:54:53 +0800

    [Feature] Add other judgelm prompts for Alignbench (#731)
    * add judgellm prompts
    
    * add judgelm prompts
    
    * update import info
    
    * fix situation that no abbr in config
    
    * fix situation that no abbr in config
    
    * add summarizer for other judgellm
    
    * change config name
    
    * add maxlen
    
    * add maxlen
    
    * dict assert
    
    * dict assert
    
    * fix strings
    
    * fix strings

--- configs/models/judge_llm/auto_j/hf_autoj_eng_13b.py
+++ configs/models/judge_llm/auto_j/hf_autoj_eng_13b.py
@@ -1,8 +1,7 @@
 from opencompass.models import HuggingFaceCausalLM
 
 
-models = [
-    dict(
+models = [dict(
         type=HuggingFaceCausalLM,
         abbr='autoj-13b-GPTQ-4bits',
         path="GAIR/autoj-13b-GPTQ-4bits",
@@ -16,5 +15,4 @@ models = [
         batch_size=8,
         model_kwargs=dict(device_map='auto', trust_remote_code=True),
         run_cfg=dict(num_gpus=1, num_procs=1),
-    )
-]
+    )]
--- opencompass/summarizers/__init__.py
+++ opencompass/summarizers/__init__.py
@@ -1,4 +1,6 @@
-from .alignmentbench import AlignmentBenchSummarizer  # noqa: F401
+# flake8: noqa: F401, E501
+from .alignmentbench import (AlignmentBenchSummarizer, AutojSummarizer,
+                             JudgeLMSummarizer)
 from .circular import CircularSummarizer  # noqa: F401
 from .corev2 import Corev2Summarizer  # noqa: F401
 from .creationv01 import Creationv01Summarizer  # noqa: F401
