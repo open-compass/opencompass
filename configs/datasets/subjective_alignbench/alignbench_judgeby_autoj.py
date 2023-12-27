f5a735926dbee3fa7c79838b79204a410ec673cf
bittersweet1999 <1487910649@qq.com>
2023-12-27 16:24:25 +0800

    fix strings

--- configs/datasets/subjective_alignbench/alignbench_judgeby_autoj.py
+++ configs/datasets/subjective_alignbench/alignbench_judgeby_autoj.py
@@ -28,7 +28,7 @@ for _name in subjective_all_sets:
                 ]),
             ),
             retriever=dict(type=ZeroRetriever),
-            inferencer=dict(type=GenInferencer， max_out_len=1024),
+            inferencer=dict(type=GenInferencer, max_out_len=2048),
         )
 
     subjective_eval_cfg = dict(
