2a497f91dc990e03578cf3c0205088fb28b6dad1
notoschord <wangzekai@kanzhun.com>
2024-01-10 21:59:15 +0800

    [Feature] Add support for Nanbeige API

---
+++ configs/api_examples/eval_api_nanbeige.py
@@ -0,0 +1,36 @@
+from mmengine.config import read_base
+from opencompass.models import Nanbeige
+from opencompass.partitioners import NaivePartitioner
+from opencompass.runners.local_api import LocalAPIRunner
+from opencompass.tasks import OpenICLInferTask
+
+
+with read_base():
+    from ..summarizers.medium import summarizer
+    from ..datasets.ceval.ceval_gen import ceval_datasets
+
+datasets = [
+    *ceval_datasets,
+]
+
+models = [
+    dict(
+        abbr='nanbeige-plus',
+        type=Nanbeige,
+        path='nanbeige-plus',
+        key="xxxxxx",
+        query_per_second=1,
+        max_out_len=2048,
+        batch_size=8),
+]
+
+infer = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalAPIRunner,
+        max_num_workers=2,
+        concurrent_users=2,
+        task=dict(type=OpenICLInferTask)),
+)
+
+work_dir ="./output/nanbeige-plus"
