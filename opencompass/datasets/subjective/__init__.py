0c367d8650c617fb0ecf0491a3c12faa2eaef560
bittersweet1999 <1487910649@qq.com>
2024-01-16 17:37:43 +0800

    reorganize subject files

---
+++ opencompass/datasets/subjective/__init__.py
@@ -0,0 +1,6 @@
+from .alignbench import AlignmentBenchDataset  # noqa: F401, F403
+from .corev2 import Corev2Dataset  # noqa: F401, F403
+from .creationbench import CreationBenchDataset  # noqa: F401, F403
+from .information_retrival import IRDataset  # noqa: F401, F403
+from .multiround import MultiroundDataset  # noqa: F401, F403
+from .subjective_cmp import SubjectiveCmpDataset  # noqa: F401, F403
