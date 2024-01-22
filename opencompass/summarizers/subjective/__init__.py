f890c0c44e6cba51a6c1e288bb078ed550f229d2
bittersweet1999 <1487910649@qq.com>
2024-01-22 14:44:32 +0800

    add mtbench

--- opencompass/summarizers/subjective/__init__.py
+++ opencompass/summarizers/subjective/__init__.py
@@ -3,4 +3,5 @@ from .alignmentbench import AlignmentBenchSummarizer
 from .corev2 import Corev2Summarizer
 from .creationbench import CreationBenchSummarizer
 from .information_retrival import IRSummarizer
+from .mtbench import MTBenchSummarizer
 from .multiround import MultiroundSummarizer
