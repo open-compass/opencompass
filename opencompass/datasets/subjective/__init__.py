7a8b0550dfe312d60bc27b376ced042e6736eaea
bittersweet1999 <148421775+bittersweet1999@users.noreply.github.com>
2024-01-23 14:37:51 +0800

    Update opencompass/datasets/subjective/__init__.py
    Co-authored-by: Songyang Zhang <tonysy@users.noreply.github.com>

--- opencompass/datasets/subjective/__init__.py
+++ opencompass/datasets/subjective/__init__.py
@@ -2,6 +2,6 @@ from .alignbench import AlignmentBenchDataset  # noqa: F401, F403
 from .corev2 import Corev2Dataset  # noqa: F401, F403
 from .creationbench import CreationBenchDataset  # noqa: F401, F403
 from .information_retrival import IRDataset  # noqa: F401, F403
-from .mtbench import MTbenchDataset  # noqa: F401, F403
+from .mtbench import MTBenchDataset  # noqa: F401, F403
 from .multiround import MultiroundDataset  # noqa: F401, F403
 from .subjective_cmp import SubjectiveCmpDataset  # noqa: F401, F403
