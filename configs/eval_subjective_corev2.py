77be07dbb5bb1721ca46029fa63bc264f3d54f63
bittersweet1999 <148421775+bittersweet1999@users.noreply.github.com>
2024-01-24 18:15:29 +0800

    [Fix] fix corev2 (#838)
    * fix corev2
    
    * fix corev2

--- configs/eval_subjective_corev2.py
+++ configs/eval_subjective_corev2.py
@@ -4,21 +4,22 @@ with read_base():
     from .models.qwen.hf_qwen_14b_chat import models as hf_qwen_14b_chat
     from .models.chatglm.hf_chatglm3_6b import models as hf_chatglm3_6b
     from .models.baichuan.hf_baichuan2_7b_chat import models as hf_baichuan2_7b
-    from .models.hf_internlm.hf_internlm_chat_7b import models as hf_internlm_chat_7b
-    from .models.hf_internlm.hf_internlm_chat_20b import models as hf_internlm_chat_20b
-    from .datasets.subjective_cmp.subjective_creation import subjective_datasets
+    from .models.hf_internlm.hf_internlm2_chat_7b import models as internlm2_7b
+    from .models.hf_internlm.hf_internlm2_chat_20b import models as internlm2_20b
+    from .datasets.subjective.subjective_cmp.subjective_corev2 import subjective_datasets
 
 datasets = [*subjective_datasets]
 
 from opencompass.models import HuggingFaceCausalLM, HuggingFace, OpenAI
-from opencompass.partitioners import NaivePartitioner
+from opencompass.partitioners import NaivePartitioner, SizePartitioner
 from opencompass.partitioners.sub_naive import SubjectiveNaivePartitioner
+from opencompass.partitioners.sub_size import SubjectiveSizePartitioner
 from opencompass.runners import LocalRunner
 from opencompass.runners import SlurmSequentialRunner
 from opencompass.tasks import OpenICLInferTask
 from opencompass.tasks.subjective_eval import SubjectiveEvalTask
-from opencompass.summarizers import Creationv01Summarizer
-models = [*hf_baichuan2_7b, *hf_chatglm3_6b, *hf_internlm_chat_20b, *hf_qwen_7b_chat, *hf_internlm_chat_7b, *hf_qwen_14b_chat]
+from opencompass.summarizers import Corev2Summarizer
+models = [*internlm2_7b, *internlm2_20b]
 
 api_meta_template = dict(
     round=[
@@ -31,10 +32,10 @@ api_meta_template = dict(
 )
 
 infer = dict(
-    partitioner=dict(type=NaivePartitioner),
+    partitioner=dict(type=SizePartitioner, max_task_size=500),
     runner=dict(
         type=SlurmSequentialRunner,
-        partition='llmeval',
+        partition='llm_dev2',
         quotatype='auto',
         max_num_workers=256,
         task=dict(type=OpenICLInferTask)),
@@ -49,7 +50,7 @@ _meta_template = dict(
 )
 
 
-judge_model = dict(
+judge_model =    dict(
         type=HuggingFaceCausalLM,
         abbr='qwen-7b-chat-hf',
         path="Qwen/Qwen-7B-Chat",
@@ -71,26 +72,27 @@ judge_model = dict(
         run_cfg=dict(num_gpus=1, num_procs=1),
     )
 
-
 eval = dict(
     partitioner=dict(
-        type=SubjectiveNaivePartitioner,
-        mode='singlescore',
-        models = [*hf_baichuan2_7b]
+        type=SubjectiveSizePartitioner,
+        mode='m2n',
+        max_task_size=500,
+        base_models = [*internlm2_7b],
+        compare_models = [*internlm2_20b]
     ),
     runner=dict(
         type=SlurmSequentialRunner,
-        partition='llmeval',
+        partition='llm_dev2',
         quotatype='auto',
         max_num_workers=256,
         task=dict(
             type=SubjectiveEvalTask,
-            judge_cfg=judge_model
+        judge_cfg=judge_model
         )),
 )
-work_dir = './creation/'
+work_dir = 'outputs/corev2/'
 
 summarizer = dict(
-    type=Creationv01Summarizer,
+    type=Corev2Summarizer,
     match_method='smart',
 )
