11652f7b0110d6be6819f4655021d61a6f4a17f9
Leymore <zfz-960727@163.com>
2024-02-19 13:55:19 +0800

    qwen2-beta -> qwen1.5

--- configs/models/qwen/hf_qwen1_5_0_5b.py
+++ configs/models/qwen/hf_qwen1_5_0_5b.py
@@ -1,18 +1,11 @@
 from opencompass.models import HuggingFaceCausalLM
 
-_meta_template = dict(
-    round=[
-        dict(role="HUMAN", begin='<|im_start|>user\n', end='<|im_end|>\n'),
-        dict(role="BOT", begin="<|im_start|>assistant\n", end='<|im_end|>\n', generate=True),
-    ],
-    eos_token_id=151645,
-)
-
 models = [
     dict(
         type=HuggingFaceCausalLM,
-        abbr='qwen2-beta-0.5b-chat-hf',
-        path="Qwen/Qwen2-beta-0_5B-Chat",
+        abbr='qwen1.5-0.5b-hf',
+        path="Qwen/Qwen1.5-0.5B",
+        tokenizer_path='Qwen/Qwen1.5-0.5B',
         model_kwargs=dict(
             device_map='auto',
             trust_remote_code=True
@@ -23,12 +16,10 @@ models = [
             trust_remote_code=True,
             use_fast=False,
         ),
-        meta_template=_meta_template,
         pad_token_id=151645,
         max_out_len=100,
         max_seq_len=2048,
         batch_size=8,
         run_cfg=dict(num_gpus=1, num_procs=1),
-        end_str='<|im_end|>',
     )
 ]
--- configs/models/qwen/vllm_qwen1_5_72b.py
+++ configs/models/qwen/vllm_qwen1_5_72b.py
@@ -4,8 +4,8 @@ from opencompass.models import VLLM
 models = [
     dict(
         type=VLLM,
-        abbr='qwen2-beta-72b-vllm',
-        path="Qwen/Qwen2-beta-72B",
+        abbr='qwen1.5-72b-vllm',
+        path="Qwen/Qwen1.5-72B",
         model_kwargs=dict(tensor_parallel_size=4),
         max_out_len=100,
         max_seq_len=2048,
