e813ea0d8ea589865767e45f0747959bf899ab5a
Xu Song <xusong.vip@gmail.com>
2024-02-20 15:47:47 +0800

    [Fix] Fix moss template config

--- configs/models/moss/hf_moss_moon_003_sft.py
+++ configs/models/moss/hf_moss_moon_003_sft.py
@@ -1,5 +1,11 @@
 from opencompass.models import HuggingFaceCausalLM
 
+_meta_template = dict(
+    round=[
+        dict(role='HUMAN', begin='<|Human|>: ', end='<eoh>\n'),
+        dict(role='BOT', begin='<|MOSS|>: ', end='<eom>\n', generate=True),
+    ],
+)
 
 models = [
     dict(
@@ -12,6 +18,7 @@ models = [
             truncation_side='left',
             trust_remote_code=True,
         ),
+        meta_template=_meta_template,
         max_out_len=100,
         max_seq_len=2048,
         batch_size=8,
