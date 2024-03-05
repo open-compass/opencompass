66d3aa4c01212f59649e0496fcacfc55d0dbe08c
Jingming <109033042+jingmingzhuo@users.noreply.github.com>
2024-03-05 11:38:28 +0800

    [Feature] Add configs of deepseek-coder (#943)

---
+++ configs/models/deepseek/hf_ deepseek_coder_33b_instruct.py
@@ -0,0 +1,34 @@
+from opencompass.models import HuggingFaceCausalLM
+
+_meta_template = dict(
+    round=[
+        dict(role="HUMAN", begin='### Instruction:\n', end='\n'),
+        dict(role="BOT", begin="### Response:\n", end='<|EOT|>', generate=True),
+    ],
+    eos_token_id=100001,
+)
+
+models = [
+    dict(
+        type=HuggingFaceCausalLM,
+        abbr='deepseek-coder-33b-hf',
+        path="deepseek-ai/deepseek-coder-33b-instruct",
+        tokenizer_path='deepseek-ai/deepseek-coder-33b-instruct',
+        model_kwargs=dict(
+            device_map='auto',
+            trust_remote_code=True,
+        ),
+        tokenizer_kwargs=dict(
+            padding_side='left',
+            truncation_side='left',
+            trust_remote_code=True,
+            use_fast=False,
+        ),
+        meta_template=_meta_template,
+        max_out_len=2048,
+        max_seq_len=2048,
+        batch_size=8,
+        run_cfg=dict(num_gpus=4, num_procs=1),
+        end_str='<|EOT|>',
+    )
+]
