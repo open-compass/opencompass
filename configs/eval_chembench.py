7a00fe1bf1216867cccebf43af703b457e672774
Leymore <zfz-960727@163.com>
2024-04-12 01:07:52 +0800

    update results

--- configs/eval_chembench.py
+++ configs/eval_chembench.py
@@ -6,3 +6,17 @@ with read_base():
 
 datasets = [*chembench_datasets]
 models = [*models]
+
+'''
+dataset                          version    metric    mode      mistral-7b-instruct-v0.2-hf
+-------------------------------  ---------  --------  ------  -----------------------------
+molbench_Name_Conversion         d4e6a1     accuracy  gen                             45.43
+molbench_Property_Prediction     d4e6a1     accuracy  gen                             47.11
+molbench_Mol2caption             d4e6a1     accuracy  gen                             64.21
+molbench_Caption2mol             d4e6a1     accuracy  gen                             35.38
+molbench_Product_Prediction      d4e6a1     accuracy  gen                             38.67
+molbench_Retrosynthesis          d4e6a1     accuracy  gen                             27
+molbench_Yield_Prediction        d4e6a1     accuracy  gen                             27
+molbench_Temperature_Prediction  d4e6a1     accuracy  gen                             26.73
+molbench_Solvent_Prediction      d4e6a1     accuracy  gen                             32.67
+'''
