647553520c4048fcb17aa34267e96791caf0d2a5
binary-husky <96192199+binary-husky@users.noreply.github.com>
2024-04-19 17:02:23 +0800

    fix: python path bug

--- opencompass/tasks/openicl_infer.py
+++ opencompass/tasks/openicl_infer.py
@@ -54,7 +54,8 @@ class OpenICLInferTask(BaseTask):
                        f'--nproc_per_node {self.num_procs} '
                        f'{script_path} {cfg_path}')
         else:
-            python = 'python3' if which('python3') else 'python'
+            import sys
+            python = sys.executable
             command = f'{python} {script_path} {cfg_path}'
 
         return template.format(task_cmd=command)
