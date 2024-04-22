68b7c5f777e448a80b1333f04ad18173be7f7a67
Leymore <zfz-960727@163.com>
2024-04-22 06:26:45 +0000

    add TheoremQA

--- configs/eval_theoremqa.py
+++ configs/eval_theoremqa.py
@@ -4,6 +4,7 @@ with read_base():
     from .models.mistral.hf_mistral_7b_v0_1 import models as hf_mistral_7b_v0_1_model
     from .models.mistral.hf_mistral_7b_v0_2 import models as hf_mistral_7b_v0_2_model
     from .models.hf_internlm.hf_internlm2_20b import models as hf_internlm2_20b_model
+    from .models.hf_internlm.hf_internlm2_math_20b import models as hf_internlm2_math_20b_model
 
     from .datasets.TheoremQA.TheoremQA_5shot_gen_6f0af8 import TheoremQA_datasets as datasets
 
@@ -12,6 +13,6 @@ models = sum([v for k, v in locals().items() if k.endswith("_model")], [])
 work_dir = 'outputs/debug/theorem-5shot'
 
 
-# dataset    version    metric    mode      mistral-7b-v0.1-hf    mistral-7b-v0.2-hf    internlm2-20b-hf
-# ---------  ---------  --------  ------  --------------------  --------------------  ------------------
-# TheoremQA  6f0af8     score     gen                    18.00                 16.75               25.87
+# dataset    version    metric    mode      mistral-7b-v0.1-hf    mistral-7b-v0.2-hf    internlm2-20b-hf    internlm2-math-20b-hf
+# ---------  ---------  --------  ------  --------------------  --------------------  ------------------  -----------------------
+# TheoremQA  6f0af8     score     gen                    18.00                 16.75               25.87                    30.88
---
+++ configs/models/hf_internlm/hf_internlm2_math_20b.py
@@ -0,0 +1,13 @@
+from opencompass.models import HuggingFaceAboveV433Base
+
+
+models = [
+    dict(
+        type=HuggingFaceAboveV433Base,
+        abbr='internlm2-math-20b-hf',
+        path="internlm/internlm2-math-20b",
+        max_out_len=1024,
+        batch_size=8,
+        run_cfg=dict(num_gpus=2),
+    )
+]
