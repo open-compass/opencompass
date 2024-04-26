0c2e5a0254292c3d02c53a92a07c81f0d2ac3fa1
Leymore <zfz-960727@163.com>
2024-04-26 11:01:07 +0000

    update drop template presentation

--- configs/datasets/drop/drop_gen.py
+++ configs/datasets/drop/drop_gen.py
@@ -1,5 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    # from .drop_gen_8a9ed9 import drop_datasets  # noqa: F401, F403
-    from .drop_gen_a779c0 import drop_datasets
+    from .drop_gen_822d33 import drop_datasets
