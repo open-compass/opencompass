bf15f12d9532be61a73bdb78682c0f2b08deb031
JuhaoLiang1997 <13702256306@163.com>
2024-04-27 19:58:36 +0800

    update readme and fix lint issue

--- configs/datasets/MMLUArabic/MMLUArabic_ppl.py
+++ configs/datasets/MMLUArabic/MMLUArabic_ppl.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .MMLUArabic_ppl_ac766d import MMLUArabic_datasets  # noqa: F401, F403
+    from .MMLUArabic_ppl_d2333a import MMLUArabic_datasets  # noqa: F401, F403
