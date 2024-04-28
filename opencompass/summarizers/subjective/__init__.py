58a57a4c45d881be27ecaf7fb1a673fbf905e72f
Yggdrasill7D6 <59858695+Yggdrasill7D6@users.noreply.github.com>
2024-04-28 18:56:24 +0800

    [Feature] add support for Flames datasets (#1093)
    * add flames datasets
    
    * fix lint
    
    * rm quota
    
    * add judgemodel info and fix os path
    
    * support flames dataset
    
    * support flames dataset
    
    ---------
    
    Co-authored-by: bittersweet1999 <1487910649@qq.com>

--- opencompass/summarizers/subjective/__init__.py
+++ opencompass/summarizers/subjective/__init__.py
@@ -6,6 +6,7 @@ from .arenahard import ArenaHardSummarizer
 from .compass_arena import CompassArenaSummarizer
 from .corev2 import Corev2Summarizer
 from .creationbench import CreationBenchSummarizer
+from .flames import FlamesSummarizer
 from .information_retrival import IRSummarizer
 from .mtbench import MTBenchSummarizer
 from .multiround import MultiroundSummarizer
