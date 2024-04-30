35c94d0cdeba5e5328e637ddd6d8ec81682ae3b3
Alexander Lam <alexandercy.lam@gmail.com>
2024-04-30 10:51:01 +0800

    [Feature] Adding support for LLM Compression Evaluation (#1108)
    * fixed formatting based on pre-commit tests
    
    * fixed typo in comments; reduced the number of models in the eval config
    
    * fixed a bug in LLMCompressionDataset, where setting samples=None would result in passing test[:None] to load_dataset
    
    * removed unnecessary variable in _format_table_pivot; changed lark_reporter message to English

--- opencompass/summarizers/__init__.py
+++ opencompass/summarizers/__init__.py
@@ -1,4 +1,5 @@
 # flake8: noqa: F401, E501
 from .circular import CircularSummarizer  # noqa: F401
 from .default import DefaultSummarizer  # noqa: F401
+from .llm_compression import LLMCompressionSummarizer
 from .subjective import *  # noqa: F401
