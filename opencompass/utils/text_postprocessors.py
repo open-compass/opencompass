c26a6530424d07ec84dffce06b72d57035e53292
Leymore <zfz-960727@163.com>
2024-05-06 03:59:31 +0000

    return empty str on failure

--- opencompass/utils/text_postprocessors.py
+++ opencompass/utils/text_postprocessors.py
@@ -186,5 +186,5 @@ def general_eval_wrapper_postprocess(text: str,
 
 def match_answer_pattern(response_text: str, answer_pattern: str):
     match = re.search(answer_pattern, response_text)
-    extracted_answer = match.group(1) if match else None
+    extracted_answer = match.group(1) if match else ''
     return extracted_answer
