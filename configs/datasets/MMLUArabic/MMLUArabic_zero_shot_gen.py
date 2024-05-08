d2c40e564855c3e2a24bfdef04f8b3c9304c5e7b
JuhaoLiang <13702256306@163.com>
2024-05-08 15:00:26 +0800

    [Feature] Add AceGPT-MMLUArabic benchmark (#1099)
    * add AceGPT-MMLUArabic benchmark
    
    * update readme and fix lint issue
    
    * remove unused package
    
    * add MMLUArabic zero-shot settings
    
    * rename filename and update readme

---
+++ configs/datasets/MMLUArabic/MMLUArabic_zero_shot_gen.py
@@ -0,0 +1,4 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .MMLUArabic_zero_shot_gen_3523e0 import MMLUArabic_datasets  # noqa: F401, F403
--- opencompass/datasets/__init__.py
+++ opencompass/datasets/__init__.py
@@ -73,6 +73,7 @@ from .mbpp import *  # noqa: F401, F403
 from .medbench import *  # noqa: F401, F403
 from .mgsm import *  # noqa: F401, F403
 from .mmlu import *  # noqa: F401, F403
+from .MMLUArabic import * # noqa: F401, F403
 from .multirc import *  # noqa: F401, F403
 from .narrativeqa import *  # noqa: F401, F403
 from .natural_question import *  # noqa: F401, F403
