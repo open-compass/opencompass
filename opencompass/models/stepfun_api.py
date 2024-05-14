62dbf047086fe9c9ee950a06168f88283ebadcee
Fengzhe Zhou <zfz-960727@163.com>
2024-05-14 22:42:23 +0800

    [Sync] update github workflow (#1156)

--- opencompass/models/stepfun_api.py
+++ opencompass/models/stepfun_api.py
@@ -1,3 +1,4 @@
+import time
 from concurrent.futures import ThreadPoolExecutor
 from typing import Dict, List, Optional, Union
 
@@ -10,17 +11,17 @@ from .base_api import BaseAPIModel
 PromptType = Union[PromptList, str]
 
 
-class AI360GPT(BaseAPIModel):
-    """Model wrapper around 360 GPT.
+class StepFun(BaseAPIModel):
+    """Model wrapper around StepFun.
 
-    Documentations: https://ai.360.com/platform/docs/overview
+    Documentation:
 
     Args:
-        path (str): Model name
-        key (str): Provide API Key
-        url (str): Provided URL
+        path (str): The name of StepFun model.
+            e.g. `moonshot-v1-32k`
+        key (str): Authorization key.
         query_per_second (int): The maximum queries allowed per second
-            between two consecutive calls of the API. Defaults to 2.
+            between two consecutive calls of the API. Defaults to 1.
         max_seq_len (int): Unused here.
         meta_template (Dict, optional): The model's meta prompt
             template if needed, in case the requirement of injecting or
@@ -30,32 +31,27 @@ class AI360GPT(BaseAPIModel):
 
     def __init__(
         self,
-        path: str,  # model name, e.g.: 360GPT_S2_V9
+        path: str,
         key: str,
-        url: str = 'https://api.360.cn/v1/chat/completions',
+        url: str,
         query_per_second: int = 2,
         max_seq_len: int = 2048,
         meta_template: Optional[Dict] = None,
         retry: int = 2,
-        generation_kwargs: Dict = {
-            'temperature': 0.9,
-            'max_tokens': 2048,
-            'top_p': 0.5,
-            'tok_k': 0,
-            'repetition_penalty': 1.05,
-        }):  # noqa E125
+        system_prompt: str = '',
+    ):
         super().__init__(path=path,
                          max_seq_len=max_seq_len,
                          query_per_second=query_per_second,
                          meta_template=meta_template,
-                         retry=retry,
-                         generation_kwargs=generation_kwargs)
+                         retry=retry)
         self.headers = {
-            'Authorization': f'Bearer {key}',
             'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + key,
         }
-        self.model = path
         self.url = url
+        self.model = path
+        self.system_prompt = system_prompt
 
     def generate(
         self,
@@ -102,34 +98,45 @@ class AI360GPT(BaseAPIModel):
             messages = [{'role': 'user', 'content': input}]
         else:
             messages = []
+            msg_buffer, last_role = [], None
             for item in input:
-                msg = {'content': item['prompt']}
-                if item['role'] == 'HUMAN':
-                    msg['role'] = 'user'
-                elif item['role'] == 'BOT':
-                    msg['role'] = 'assistant'
-                elif item['role'] == 'SYSTEM':
-                    msg['role'] = 'system'
-                messages.append(msg)
+                item['role'] = 'assistant' if item['role'] == 'BOT' else 'user'
+                if item['role'] != last_role and last_role is not None:
+                    messages.append({
+                        'content': '\n'.join(msg_buffer),
+                        'role': last_role
+                    })
+                    msg_buffer = []
+                msg_buffer.append(item['prompt'])
+                last_role = item['role']
+            messages.append({
+                'content': '\n'.join(msg_buffer),
+                'role': last_role
+            })
 
-        data = {
-            'model': self.model,
-            'messages': messages,
-            'stream': False,
-            # "user": "OpenCompass"
-        }
+        if self.system_prompt:
+            system = {'role': 'system', 'content': self.system_prompt}
+            messages.insert(0, system)
 
-        data.update(self.generation_kwargs)
+        data = {'model': self.model, 'messages': messages}
 
         max_num_retries = 0
         while max_num_retries < self.retry:
             self.acquire()
-            # payload = json.dumps(data)
-            raw_response = requests.request('POST',
-                                            url=self.url,
-                                            headers=self.headers,
-                                            json=data)
-            response = raw_response.json()
+            try:
+                raw_response = requests.request('POST',
+                                                url=self.url,
+                                                headers=self.headers,
+                                                json=data)
+            except Exception as err:
+                print('Request Error:{}'.format(err))
+                time.sleep(2)
+                continue
+
+            try:
+                response = raw_response.json()
+            except Exception:
+                response = None
             self.release()
 
             if response is None:
@@ -139,34 +146,37 @@ class AI360GPT(BaseAPIModel):
                 # to slow down the request
                 self.wait()
                 continue
+
             if raw_response.status_code == 200:
-                msg = response['choices'][0]['message']['content'].strip()
+                # msg = json.load(response.text)
+                # response
+                msg = response['choices'][0]['message']['content']
                 self.logger.debug(f'Generated: {msg}')
                 return msg
 
-            # sensitive content, prompt overlength, network error
-            # or illegal prompt
-            if raw_response.status_code in [400, 401, 402, 429, 500]:
-                if 'error' not in response:
-                    print(raw_response.status_code)
-                    print(raw_response.text)
-                    continue
-                print(response)
-                # tpm(token per minitue) limit
-                if response['error']['code'] == '1005':
-                    self.logger.debug('tpm limit, ignoring')
-                    continue
-                elif response['error']['code'] == '1001':
-                    msg = '参数错误:messages参数过长或max_tokens参数值过大'
-                    self.logger.debug(f'Generated: {msg}')
-                    return msg
-                else:
-                    print(response)
+            if raw_response.status_code == 400:
+                print(messages, response)
+                print('请求失败，状态码:', raw_response)
+                msg = 'The context length exceeded'
+                return msg
+            elif raw_response.status_code == 403:
+                print('请求被拒绝 api_key错误')
+                continue
+            elif raw_response.status_code == 429:
+                print(messages, response)
+                print('请求失败，状态码:', raw_response)
+                time.sleep(5)
+                continue
+            elif raw_response.status_code == 451:
+                print(messages, response)
+                print('请求失败，状态码:', raw_response)
+                msg = 'The request was rejected because high risk'
+                return msg
+            else:
+                print(messages, response)
+                print('请求失败，状态码:', raw_response)
+                time.sleep(1)
 
-                self.logger.error('Find error message in response: ',
-                                  str(response['error']))
-
-            print(raw_response)
             max_num_retries += 1
 
-        raise RuntimeError(raw_response.text)
+        raise RuntimeError(raw_response)
