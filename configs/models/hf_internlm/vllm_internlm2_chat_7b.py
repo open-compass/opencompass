55961a047448eafd9fda14abd366ddd3d782e90e
Leymore <zfz-960727@163.com>
2024-05-15 17:36:50 +0800

    rm vllm_internlm2_chat_7b

--- configs/models/hf_internlm/vllm_internlm2_chat_7b.py
+++ configs/models/hf_internlm/vllm_internlm2_chat_7b.py
@@ -1,29 +1,13 @@
-from opencompass.models.vllm import VLLM
-
-
-_meta_template = dict(
-    begin='<s>',
-    round=[
-        dict(role='HUMAN', begin='<|im_start|>user\n', end='<|im_end|>\n'),
-        dict(role='BOT', begin='<|im_start|>assistant\n', end='<|im_end|>\n', generate=True),
-    ],
-)
+from opencompass.models import VLLMwithChatTemplate
 
 models = [
     dict(
-        type=VLLM,
+        type=VLLMwithChatTemplate,
         abbr='internlm2-chat-7b-vllm',
         path='internlm/internlm2-chat-7b',
         model_kwargs=dict(tensor_parallel_size=1),
-        meta_template=_meta_template,
-        generation_kwargs=dict(
-            top_k=1,
-            temperature=0,
-            stop_token_ids=[2, 92542],
-        ),
-        max_out_len=2000,
-        max_seq_len=32768,
+        max_out_len=1024,
         batch_size=32768,
-        run_cfg=dict(num_gpus=1, num_procs=1),
+        run_cfg=dict(num_gpus=1),
     )
 ]
