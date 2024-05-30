0bbd22ad65222afe9caef07cfb044ac9ea3c32e1
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-05-30 16:19:14 +0800

    update

--- configs/eval_openai_format_task.py
+++ configs/eval_openai_format_task.py
@@ -3,7 +3,6 @@ from mmengine.config import read_base
 from opencompass.models import OpenAI
 from opencompass.partitioners import NaivePartitioner
 from opencompass.runners import LocalRunner
-from opencompass.summarizers import DefaultSummarizer
 from opencompass.tasks import OpenICLInferTask
 
 
@@ -51,25 +50,55 @@ infer = dict(
 )
 
 
-summarizer = dict(
-    dataset_abbrs=[
-        ['ceval-test', 'naive_average'],
+ceval_summary_groups = []
 
-        # ['mmlu', 'naive_average'],
-        # ['cmmlu', 'naive_average'],
-        # ['GaokaoBench', 'weighted_average'],
-        # ['triviaqa_wiki_1shot', 'score'],
-        # ['nq_open_1shot', 'score'],
-        # ['race-high', 'accuracy'],
-        # ['winogrande', 'accuracy'],
-        # ['hellaswag', 'accuracy'],
-        # ['bbh', 'naive_average'],
-        # ['gsm8k', 'accuracy'],
-        # ['math', 'accuracy'],
-        # ['TheoremQA', 'accuracy'],
-        # ['openai_humaneval', 'humaneval_pass@1'],
-        # ['sanitized_mbpp', 'score'],
-    ],
-    summary_groups=sum(
-        [v for k, v in locals().items() if k.endswith('_summary_groups')], []),
+_ceval_stem = ['computer_network', 'operating_system', 'computer_architecture', 'college_programming', 'college_physics', 'college_chemistry', 'advanced_mathematics', 'probability_and_statistics', 'discrete_mathematics', 'electrical_engineer', 'metrology_engineer', 'high_school_mathematics', 'high_school_physics', 'high_school_chemistry', 'high_school_biology', 'middle_school_mathematics', 'middle_school_biology', 'middle_school_physics', 'middle_school_chemistry', 'veterinary_medicine']
+_ceval_stem = ['ceval-' + s for s in _ceval_stem]
+ceval_summary_groups.append({'name': 'ceval-stem', 'subsets': _ceval_stem})
+
+_ceval_social_science = ['college_economics', 'business_administration', 'marxism', 'mao_zedong_thought', 'education_science', 'teacher_qualification', 'high_school_politics', 'high_school_geography', 'middle_school_politics', 'middle_school_geography']
+_ceval_social_science = ['ceval-' + s for s in _ceval_social_science]
+ceval_summary_groups.append({'name': 'ceval-social-science', 'subsets': _ceval_social_science})
+
+_ceval_humanities = ['modern_chinese_history', 'ideological_and_moral_cultivation', 'logic', 'law', 'chinese_language_and_literature', 'art_studies', 'professional_tour_guide', 'legal_professional', 'high_school_chinese', 'high_school_history', 'middle_school_history']
+_ceval_humanities = ['ceval-' + s for s in _ceval_humanities]
+ceval_summary_groups.append({'name': 'ceval-humanities', 'subsets': _ceval_humanities})
+
+_ceval_other = ['civil_servant', 'sports_science', 'plant_protection', 'basic_medicine', 'clinical_medicine', 'urban_and_rural_planner', 'accountant', 'fire_engineer', 'environmental_impact_assessment_engineer', 'tax_accountant', 'physician']
+_ceval_other = ['ceval-' + s for s in _ceval_other]
+ceval_summary_groups.append({'name': 'ceval-other', 'subsets': _ceval_other})
+
+_ceval_hard = ['advanced_mathematics', 'discrete_mathematics', 'probability_and_statistics', 'college_chemistry', 'college_physics', 'high_school_mathematics', 'high_school_chemistry', 'high_school_physics']
+_ceval_hard = ['ceval-' + s for s in _ceval_hard]
+ceval_summary_groups.append({'name': 'ceval-hard', 'subsets': _ceval_hard})
+
+_ceval_all = _ceval_stem + _ceval_social_science + _ceval_humanities + _ceval_other
+ceval_summary_groups.append({'name': 'ceval', 'subsets': _ceval_all})
+
+_ceval_stem = ['computer_network', 'operating_system', 'computer_architecture', 'college_programming', 'college_physics', 'college_chemistry', 'advanced_mathematics', 'probability_and_statistics', 'discrete_mathematics', 'electrical_engineer', 'metrology_engineer', 'high_school_mathematics', 'high_school_physics', 'high_school_chemistry', 'high_school_biology', 'middle_school_mathematics', 'middle_school_biology', 'middle_school_physics', 'middle_school_chemistry', 'veterinary_medicine']
+_ceval_stem = ['ceval-test-' + s for s in _ceval_stem]
+ceval_summary_groups.append({'name': 'ceval-test-stem', 'subsets': _ceval_stem})
+
+_ceval_social_science = ['college_economics', 'business_administration', 'marxism', 'mao_zedong_thought', 'education_science', 'teacher_qualification', 'high_school_politics', 'high_school_geography', 'middle_school_politics', 'middle_school_geography']
+_ceval_social_science = ['ceval-test-' + s for s in _ceval_social_science]
+ceval_summary_groups.append({'name': 'ceval-test-social-science', 'subsets': _ceval_social_science})
+
+_ceval_humanities = ['modern_chinese_history', 'ideological_and_moral_cultivation', 'logic', 'law', 'chinese_language_and_literature', 'art_studies', 'professional_tour_guide', 'legal_professional', 'high_school_chinese', 'high_school_history', 'middle_school_history']
+_ceval_humanities = ['ceval-test-' + s for s in _ceval_humanities]
+ceval_summary_groups.append({'name': 'ceval-test-humanities', 'subsets': _ceval_humanities})
+
+_ceval_other = ['civil_servant', 'sports_science', 'plant_protection', 'basic_medicine', 'clinical_medicine', 'urban_and_rural_planner', 'accountant', 'fire_engineer', 'environmental_impact_assessment_engineer', 'tax_accountant', 'physician']
+_ceval_other = ['ceval-test-' + s for s in _ceval_other]
+ceval_summary_groups.append({'name': 'ceval-test-other', 'subsets': _ceval_other})
+
+_ceval_hard = ['advanced_mathematics', 'discrete_mathematics', 'probability_and_statistics', 'college_chemistry', 'college_physics', 'high_school_mathematics', 'high_school_chemistry', 'high_school_physics']
+_ceval_hard = ['ceval-test-' + s for s in _ceval_hard]
+ceval_summary_groups.append({'name': 'ceval-test-hard', 'subsets': _ceval_hard})
+
+_ceval_all = _ceval_stem + _ceval_social_science + _ceval_humanities + _ceval_other
+ceval_summary_groups.append({'name': 'ceval-test', 'subsets': _ceval_all})
+
+
+summarizer = dict(
+    summary_groups=ceval_summary_groups,
 )
