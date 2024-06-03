3d247150ea44cd1e56b7be0c495a0fd25fa7a08d
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-06-03 15:48:53 +0800

    update teval to v2 version

--- configs/datasets/teval/teval_en_gen_1ac254.py
+++ configs/datasets/teval/teval_en_gen_1ac254.py
@@ -5,13 +5,13 @@ from opencompass.openicl.icl_evaluator import TEvalEvaluator
 from opencompass.datasets import teval_postprocess, TEvalDataset
 
 teval_subject_mapping = {
-    'instruct': ['instruct_v1'],
-    'plan': ['plan_json_v1', 'plan_str_v1'],
-    'review': ['review_str_v1'],
-    'reason_retrieve_understand': ['reason_retrieve_understand_json_v1'],
-    'reason': ['reason_str_v1'],
-    'retrieve': ['retrieve_str_v1'],
-    'understand': ['understand_str_v1'],
+    'instruct': ['instruct_v2'],
+    'plan': ['plan_json_v2', 'plan_str_v2'],
+    'review': ['review_str_v2'],
+    'reason_retrieve_understand': ['reason_retrieve_understand_json_v2'],
+    'reason': ['reason_str_v2'],
+    'retrieve': ['retrieve_str_v2'],
+    'understand': ['understand_str_v2'],
 }
 
 teval_reader_cfg = dict(input_columns=['prompt'], output_column='ground_truth')
