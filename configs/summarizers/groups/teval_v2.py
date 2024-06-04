8ef9ac2d1c7a8b40fc3d1fb9ad42296c5b53a1b1
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-06-04 15:53:29 +0800

    update teval_v2

--- configs/summarizers/groups/teval_v2.py
+++ configs/summarizers/groups/teval_v2.py
@@ -14,7 +14,7 @@ _base_summary_groups = [
         'metric': 'args_em_metric',
         'subsets': [
             ['teval-instruct_v2_subset', 'string_args_em_metric'],
-            ['teval-instruct_v2', 'json_args_em_metric'],
+            ['teval-instruct_v2_subset', 'json_args_em_metric'],
         ]
     },
     {
@@ -34,9 +34,9 @@ _base_summary_groups = [
         ]
     },
     {
-        'name': 'copy_teval-review_str_v2',
+        'name': 'copy_teval-review_str_v2_subset',
         'subsets': [
-            ['teval-review_str_v2', 'review_quality'],
+            ['teval-review_str_v2_subset', 'review_quality'],
         ],
     },
     {
@@ -44,16 +44,16 @@ _base_summary_groups = [
         'subsets': [
             ['teval-instruct_v2_subset', 'format_metric'],
             ['teval-instruct_v2_subset', 'args_em_metric'],
-            ['teval-plan_str_v2', 'f1_score'],
-            ['teval-plan_json_v2', 'f1_score'],
-            ['teval-reason_str_v2', 'thought'],
-            ['teval-reason_retrieve_understand_json_v2', 'thought'],
-            ['teval-retrieve_str_v2', 'name'],
-            ['teval-reason_retrieve_understand_json_v2', 'name'],
-            ['teval-understand_str_v2', 'args'],
-            ['teval-reason_retrieve_understand_json_v2', 'args'],
-            ['teval-review_str_v2', 'review_quality'],
-            ['copy_teval-review_str_v2', 'naive_average'],  # a hack for review * 2
+            ['teval-plan_str_v2_subset', 'f1_score'],
+            ['teval-plan_json_v2_subset', 'f1_score'],
+            ['teval-reason_str_v2_subset', 'thought'],
+            ['teval-reason_retrieve_understand_json_v2_subset', 'thought'],
+            ['teval-retrieve_str_v2_subset', 'name'],
+            ['teval-reason_retrieve_understand_json_v2_subset', 'name'],
+            ['teval-understand_str_v2_subset', 'args'],
+            ['teval-reason_retrieve_understand_json_v2_subset', 'args'],
+            ['teval-review_str_v2_subset', 'review_quality'],
+            ['copy_teval-review_str_v2_subset', 'naive_average'],  # a hack for review * 2
         ]
     },
 ]
@@ -68,6 +68,6 @@ for group in _base_summary_groups:
 # base _zh
 for group in _base_summary_groups:
     group = deepcopy(group)
-    group['name'] = group['name'] + '_zh'
-    group['subsets'] = [[subset[0] + '_zh', subset[1]] for subset in group['subsets']]
+    group['name'] = group['name'] + '_subset_zh'
+    group['subsets'] = [[subset[0] + '_subset_zh', subset[1]] for subset in group['subsets']]
     teval_summary_groups.append(group)
