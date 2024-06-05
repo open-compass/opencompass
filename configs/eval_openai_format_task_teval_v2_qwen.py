f11d72745654aaad72402de3d5d6ba1140d82a83
xingjun.wang <xingjun.wxj@alibaba-inc.com>
2024-06-05 17:46:36 +0800

    update

--- configs/eval_openai_format_task_teval_v2_qwen.py
+++ configs/eval_openai_format_task_teval_v2_qwen.py
@@ -7,10 +7,10 @@ from opencompass.tasks import OpenICLInferTask
 
 
 # chat model
-# SWIFT_DEPLOY_API = 'http://127.0.0.1:8000/v1/chat/completions'
+# SWIFT_DEPLOY_URL = 'http://127.0.0.1:8000/v1/chat/completions'
 
 # base model
-SWIFT_DEPLOY_API = 'http://127.0.0.1:8000/v1/completions'
+SWIFT_DEPLOY_URL = 'http://127.0.0.1:8000/v1/completions'
 
 # TODO: BY JASON ONLY FOR TEST!
 with read_base():
@@ -38,14 +38,14 @@ models = [
          type=OpenAI,
          path='qwen-7b',
          # The key will be obtained from $OPENAI_API_KEY, but you can write down your key here as well
-         key='your_openai_api_key',     # No need for swift deployment API
+         key='your_openai_api_key',  # No need for swift deployment API
          meta_template=api_meta_template,
          query_per_second=1,
          max_out_len=2048,
          max_seq_len=2048,
          batch_size=1,
          run_cfg=dict(num_gpus=0),
-         openai_api_base=SWIFT_DEPLOY_API,
+         openai_api_base=SWIFT_DEPLOY_URL,
          ),
 ]
 
