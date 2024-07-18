8b4d763064e5b6059c316d60bdfdd57d5f4b4c63
zhulin1 <zhulin1@pjlab.org.cn>
2024-07-18 18:09:33 +0800

    update

--- .github/scripts/eval_regression_base.py
+++ .github/scripts/eval_regression_base.py
@@ -19,6 +19,8 @@ with read_base():
         models as hf_gemma_7b_model  # noqa: F401, E501
     from ...configs.models.hf_internlm.lmdeploy_internlm2_1_8b import \
         models as lmdeploy_internlm2_1_8b_model  # noqa: F401, E501
+    from ...configs.models.hf_internlm.lmdeploy_internlm2_5_7b import \
+        models as lmdeploy_internlm2_5_7b_model  # noqa: F401, E501
     from ...configs.models.hf_internlm.lmdeploy_internlm2_7b import \
         models as lmdeploy_internlm2_7b_model  # noqa: F401, E501
     from ...configs.models.hf_internlm.lmdeploy_internlm2_base_7b import \
