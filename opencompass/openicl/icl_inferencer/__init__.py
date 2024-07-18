c6ee3a0d78ff16cd9862571ca46d45b77422f75e
quehry <quehry@gmail.com>
2024-07-18 15:25:25 +0800

    revise

--- opencompass/openicl/icl_inferencer/__init__.py
+++ opencompass/openicl/icl_inferencer/__init__.py
@@ -4,6 +4,8 @@ from .icl_base_inferencer import BaseInferencer  # noqa
 from .icl_chat_inferencer import ChatInferencer  # noqa
 from .icl_clp_inferencer import CLPInferencer  # noqa
 from .icl_gen_inferencer import GenInferencer  # noqa
+from .icl_inference_ppl_only_inferencer import \
+    InferencePPLOnlyInferencer  # noqa
 from .icl_ll_inferencer import LLInferencer  # noqa
 from .icl_mink_percent_inferencer import MinKPercentInferencer  # noqa
 from .icl_ppl_inferencer import PPLInferencer  # noqa
@@ -11,4 +13,3 @@ from .icl_ppl_only_inferencer import PPLOnlyInferencer  # noqa
 from .icl_sc_inferencer import SCInferencer  # noqa
 from .icl_sw_ce_loss_inferencer import SWCELossInferencer  # noqa
 from .icl_tot_inferencer import ToTInferencer  # noqa
-from .icl_inference_ppl_only_inferencer import InferencePPLOnlyInferencer  # noqa
