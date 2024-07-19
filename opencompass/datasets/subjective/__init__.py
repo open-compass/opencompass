92a4fbe4f6425417bf609899afeb0ea46eb55df7
bittersweet1999 <1487910649@qq.com>
2024-07-19 14:35:34 +0800

    support checklist eval

--- opencompass/datasets/subjective/__init__.py
+++ opencompass/datasets/subjective/__init__.py
@@ -2,12 +2,15 @@ from .alignbench import AlignmentBenchDataset  # noqa: F401, F403
 from .arena_hard import ArenaHardDataset  # noqa: F401, F403
 from .compass_arena import CompassArenaDataset  # noqa: F401, F403
 from .compassbench import CompassBenchDataset  # noqa: F401, F403
+from .compassbench_checklist import \
+    CompassBenchCheklistDataset  # noqa: F401, F403
 from .compassbench_control_length_bias import \
     CompassBenchControlLengthBiasDataset  # noqa: F401, F403
 from .corev2 import Corev2Dataset  # noqa: F401, F403
 from .creationbench import CreationBenchDataset  # noqa: F401, F403
 from .fofo import FofoDataset  # noqa: F401, F403
 from .information_retrival import IRDataset  # noqa: F401, F403
+from .judgerbench import JudgerBenchDataset  # noqa: F401, F403
 from .mtbench import MTBenchDataset  # noqa: F401, F403
 from .mtbench101 import MTBench101Dataset  # noqa: F401, F403
 from .multiround import MultiroundDataset  # noqa: F401, F403
