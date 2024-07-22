8736aa0e3c49cce8b3566d48130593dea756d21d
MaiziXiao <xxllcc1993@gmail.com>
2024-07-22 19:28:32 +0800

    fix lint

--- configs/datasets/ruler/ruler_fwe.py
+++ configs/datasets/ruler/ruler_fwe.py
@@ -7,26 +7,26 @@ from opencompass.datasets.RULER.ruler_fwe import RulerFweEvaluator
 # FWE Dataset
 fwe_datasets = [
     {
-        "abbr": "ruler_fwe",
-        "type": RulerFweDataset,
-        "tokens_to_generate": 50,
-        "alpha": 2.0,
-        "coded_wordlen": 6,
-        "reader_cfg": dict(input_columns=["prompt"], output_column="answer"),
-        "infer_cfg": dict(
+        'abbr': 'ruler_fwe',
+        'type': RulerFweDataset,
+        'tokens_to_generate': 50,
+        'alpha': 2.0,
+        'coded_wordlen': 6,
+        'reader_cfg': dict(input_columns=['prompt'], output_column='answer'),
+        'infer_cfg': dict(
             prompt_template=dict(
                 type=PromptTemplate,
                 template=dict(
                     round=[
-                        dict(role="HUMAN", prompt="{prompt}"),
-                        dict(role="BOT", prompt="{answer}\n"),
+                        dict(role='HUMAN', prompt='{prompt}'),
+                        dict(role='BOT', prompt='{answer}\n'),
                     ]
                 ),
             ),
             retriever=dict(type=ZeroRetriever),
             inferencer=dict(type=GenInferencer),
         ),
-        "eval_cfg": dict(
+        'eval_cfg': dict(
             evaluator=dict(type=RulerFweEvaluator),
         ),
     }
