8127fc3518d60915cf9759655b10b42270fe648d
Linchen Xiao <xxllcc1993@gmail.com>
2024-07-23 12:29:57 +0800

    CompassBench subjective summarizer added (#1349)
    * subjective summarizer added
    
    * fix lint

--- configs/eval_compassbench_v1_3_subjective.py
+++ configs/eval_compassbench_v1_3_subjective.py
@@ -4,14 +4,16 @@ with read_base():
     from .datasets.subjective.compassbench.compassbench_checklist import (
         checklist_datasets,
     )
-from opencompass.partitioners import NaivePartitioner, SizePartitioner
+from opencompass.partitioners import NaivePartitioner
 from opencompass.partitioners.sub_naive import SubjectiveNaivePartitioner
 from opencompass.runners import LocalRunner
 from opencompass.tasks import OpenICLInferTask
 from opencompass.tasks.subjective_eval import SubjectiveEvalTask
-# from opencompass.summarizers import SubjectiveSummarizer
+
+from opencompass.summarizers.subjective.compassbench_v13 import CompassBenchSummarizer
 from opencompass.models import HuggingFacewithChatTemplate
 from opencompass.models import TurboMindModelwithChatTemplate
+
 api_meta_template = dict(
     round=[
         dict(role='HUMAN', api_role='HUMAN'),
@@ -19,6 +21,7 @@ api_meta_template = dict(
     ]
 )
 models = [
+    # Choose different engines to start the job
     # dict(
     #     type=HuggingFacewithChatTemplate,
     #     abbr="internlm2-chat-1.8b",
@@ -46,9 +49,10 @@ models = [
         batch_size=16,
         run_cfg=dict(num_gpus=1),
     ),
+    # Mock as gpt4o
     dict(
         type=TurboMindModelwithChatTemplate,
-        abbr='judgellm',
+        abbr='gpt4o',
         path='internlm/internlm2-chat-1_8b',
         engine_config=dict(session_len=7168, max_batch_size=16, tp=1),
         gen_config=dict(top_k=1000, temperature=1, top_p=0.9, max_new_tokens=2048),
@@ -56,7 +60,7 @@ models = [
         max_out_len=2048,
         batch_size=16,
         run_cfg=dict(num_gpus=1),
-    )
+    ),
 ]
 # -------------Inference Stage ----------------------------------------
 # For subjective evaluation, we often set do sample for models
@@ -79,6 +83,5 @@ eval = dict(
         type=LocalRunner, max_num_workers=16, task=dict(type=SubjectiveEvalTask)
     ),
 )
-# TODO summarizer to be implemented
-# summarizer = dict(type=SubjectiveSummarizer, function='subjective')
+summarizer = dict(type=CompassBenchSummarizer)
 work_dir = 'outputs/debug_checklist/'
