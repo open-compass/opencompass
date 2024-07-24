b7044c841b1daabd3d9630c58773dfb9c3fbe4ab
heya5 <yanhe.ian@outlook.com>
2024-07-24 13:56:21 +0000

    fix linting issue

--- opencompass/models/openai_api.py
+++ opencompass/models/openai_api.py
@@ -53,7 +53,7 @@ class OpenAI(BaseAPIModel):
         tokenizer_path (str, optional): The path to the tokenizer. Use path if
             'tokenizer_path' is None, otherwise use the 'tokenizer_path'.
             Defaults to None.
-        extra_body (Dict, optional): Add additional JSON properties to 
+        extra_body (Dict, optional): Add additional JSON properties to
             the request
     """
 
@@ -308,8 +308,8 @@ class OpenAI(BaseAPIModel):
             int: Length of the input tokens
         """
         try:
-            enc = self.tiktoken.encoding_for_model(self.path
-                or self.tokenizer_path)
+            enc = self.tiktoken.encoding_for_model(
+                self.path or self.tokenizer_path)
             return len(enc.encode(prompt))
         except Exception:
             from transformers import AutoTokenizer
