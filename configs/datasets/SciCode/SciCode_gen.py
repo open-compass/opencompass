7360414b1096621949e4d708610a5e2a2c55978b
root <HariSeldon0>
2024-07-31 10:55:45 +0800

    add SciCode

---
+++ configs/datasets/SciCode/SciCode_gen.py
@@ -0,0 +1,30 @@
+from opencompass.openicl.icl_prompt_template import PromptTemplate
+from opencompass.openicl.icl_retriever import ZeroRetriever
+from opencompass.openicl.icl_inferencer import ChatInferencer
+from opencompass.datasets import SciCodeDataset, SciCodeEvaluator
+
+
+SciCode_dataset_path = './data/SciCode'
+
+SciCode_reader_cfg = dict(input_columns=['prompt'], output_column=None)
+
+SciCode_infer_cfg = dict(
+    ice_template=dict(
+        type=PromptTemplate,
+        template='',
+        ),
+
+    retriever=dict(type=ZeroRetriever),
+    inferencer=dict(type=ChatInferencer, infer_mode='every', max_out_len=4096))
+
+SciCode_eval_cfg = dict(evaluator=dict(type=SciCodeEvaluator, dataset_path=SciCode_dataset_path))
+
+SciCode_datasets = [
+    dict(
+        abbr='SciCode',
+        type=SciCodeDataset,
+        path=SciCode_dataset_path,
+        reader_cfg=SciCode_reader_cfg,
+        infer_cfg=SciCode_infer_cfg,
+        eval_cfg=SciCode_eval_cfg)
+]
