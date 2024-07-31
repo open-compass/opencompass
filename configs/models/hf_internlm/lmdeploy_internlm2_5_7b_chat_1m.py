b83396f57ca5836ee74b01e8a32ce6f26117cf58
Mo Li <2568818204@qq.com>
2024-07-31 14:53:51 +0800

    add 1m config (#1383)

--- configs/models/hf_internlm/lmdeploy_internlm2_5_7b_chat_1m.py
+++ configs/models/hf_internlm/lmdeploy_internlm2_5_7b_chat_1m.py
@@ -5,7 +5,7 @@ models = [
         type=TurboMindModelwithChatTemplate,
         abbr='internlm2_5-7b-chat-1m-turbomind',
         path='internlm/internlm2_5-7b-chat-1m',
-        engine_config=dict(rope_scaling_factor=2.5, session_len=1048576, max_batch_size=1, tp=4), # 1M context length
+        engine_config=dict(rope_scaling_factor=2.5, session_len=1048576, max_batch_size=1, cache_max_entry_count=0.7, tp=4), # 1M context length
         gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=2048),
         max_seq_len=1048576,
         max_out_len=2048,
