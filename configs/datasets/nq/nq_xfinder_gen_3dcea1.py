b6f7336eb2372e23b7049815d896faac09f42c68
liushz <liuhongwei@pjlab.rog.cn>
2024-09-04 00:05:15 +0800

    Add model postprocess function

--- configs/datasets/nq/nq_xfinder_gen_3dcea1.py
+++ configs/datasets/nq/nq_xfinder_gen_3dcea1.py
@@ -2,6 +2,7 @@ from opencompass.openicl.icl_prompt_template import PromptTemplate
 from opencompass.openicl.icl_retriever import ZeroRetriever
 from opencompass.openicl.icl_inferencer import GenInferencer
 from opencompass.datasets import NaturalQuestionDataset, NQEvaluator
+from opencompass.utils.model_postprocessors import xfinder_postprocess
 
 nq_reader_cfg = dict(
     input_columns=['question'], output_column='answer', train_split='test')
@@ -16,7 +17,9 @@ nq_infer_cfg = dict(
     retriever=dict(type=ZeroRetriever),
     inferencer=dict(type=GenInferencer))
 
-nq_eval_cfg = dict(evaluator=dict(type=NQEvaluator), pred_role='BOT')
+nq_eval_cfg = dict(evaluator=dict(type=NQEvaluator), pred_role='BOT',
+                model_postprocessor=dict(type=xfinder_postprocess, question_type='short_text', xfinder_model_name='xFinder-qwen1505', xfiner_api_url='http://0.0.0.0:23333/v1,http://0.0.0.0:23334/v1'),
+)
 
 nq_datasets = [
     dict(
