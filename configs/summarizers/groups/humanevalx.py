21507c7c062e412f517a897d286346fcce050d36
zhangsongyang <zhangsongyang@pjlab.org.cn>
2024-09-12 11:59:15 +0000

    Update

--- configs/summarizers/groups/humanevalx.py
+++ configs/summarizers/groups/humanevalx.py
@@ -1,5 +1,5 @@
 humanevalx_summary_groups = []
 
 _humanevalx_all = ['python', 'cpp', 'go', 'java', 'js']
-_humanevalx_all = ['humanevalx-' + d for d in _ds1000_all]
+_humanevalx_all = ['humanevalx-' + d for d in _humanevalx_all]
 humanevalx_summary_groups.append({'name': 'humanevalx', 'subsets': _humanevalx_all})
