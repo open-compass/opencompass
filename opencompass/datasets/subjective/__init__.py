f895a7cc6f79c3174b1aafcf9f6e82f7c2d4bede
Linchen Xiao <xxllcc1993@gmail.com>
2024-09-13 10:30:43 +0800

    [Feature] Dataset prompts update for ARC, BoolQ, Race (#1527)
    add judgerbench and reorg sub
    
    add judgerbench and reorg subeval
    
    add judgerbench and reorg subeval

--- opencompass/datasets/subjective/__init__.py
+++ opencompass/datasets/subjective/__init__.py
@@ -1,5 +1,9 @@
 from .alignbench import AlignmentBenchDataset  # noqa: F401, F403
+from .alignbench import alignbench_postprocess  # noqa: F401, F403
+from .alpacaeval import AlpacaEvalDataset  # noqa: F401, F403
+from .alpacaeval import alpacaeval_postprocess  # noqa: F401, F403
 from .arena_hard import ArenaHardDataset  # noqa: F401, F403
+from .arena_hard import arenahard_postprocess  # noqa: F401, F403
 from .compass_arena import CompassArenaDataset  # noqa: F401, F403
 from .compassbench import CompassBenchDataset  # noqa: F401, F403
 from .compassbench_checklist import \
@@ -8,11 +12,14 @@ from .compassbench_control_length_bias import \
     CompassBenchControlLengthBiasDataset  # noqa: F401, F403
 from .corev2 import Corev2Dataset  # noqa: F401, F403
 from .creationbench import CreationBenchDataset  # noqa: F401, F403
-from .fofo import FofoDataset  # noqa: F401, F403
+from .fofo import FofoDataset, fofo_postprocess  # noqa: F401, F403
 from .followbench import FollowBenchDataset  # noqa: F401, F403
 from .information_retrival import IRDataset  # noqa: F401, F403
+from .judgerbench import JudgerBenchDataset  # noqa: F401, F403
+from .judgerbench import JudgerBenchEvaluator  # noqa: F401, F403
 from .mtbench import MTBenchDataset  # noqa: F401, F403
 from .mtbench101 import MTBench101Dataset  # noqa: F401, F403
 from .multiround import MultiroundDataset  # noqa: F401, F403
 from .subjective_cmp import SubjectiveCmpDataset  # noqa: F401, F403
 from .wildbench import WildBenchDataset  # noqa: F401, F403
+from .wildbench import wildbench_postprocess  # noqa: F401, F403
