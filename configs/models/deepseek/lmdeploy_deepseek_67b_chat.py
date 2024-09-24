edaa8456ca154394c64fe6c0d8b3997fcc7295f0
zhangsongyang <zhangsongyang@pjlab.org.cn>
2024-09-24 13:45:30 +0000

    [Feature] Update the max_out_len for many models

--- configs/models/deepseek/lmdeploy_deepseek_67b_chat.py
+++ configs/models/deepseek/lmdeploy_deepseek_67b_chat.py
@@ -7,8 +7,8 @@ models = [
         path='deepseek-ai/deepseek-llm-67b-chat',
         engine_config=dict(max_batch_size=16, tp=4),
         gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9),
-        max_seq_len=7168,
-        max_out_len=1024,
+        max_seq_len=8192,
+        max_out_len=2048,
         batch_size=16,
         run_cfg=dict(num_gpus=4),
     )
--- configs/models/mistral/lmdeploy_mixtral_large_instruct_2407.py
+++ configs/models/mistral/lmdeploy_mixtral_large_instruct_2407.py
@@ -3,13 +3,13 @@ from opencompass.models import TurboMindModelwithChatTemplate
 models = [
     dict(
         type=TurboMindModelwithChatTemplate,
-        abbr='qwen2-7b-instruct-turbomind',
-        path='Qwen/Qwen2-7B-Instruct',
-        engine_config=dict(session_len=16384, max_batch_size=16, tp=1),
+        abbr='mixtral-large-instruct-2407-turbomind',
+        path='mistralai/Mistral-Large-Instruct-2407',
+        engine_config=dict(session_len=32768, max_batch_size=16, tp=4),
         gen_config=dict(top_k=1, temperature=1e-6, top_p=0.9, max_new_tokens=4096),
-        max_seq_len=16384,
+        max_seq_len=32768,
         max_out_len=4096,
         batch_size=16,
-        run_cfg=dict(num_gpus=1),
+        run_cfg=dict(num_gpus=4),
     )
 ]
