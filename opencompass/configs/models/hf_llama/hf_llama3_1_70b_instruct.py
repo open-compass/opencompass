e7681943f3e491a0f4bb6648ce69c80858ca1454
Songyang Zhang <tonysy@users.noreply.github.com>
2024-09-24 21:52:28 +0800

    [Feature] Update the max_out_len for many models (#1559)

--- opencompass/configs/models/hf_llama/hf_llama3_1_70b_instruct.py
+++ opencompass/configs/models/hf_llama/hf_llama3_1_70b_instruct.py
@@ -5,7 +5,7 @@ models = [
         type=HuggingFacewithChatTemplate,
         abbr='llama-3_1-70b-instruct-hf',
         path='meta-llama/Meta-Llama-3.1-70B-Instruct',
-        max_out_len=1024,
+        max_out_len=4096,
         batch_size=8,
         run_cfg=dict(num_gpus=4),
         stop_words=['<|end_of_text|>', '<|eot_id|>'],
