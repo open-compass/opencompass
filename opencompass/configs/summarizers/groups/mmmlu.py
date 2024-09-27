811a187618022d44f247851d71f7b9bf635f524f
BobTsang <BobTsang1995@gmail.com>
2024-09-27 21:24:56 +0800

    modify format according to reviewer

--- opencompass/configs/summarizers/groups/mmmlu.py
+++ opencompass/configs/summarizers/groups/mmmlu.py
@@ -1,5 +1,5 @@
 categories = ['mmlu_AR-XY','mmlu_BN-BD','mmlu_DE-DE','mmlu_ES-LA','mmlu_FR-FR','mmlu_HI-IN','mmlu_ID-ID','mmlu_IT-IT','mmlu_JA-JP','mmlu_KO-KR','mmlu_PT-BR','mmlu_SW-KE','mmlu_YO-NG','mmlu_ZH-CN']
 
-mmlu_pro_summary_groups = [
+mmmlu_summary_groups = [
     {'name': 'mmmlu', 'subsets': [c.replace(' ', '_') for c in categories]},
 ]
