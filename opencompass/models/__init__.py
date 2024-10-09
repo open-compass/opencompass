b52ba65c267c4d8bf05cd57ed3386a2d466887db
Lyu Han <lvhan_028@163.com>
2024-10-09 22:58:06 +0800

    [Feature] Integrate lmdeploy pipeline api (#1198)
    * integrate lmdeploy's pipeline api
    
    * fix linting
    
    * update user guide
    
    * rename
    
    * update
    
    * update
    
    * update
    
    * rollback class name
    
    * update
    
    * remove unused code
    
    * update
    
    * update
    
    * fix ci check
    
    * compatibility
    
    * remove concurrency
    
    * Update configs/models/hf_internlm/lmdeploy_internlm2_chat_7b.py
    
    * Update docs/zh_cn/advanced_guides/evaluation_lmdeploy.md
    
    * [Bug] fix lint
    
    ---------
    
    Co-authored-by: Songyang Zhang <tonysy@users.noreply.github.com>
    Co-authored-by: tonysy <sy.zhangbuaa@gmail.com>

--- opencompass/models/__init__.py
+++ opencompass/models/__init__.py
@@ -25,8 +25,6 @@ from .interntrain import InternTrain  # noqa: F401
 from .krgpt_api import KrGPT  # noqa: F401
 from .lightllm_api import LightllmAPI, LightllmChatAPI  # noqa: F401
 from .llama2 import Llama2, Llama2Chat  # noqa: F401
-from .lmdeploy_pytorch import LmdeployPytorchModel  # noqa: F401
-from .lmdeploy_tis import LmdeployTisModel  # noqa: F401
 from .minimax_api import MiniMax, MiniMaxChatCompletionV2  # noqa: F401
 from .mistral_api import Mistral  # noqa: F401
 from .mixtral import Mixtral  # noqa: F401
@@ -41,7 +39,6 @@ from .rendu_api import Rendu  # noqa: F401
 from .sensetime_api import SenseTime  # noqa: F401
 from .stepfun_api import StepFun  # noqa: F401
 from .turbomind import TurboMindModel  # noqa: F401
-from .turbomind_tis import TurboMindTisModel  # noqa: F401
 from .turbomind_with_tf_above_v4_33 import \
     TurboMindModelwithChatTemplate  # noqa: F401
 from .unigpt_api import UniGPT  # noqa: F401
