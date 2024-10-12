c58d32df9e9a15196447d328fefb6d212fdbe1ca
liushz <qq1791167085@163.com>
2024-10-12 07:35:22 +0000

    Update GAOKAO Math Eval Dataset

--- opencompass/datasets/__init__.py
+++ opencompass/datasets/__init__.py
@@ -90,7 +90,6 @@ from .NPHardEval import *  # noqa: F401, F403
 from .obqa import *  # noqa: F401, F403
 from .OpenFinData import *  # noqa: F401, F403
 from .piqa import *  # noqa: F401, F403
-from .prm800k import *  # noqa: F401, F403
 from .py150 import *  # noqa: F401, F403
 from .qasper import *  # noqa: F401, F403
 from .qaspercut import *  # noqa: F401, F403
