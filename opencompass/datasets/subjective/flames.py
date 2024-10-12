3f7a3730d740aaf04a330833bc472069a856304a
bittersweet1999 <148421775+bittersweet1999@users.noreply.github.com>
2024-10-12 14:34:59 +0800

    [Fix] fix Flames (#1599)
    * fix pip version
    
    * fix pip version
    
    * fix flames
    
    * fix flames

--- opencompass/datasets/subjective/flames.py
+++ opencompass/datasets/subjective/flames.py
@@ -9,7 +9,7 @@ from datasets import Dataset, DatasetDict
 from opencompass.registry import LOAD_DATASET
 from opencompass.utils import get_data_path
 
-from .subjective.subjective_cmp import SubjectiveCmpDataset
+from .subjective_cmp import SubjectiveCmpDataset
 
 
 class Config:
@@ -36,11 +36,7 @@ def prompt_construct(sample, config: Config):
 @LOAD_DATASET.register_module()
 class FlamesDataset(SubjectiveCmpDataset):
 
-    def load(
-        self,
-        path: str,
-        name: str,
-    ):
+    def load(self, path: str, name: str, *args, **kwargs):
         path = get_data_path(path, local_mode=True)
         config = Config(path, f'{name}_config.txt')
 
