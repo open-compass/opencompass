8b797ec63d6deee551b0181337293961722bbca1
MaiziXiao <xxllcc1993@gmail.com>
2024-10-25 10:46:29 +0000

    update

--- opencompass/configs/datasets/humaneval/internal_humaneval_gen_ce6b06.py
+++ opencompass/configs/datasets/humaneval/internal_humaneval_gen_ce6b06.py
@@ -1,7 +1,7 @@
 from opencompass.openicl.icl_prompt_template import PromptTemplate
 from opencompass.openicl.icl_retriever import ZeroRetriever
 from opencompass.openicl.icl_inferencer import GenInferencer
-from opencompass.datasets import HumanevalDataset, HumanEvalEvaluator, humaneval_internal_v1_postprocess
+from opencompass.datasets import HumanevalDataset, HumanEvalEvaluator, humaneval_internal_v2_postprocess
 
 humaneval_reader_cfg = dict(
     input_columns=['prompt'], output_column='task_id', train_split='test')
@@ -19,12 +19,12 @@ humaneval_eval_cfg = dict(
     evaluator=dict(type=HumanEvalEvaluator),
     pred_role='BOT',
     k=[1, 10, 100],  # the parameter only for humaneval
-    pred_postprocessor=dict(type=humaneval_internal_v1_postprocess),
+    pred_postprocessor=dict(type=humaneval_internal_v2_postprocess),
 )
 
 humaneval_datasets = [
     dict(
-        abbr='openai_humaneval_internal_v2',
+        abbr='openai_humaneval',
         type=HumanevalDataset,
         path='opencompass/humaneval',
         reader_cfg=humaneval_reader_cfg,
