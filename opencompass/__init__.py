a3f45739baf271c69750c1176e4434a69c4ab49e
zhangsongyang <zhangsongyang@pjlab.org.cn>
2024-11-01 07:47:04 +0000

    Update CI

--- opencompass/__init__.py
+++ opencompass/__init__.py
@@ -1,9 +1,8 @@
-import warnings
-
 __version__ = '0.3.4'
 
 
 def _warn_about_config_migration():
+    import warnings
     warnings.warn(
         'Starting from v0.4.0, all AMOTIC configuration files currently '
         'located in `./configs/datasets`, `./configs/models`, and '
