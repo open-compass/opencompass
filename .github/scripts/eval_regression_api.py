a9d6b6461ff15d11aaaaee02a8c3552c93321c59
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2024-11-12 18:40:27 +0800

    [ci] react daily test (#1668)
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * update
    
    * update
    
    * refactor summarize
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update daily-run-test.yml
    
    * Update daily-run-test.yml
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update daily-run-test.yml
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update daily-run-test.yml
    
    * Update daily-run-test.yml
    
    * update
    
    * update
    
    * Update daily-run-test.yml
    
    * update
    
    * update
    
    * update
    
    ---------
    
    Co-authored-by: zhulin1 <zhulin1@pjlab.org.cn>

---
+++ .github/scripts/eval_regression_api.py
@@ -0,0 +1,39 @@
+from mmengine.config import read_base
+
+from opencompass.models.openai_api import OpenAISDK
+
+with read_base():
+    # choose a list of datasets
+    from opencompass.configs.datasets.gsm8k.gsm8k_gen import \
+        gsm8k_datasets  # noqa: F401, E501
+    from opencompass.configs.datasets.race.race_gen import \
+        race_datasets  # noqa: F401, E501
+
+datasets = sum([v for k, v in locals().items() if k.endswith('_datasets')], [])
+
+api_meta_template = dict(
+    round=[
+        dict(role='HUMAN', api_role='HUMAN'),
+        dict(role='BOT', api_role='BOT', generate=True),
+    ],
+    reserved_roles=[dict(role='SYSTEM', api_role='SYSTEM')],
+)
+
+models = [
+    dict(
+        abbr='lmdeploy-api-test',
+        type=OpenAISDK,
+        key='EMPTY',
+        openai_api_base='http://localhost:23333/v1',
+        path='internlm2',
+        tokenizer_path='internlm/internlm2_5-7b-chat',
+        rpm_verbose=True,
+        meta_template=api_meta_template,
+        query_per_second=128,
+        max_out_len=1024,
+        max_seq_len=4096,
+        temperature=0.01,
+        batch_size=128,
+        retry=20,
+    )
+]
