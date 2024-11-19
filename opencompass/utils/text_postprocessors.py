ae2e5dbacfcb37691816edade3da74028359f89b
zhulin1 <zhulin1@pjlab.org.cn>
2024-11-19 13:31:49 +0800

    update

--- opencompass/utils/text_postprocessors.py
+++ opencompass/utils/text_postprocessors.py
@@ -128,10 +128,7 @@ def first_option_postprocess(text: str, options: str, cushion=True) -> str:
         text = text.strip()
         match = re.search(pattern, text, re.DOTALL)
         if match:
-            if match.group(1) is not None and match.group(1) != '':
-                outputs = match.group(1)
-            else:
-                outputs = match.group(0)
+            outputs = match.group(1)
             for i in options:
                 if i in outputs:
                     return i
