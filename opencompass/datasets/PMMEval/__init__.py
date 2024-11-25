d9bcfad112c94e5ea0f229e4a8e529994397e2cf
wanyu2018umac <42405907+wanyu2018umac@users.noreply.github.com>
2024-11-25 17:17:20 +0800

    Update __init__.py

--- opencompass/datasets/PMMEval/__init__.py
+++ opencompass/datasets/PMMEval/__init__.py
@@ -1,17 +1,8 @@
-from .combination_checker import combination_checker
-from .detectable_content_checker import detectable_content_checker
-from .detectable_format_checker import detectable_format_checker
-from .keywords_checker import keywords_checker
-from .length_constraints_checker import length_constraints_checker
-from .punctuation_checker import punctuation_checker
-from .startend_checker import startend_checker
-
-mifeval_class_map = {
-    'combination': combination_checker,
-    'detectable_content': detectable_content_checker,
-    'detectable_format': detectable_format_checker,
-    'keywords': keywords_checker,
-    'length_constraints': length_constraints_checker,
-    'punctuation': punctuation_checker,
-    'startend': startend_checker
-}
+from .flores import *  # noqa: F401, F403
+from .humanevalxl import *  # noqa: F401, F403
+from .mgsm import *  # noqa: F401, F403
+from .mhellaswag import *  # noqa: F401, F403
+from .mifeval import *  # noqa: F401, F403
+from .mlogiqa import *  # noqa: F401, F403
+from .mmmlu import *  # noqa: F401, F403
+from .xnli import *  # noqa: F401, F403
