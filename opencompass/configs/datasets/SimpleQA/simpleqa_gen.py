a17bf378c731defcc2752c26ae9e2206193d3fda
liushz <qq1791167085@163.com>
2024-11-28 05:44:08 +0000

    Add Openai SimpleQA dataset

--- opencompass/configs/datasets/SimpleQA/simpleqa_gen.py
+++ opencompass/configs/datasets/SimpleQA/simpleqa_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .simpleqa_gen_1d7fe4 import simpleqa_datasets  # noqa: F401, F403
+    from .simpleqa_gen_0283c3 import simpleqa_datasets  # noqa: F401, F403
