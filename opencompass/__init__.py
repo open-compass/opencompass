bca1d442266c3649d7b67677a2d514063ae623a0
MaiziXiao <xxllcc1993@gmail.com>
2024-12-03 10:47:23 +0000

    [Bump] Bump version to 0.3.7

--- opencompass/__init__.py
+++ opencompass/__init__.py
@@ -1,8 +1,9 @@
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 
 
 def _warn_about_config_migration():
     import warnings
+
     warnings.warn(
         'Starting from v0.4.0, all AMOTIC configuration files currently '
         'located in `./configs/datasets`, `./configs/models`, and '
@@ -10,7 +11,8 @@ def _warn_about_config_migration():
         '`opencompass/configs/` package. Please update your configuration '
         'file paths accordingly.',
         UserWarning,  # Changed to UserWarning
-        stacklevel=2)
+        stacklevel=2,
+    )
 
 
 # Trigger the warning
