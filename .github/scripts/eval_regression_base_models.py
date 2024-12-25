c48bbde26fc98a0876a3450fe8655f6a0e7f2faf
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2024-12-25 17:26:50 +0800

    [ci] remove testcase into volc engine (#1777)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * updaste
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * Update daily-run-test.yml
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- .github/scripts/eval_regression_base_models.py
+++ .github/scripts/eval_regression_base_models.py
@@ -13,12 +13,22 @@ with read_base():
     # read hf models - chat models
     from opencompass.configs.models.chatglm.hf_glm4_9b import \
         models as hf_glm4_9b_model  # noqa: F401, E501
+    from opencompass.configs.models.chatglm.lmdeploy_glm4_9b import \
+        models as lmdeploy_glm4_9b_model  # noqa: F401, E501
+    from opencompass.configs.models.deepseek.hf_deepseek_7b_base import \
+        models as hf_deepseek_7b_base_model  # noqa: F401, E501
+    from opencompass.configs.models.deepseek.hf_deepseek_67b_base import \
+        models as hf_deepseek_67b_base_model  # noqa: F401, E501
     from opencompass.configs.models.deepseek.hf_deepseek_moe_16b_base import \
         models as hf_deepseek_moe_16b_base_model  # noqa: F401, E501
     from opencompass.configs.models.deepseek.hf_deepseek_v2_lite import \
         models as hf_deepseek_v2_lite_model  # noqa: F401, E501
     from opencompass.configs.models.deepseek.lmdeploy_deepseek_7b_base import \
         models as lmdeploy_deepseek_7b_base_model  # noqa: F401, E501
+    from opencompass.configs.models.deepseek.lmdeploy_deepseek_67b_base import \
+        models as lmdeploy_deepseek_67b_base_model  # noqa: F401, E501
+    from opencompass.configs.models.deepseek.lmdeploy_deepseek_v2 import \
+        lmdeploy_deepseek_v2_model  # noqa: F401, E501
     from opencompass.configs.models.deepseek.vllm_deepseek_moe_16b_base import \
         models as vllm_deepseek_moe_16b_base_model  # noqa: F401, E501
     from opencompass.configs.models.gemma.hf_gemma2_2b import \
@@ -29,6 +39,8 @@ with read_base():
         models as hf_gemma_2b_model  # noqa: F401, E501
     from opencompass.configs.models.gemma.hf_gemma_7b import \
         models as hf_gemma_7b_model  # noqa: F401, E501
+    from opencompass.configs.models.gemma.lmdeploy_gemma_9b import \
+        models as lmdeploy_gemma_9b_model  # noqa: F401, E501
     from opencompass.configs.models.gemma.vllm_gemma_2b import \
         models as vllm_gemma_2b_model  # noqa: F401, E501
     from opencompass.configs.models.gemma.vllm_gemma_7b import \
@@ -59,10 +71,14 @@ with read_base():
         models as hf_llama3_1_8b_model  # noqa: F401, E501
     from opencompass.configs.models.hf_llama.hf_llama3_8b import \
         models as hf_llama3_8b_model  # noqa: F401, E501
+    from opencompass.configs.models.hf_llama.hf_llama3_70b import \
+        models as hf_llama3_70b_model  # noqa: F401, E501
     from opencompass.configs.models.hf_llama.lmdeploy_llama3_1_8b import \
         models as lmdeploy_llama3_1_8b_model  # noqa: F401, E501
     from opencompass.configs.models.hf_llama.lmdeploy_llama3_8b import \
         models as lmdeploy_llama3_8b_model  # noqa: F401, E501
+    from opencompass.configs.models.hf_llama.lmdeploy_llama3_70b import \
+        models as lmdeploy_llama3_70b_model  # noqa: F401, E501
     from opencompass.configs.models.mistral.hf_mistral_7b_v0_2 import \
         models as hf_mistral_7b_v0_2_model  # noqa: F401, E501
     from opencompass.configs.models.mistral.hf_mistral_7b_v0_3 import \
@@ -73,10 +89,16 @@ with read_base():
         models as hf_qwen_2_5_7b_model  # noqa: F401, E501
     from opencompass.configs.models.qwen2_5.hf_qwen_2_5_14b import \
         models as hf_qwen_2_5_14b_model  # noqa: F401, E501
+    from opencompass.configs.models.qwen2_5.hf_qwen_2_5_32b import \
+        models as hf_qwen_2_5_32b_model  # noqa: F401, E501
     from opencompass.configs.models.qwen2_5.lmdeploy_qwen2_5_1_5b import \
         models as lmdeploy_qwen2_5_1_5b_model  # noqa: F401, E501
     from opencompass.configs.models.qwen2_5.lmdeploy_qwen2_5_7b import \
         models as lmdeploy_qwen2_5_7b_model  # noqa: F401, E501
+    from opencompass.configs.models.qwen2_5.lmdeploy_qwen2_5_32b import \
+        models as lmdeploy_qwen2_5_32b_model  # noqa: F401, E501
+    from opencompass.configs.models.qwen2_5.lmdeploy_qwen2_5_72b import \
+        models as lmdeploy_qwen2_5_72b_model  # noqa: F401, E501
     from opencompass.configs.models.qwen.hf_qwen1_5_moe_a2_7b import \
         models as hf_qwen1_5_moe_a2_7b_model  # noqa: F401, E501
     from opencompass.configs.models.qwen.hf_qwen2_0_5b import \
@@ -95,6 +117,10 @@ with read_base():
         models as hf_yi_1_5_6b_model  # noqa: F401, E501
     from opencompass.configs.models.yi.hf_yi_1_5_9b import \
         models as hf_yi_1_5_9b_model  # noqa: F401, E501
+    from opencompass.configs.models.yi.lmdeploy_yi_1_5_9b import \
+        models as lmdeploy_yi_1_5_9b_model  # noqa: F401, E501
+
+    from ...volc import infer as volc_infer  # noqa: F401, E501
 
 race_datasets = [race_datasets[1]]
 models = sum([v for k, v in locals().items() if k.endswith('_model')], [])
