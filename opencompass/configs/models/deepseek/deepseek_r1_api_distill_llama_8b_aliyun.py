c3b08030139fe1eac09f4233fb039290bac29b98
hoteryoung <hoteryoung@163.com>
2025-02-10 10:55:37 +0800

    support deepseek-r1-distill-qwen-7b and -llama-8b

---
+++ opencompass/configs/models/deepseek/deepseek_r1_api_distill_llama_8b_aliyun.py
@@ -0,0 +1,17 @@
+from opencompass.models import OpenAISDK
+
+models = [
+    dict(
+        abbr='deepseek_r1_api_distill_llama_8b_aliyun',
+        type=OpenAISDK,
+        path='deepseek-r1-distill-llama-8b',
+        key='ENV_ALIYUN',
+        openai_api_base='https://dashscope.aliyuncs.com/compatible-mode/v1',
+        query_per_second=1,
+        max_out_len=2048,
+        max_seq_len=4096,
+        batch_size=8,
+        retry=30,
+        verbose=True,
+    ),
+]
