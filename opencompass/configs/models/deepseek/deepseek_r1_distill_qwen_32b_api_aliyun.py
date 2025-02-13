09717773480391db93c5021233c0331d99cf26ab
Hoter Young <hoteryoung@163.com>
2025-02-13 21:42:16 +0800

    [Feature] Support DeepSeep-R1-Distill-Qwen_32B (#30)

---
+++ opencompass/configs/models/deepseek/deepseek_r1_distill_qwen_32b_api_aliyun.py
@@ -0,0 +1,17 @@
+from opencompass.models import OpenAISDK
+
+models = [
+    dict(
+        abbr='deepseek_r1_distill_qwen_32b_api_aliyun',
+        type=OpenAISDK,
+        path='deepseek-r1-distill-qwen-32b',
+        key='ENV_ALIYUN',
+        openai_api_base='https://dashscope.aliyuncs.com/compatible-mode/v1',
+        query_per_second=1,
+        max_out_len=8192,
+        max_seq_len=32768,
+        batch_size=1,
+        retry=30,
+        verbose=True,
+    ),
+]
