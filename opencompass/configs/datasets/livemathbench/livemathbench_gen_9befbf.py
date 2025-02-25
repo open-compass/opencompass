33a9c4ab2fd4f5f0cd3eb5e3cbb94ff23ade1a14
Junnan Liu <to.liujn@outlook.com>
2025-02-25 15:45:11 +0800

    update livemathbench_gen_9befbf.py

--- opencompass/configs/datasets/livemathbench/livemathbench_gen_9befbf.py
+++ opencompass/configs/datasets/livemathbench/livemathbench_gen_9befbf.py
@@ -5,54 +5,47 @@ from opencompass.openicl.icl_inferencer import GenInferencer
 from opencompass.datasets.livemathbench import LiveMathBenchDataset, LiveMathBenchEvaluator
 
 
-reader_cfg = dict(
-    input_columns=['prompt'], 
-    output_column='answer'
-)
-
-infer_cfg = dict(
-    prompt_template=dict(
-        type=PromptTemplate,
-        template=dict(
-            round=[
-                dict(role='HUMAN', prompt='{prompt}'),
-            ]
+livemathbench_dataset = dict(
+    type=LiveMathBenchDataset,
+    path='',
+    k=16,
+    replication=3,
+    dataset_splits=['CNMO', 'CCEE', 'AMC', 'WLPMC'],
+    dataset_languages=['cn', 'en'],
+    cot=True,
+    version='202412',
+    abbr='LiveMathBench-v202412',
+    reader_cfg=dict(
+        input_columns=['prompt'], 
+        output_column='answer'
+    ),
+    infer_cfg=dict(
+        prompt_template=dict(
+            type=PromptTemplate,
+            template=dict(
+                round=[
+                    dict(role='HUMAN', prompt='{prompt}'),
+                ]
+            )
+        ),
+        retriever=dict(type=ZeroRetriever),
+        inferencer=dict(
+            type=GenInferencer, 
+            max_out_len=8192
+        ),
+    ),
+    eval_cfg=dict(
+        evaluator=dict(
+            type=LiveMathBenchEvaluator,
+            model_name='',
+            url=[],
+            use_extract_model=False,
+            extract_url=[],
+            extract_model_name='',
+            k=[4, 8, 16],
+            replication=3,
+            thresholds=[0.0, 0.25, 0.5, 0.75, 1.0]
         )
-    ),
-    retriever=dict(type=ZeroRetriever),
-    inferencer=dict(
-        type=GenInferencer, 
-        max_out_len=8192
-    ),
-)
-
-eval_cfg = dict(
-    evaluator=dict(
-        type=LiveMathBenchEvaluator,
-        model_name='',
-        url=[],
-        use_extract_model=False,
-        extract_url=[],
-        extract_model_name='',
-        k=[4, 8, 16],
-        replication=3,
-        thresholds=[0.0, 0.25, 0.5, 0.75, 1.0]
     )
 )
-
-livemathbench_datasets = [
-    dict(
-        type=LiveMathBenchDataset,
-        path='',
-        k=16,
-        replication=3,
-        dataset_splits=['CNMO', 'CCEE', 'AMC', 'WLPMC'],
-        dataset_languages=['cn', 'en'],
-        cot=True,
-        version='202412',
-        abbr='LiveMathBench-v202412-k16r3',
-        reader_cfg=reader_cfg,
-        infer_cfg=infer_cfg,
-        eval_cfg=eval_cfg
-    )
-]
+livemathbench_datasets = [livemathbench_dataset]
