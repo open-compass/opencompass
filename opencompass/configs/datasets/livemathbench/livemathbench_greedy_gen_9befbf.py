32a8d81b1d94971c53db96379d4d9f92c400720e
jnanliu <to.liujn@outlook.com>
2025-02-26 06:39:12 +0000

    del extract_model param in livemathbench config

--- opencompass/configs/datasets/livemathbench/livemathbench_greedy_gen_9befbf.py
+++ opencompass/configs/datasets/livemathbench/livemathbench_greedy_gen_9befbf.py
@@ -38,10 +38,7 @@ livemathbench_dataset = dict(
         evaluator=dict(
             type=LiveMathBenchEvaluator,
             model_name='',
-            url=[],
-            use_extract_model=False,
-            extract_url=[],
-            extract_model_name=''
+            url=[]
         )
     )
 )
