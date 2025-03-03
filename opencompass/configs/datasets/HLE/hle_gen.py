5a2462a26faf56c0990513309afb7676aa50a8d8
liushz <qq1791167085@163.com>
2025-03-03 10:52:30 +0000

    Add HLE dataset

--- opencompass/configs/datasets/HLE/hle_gen.py
+++ opencompass/configs/datasets/HLE/hle_gen.py
@@ -1,4 +1,5 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .hle_llmjudge_gen_63a000 import hle_datasets  # noqa: F401, F403
+    # Default use LLM as a judge
+    from .hle_llmverify_gen_6ff468 import hle_datasets  # noqa: F401, F403
