8709af3d258fe7ee1640e9f6d1a05ff67a371587
Myhs-phz <demarcia2014@126.com>
2025-03-12 06:02:14 +0000

    fix

--- opencompass/configs/models/qwq/lmdeploy_qwq_32b.py
+++ opencompass/configs/models/qwq/lmdeploy_qwq_32b.py
@@ -1,4 +1,5 @@
 from opencompass.models import TurboMindModelwithChatTemplate
+from opencompass.utils.text_postprocessors import extract_non_reasoning_content
 
 models = [
     dict(
@@ -11,5 +12,6 @@ models = [
         max_out_len=8192,
         batch_size=16,
         run_cfg=dict(num_gpus=2),
+        pred_postprocessor=dict(type=extract_non_reasoning_content)
     )
 ]
