df4ef4353477202dd7539dc316b8ec0271587f6e
MaiziXiao <xxllcc1993@gmail.com>
2025-03-13 09:20:43 +0000

    update

--- opencompass/configs/datasets/bbeh/bbeh_llmjudge_gen_86c3a0.py
+++ opencompass/configs/datasets/bbeh/bbeh_llmjudge_gen_86c3a0.py
@@ -124,39 +124,3 @@ for _name in bbeh_multiple_choice_sets + bbeh_free_form_sets:
             eval_cfg=bbeh_eval_cfg,
         )
     )
-
-# for _name in bbeh_free_form_sets:
-#     bbeh_infer_cfg = dict(
-#         prompt_template=dict(
-#             type=PromptTemplate,
-#             template=dict(
-#                 round=[
-#                     dict(
-#                         role='HUMAN',
-#                         prompt=f"Think step by step, and when you provide the final answer, please use the prefix \"The answer is:\"without any modification, and provide the answer directly, with no formatting, no bolding, and no markup. For instance: \"The answer is: 42\" or \"The answer is: yes\". If the question is multiple choice with a single correct answer, the final answer must only be the letter corresponding to the correct answer. For example, \"The answer is: (a)\"\n\nQ: {{input}}\nA: ",
-#                     )
-#                 ]
-#             ),
-#         ),
-#         retriever=dict(type=ZeroRetriever),
-#         inferencer=dict(type=GenInferencer),
-#     )
-
-#     bbeh_eval_cfg = dict(
-#         evaluator=dict(type=BBEHEvaluator),
-#         pred_role='BOT',
-#         pred_postprocessor=dict(type=bbeh_mcq_postprocess),
-#         dataset_postprocessor=dict(type=bbeh_mcq_postprocess),
-#     )
-
-#     bbeh_datasets.append(
-#         dict(
-#             type=BBEHDataset,
-#             path='opencompass/bbeh',
-#             name=_name,
-#             abbr=_name,
-#             reader_cfg=bbeh_reader_cfg,
-#             infer_cfg=bbeh_infer_cfg.copy(),
-#             eval_cfg=bbeh_eval_cfg.copy(),
-#         )
-#     )
