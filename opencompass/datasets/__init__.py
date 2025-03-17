cd2678dddf6e0a3c599a75e2f16ca2569fe587a6
yufeng zhao <zhaoyufeng@pjlab.org.cn>
2025-03-17 09:13:00 +0000

    cleaned_rebased

--- opencompass/datasets/__init__.py
+++ opencompass/datasets/__init__.py
@@ -11,8 +11,6 @@ from .ax import *  # noqa: F401, F403
 from .babilong import *  # noqa: F401, F403
 from .bbeh import *  # noqa: F401, F403
 from .bbh import *  # noqa: F401, F403
-from .bbeh import *  # noqa: F401, F403
-from .bbh import *  # noqa: F401, F403
 from .bigcodebench import *  # noqa: F401, F403
 from .boolq import *  # noqa: F401, F403
 from .bustum import *  # noqa: F401, F403
