199052e92e85d6895d43612eb9a5f09d33ad9391
Myhs-phz <demarcia2014@126.com>
2025-03-24 10:15:34 +0000

    fix

--- opencompass/configs/datasets/IFEval/IFEval_gen.py
+++ opencompass/configs/datasets/IFEval/IFEval_gen.py
@@ -1,33 +1,4 @@
-from opencompass.openicl.icl_prompt_template import PromptTemplate
-from opencompass.openicl.icl_retriever import ZeroRetriever
-from opencompass.openicl.icl_inferencer import GenInferencer
-from opencompass.datasets import IFEvalDataset, IFEvaluator
+from mmengine.config import read_base
 
-ifeval_reader_cfg = dict(
-    input_columns=['prompt'], output_column='reference')
-
-ifeval_infer_cfg = dict(
-    prompt_template=dict(
-        type=PromptTemplate,
-        template=dict(round=[
-            dict(
-                role='HUMAN',
-                prompt='{prompt}'),
-        ])),
-    retriever=dict(type=ZeroRetriever),
-    inferencer=dict(type=GenInferencer))
-
-ifeval_eval_cfg = dict(
-    evaluator=dict(type=IFEvaluator),
-    pred_role='BOT',
-)
-
-ifeval_datasets = [
-    dict(
-        abbr='IFEval',
-        type=IFEvalDataset,
-        path='data/ifeval/input_data.jsonl',
-        reader_cfg=ifeval_reader_cfg,
-        infer_cfg=ifeval_infer_cfg,
-        eval_cfg=ifeval_eval_cfg)
-]
+with read_base():
+    from .IFEval_gen_353ae7 import ifeval_datasets  # noqa: F401, F403
---
+++ opencompass/configs/datasets/aime2024/aime2024_gen_17d799.py
@@ -0,0 +1,40 @@
+from opencompass.openicl.icl_prompt_template import PromptTemplate
+from opencompass.openicl.icl_retriever import ZeroRetriever
+from opencompass.openicl.icl_inferencer import GenInferencer
+from opencompass.datasets import Aime2024Dataset, MATHEvaluator, math_postprocess_v2
+from opencompass.datasets import CustomDataset
+
+
+aime2024_reader_cfg = dict(
+    input_columns=['question'],
+    output_column='answer'
+)
+
+
+aime2024_infer_cfg = dict(
+    prompt_template=dict(
+        type=PromptTemplate,
+        template=dict(
+            round=[
+                dict(role='HUMAN', prompt='{question}\nPlease reason step by step, and put your final answer within \\boxed{}.'),
+            ],
+        )
+    ),
+    retriever=dict(type=ZeroRetriever),
+    inferencer=dict(type=GenInferencer)
+)
+
+aime2024_eval_cfg = dict(
+    evaluator=dict(type=MATHEvaluator, version='v2'), pred_postprocessor=dict(type=math_postprocess_v2)
+)
+
+aime2024_datasets = [
+    dict(
+        abbr='aime2024',
+        type=CustomDataset,
+        path='opencompass/aime2025',
+        reader_cfg=aime2024_reader_cfg,
+        infer_cfg=aime2024_infer_cfg,
+        eval_cfg=aime2024_eval_cfg,
+    )
+]
