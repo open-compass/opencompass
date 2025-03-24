796e2d01777195691b551c076f4aa843be0834fa
Myhs-phz <demarcia2014@126.com>
2025-03-24 10:29:32 +0000

    fix

--- opencompass/configs/datasets/bbh/bbh_llm_judge_gen.py
+++ opencompass/configs/datasets/bbh/bbh_llm_judge_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .bbh_llmjudge_gen_ee62e9 import bbh_datasets  # noqa: F401, F403
+    from .bbh_llmjudge_gen_b5bdf1 import bbh_datasets  # noqa: F401, F403
