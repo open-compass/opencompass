55d6941ed9589a6420bc1434481433759561fe16
liushz <qq1791167085@163.com>
2025-03-28 11:46:29 +0000

    Add olymmath dataset

--- opencompass/configs/datasets/OlymMATH/olymmath_gen.py
+++ opencompass/configs/datasets/OlymMATH/olymmath_gen.py
@@ -2,4 +2,4 @@ from mmengine.config import read_base
 
 with read_base():
     # Default use LLM as a judge
-    from .olymmath_llmverify_gen_6ff468 import olymmath_datasets  # noqa: F401, F403
+    from .olymmath_llmverify_gen_97b203 import olymmath_datasets  # noqa: F401, F403
