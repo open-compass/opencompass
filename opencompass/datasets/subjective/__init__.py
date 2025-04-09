6ac9b06bc2209d82333987671d20175d7cd0d2f7
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2025-04-09 14:09:35 +0800

    [ci] update baseline for kernal change of vllm and lmdeploy (#2011)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update

--- opencompass/datasets/subjective/__init__.py
+++ opencompass/datasets/subjective/__init__.py
@@ -7,6 +7,7 @@ from .alpacaeval import alpacaeval_postprocess  # noqa: F401, F403
 from .arena_hard import ArenaHardDataset  # noqa: F401, F403
 from .arena_hard import arenahard_bradleyterry_postprocess  # noqa: F401, F403
 from .arena_hard import arenahard_postprocess  # noqa: F401, F403
+from .commonbench import commonbench_postprocess
 from .compass_arena import CompassArenaDataset  # noqa: F401, F403
 from .compass_arena import \
     compassarena_bradleyterry_postprocess  # noqa: F401, F403
