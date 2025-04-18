d74bb2ac7f3b42ec611cc171ec0c474bc4f36cd3
MaiziXiao <xxllcc1993@gmail.com>
2025-04-18 07:27:13 +0000

    update

--- opencompass/configs/datasets/SmolInstruct/smolinstruct_gen.py
+++ opencompass/configs/datasets/SmolInstruct/smolinstruct_gen.py
@@ -1,10 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from opencompass.configs.datasets.SmolInstruct.smolinstruct_nc_gen import nc_datasets
-    from opencompass.configs.datasets.SmolInstruct.smolinstruct_pp_acc_gen import pp_acc_datasets
-    from opencompass.configs.datasets.SmolInstruct.smolinstruct_rmse_gen import pp_rmse_datasets
-    from opencompass.configs.datasets.SmolInstruct.smolinstruct_fts_gen import fts_datasets
-    from opencompass.configs.datasets.SmolInstruct.smolinstruct_meteor_gen import meteor_datasets
-
-smolinstruct_datasets = nc_datasets + pp_rmse_datasets + pp_acc_datasets + meteor_datasets + fts_datasets
+    from smolinstruct_gen_c84c18 import smolinstruct_datasets
