70e0dc1674655a4d1fdb11facff5bd2ebdc85275
Mor-Li <2568818204@qq.com>
2025-04-25 18:14:57 +0800

    fix lint

--- opencompass/configs/models/gemma/vllm_gemma_3_27b_it.py
+++ opencompass/configs/models/gemma/vllm_gemma_3_27b_it.py
@@ -7,7 +7,7 @@ models = [
         path='google/gemma-3-27b-it',
         model_kwargs=dict(tensor_parallel_size=4,
                           # for long context
-                          rope_scaling={"factor": 8.0, "rope_type": "linear"}),
+                          rope_scaling={'factor': 8.0, 'rope_type': 'linear'}),
         max_out_len=4096,
         batch_size=1,
         generation_kwargs=dict(temperature=0),
--- opencompass/configs/models/gemma/vllm_gemma_3_4b_it.py
+++ opencompass/configs/models/gemma/vllm_gemma_3_4b_it.py
@@ -7,7 +7,7 @@ models = [
         path='google/gemma-3-4b-it',
         model_kwargs=dict(tensor_parallel_size=2, 
                           # for long context
-                          rope_scaling={"factor": 8.0, "rope_type": "linear"}),
+                          rope_scaling={'factor': 8.0, 'rope_type': 'linear'}),
         max_seq_len=140000,
         max_out_len=4096,
         batch_size=1,
