ea413544e23f520a85b142dd71a0558ac9655c33
taolinzhang <673879891@qq.com>
2025-04-30 06:33:26 +0000

    add judgebench

--- opencompass/datasets/judge/__init__.py
+++ opencompass/datasets/judge/__init__.py
@@ -1,3 +1,3 @@
+from .judgebench import JudgeBenchDataset  # noqa: F401, F403
 from .rewardbench import RewardBenchDataset  # noqa: F401, F403
 from .rmb import RMBDataset  # noqa: F401, F403
-from .judgebench import JudgeBenchDataset
