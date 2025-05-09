b7c7d1fd5c16fe2b4de9569acd7b694929c2094d
huihui <huihui.mlpeople@gmail.com>
2025-05-09 08:55:51 +0000

    fix lint

--- opencompass/configs/datasets/SciKnowEval/SciKnowEval_llmjudge_gen_ebe47d.py
+++ opencompass/configs/datasets/SciKnowEval/SciKnowEval_llmjudge_gen_ebe47d.py
@@ -4,6 +4,7 @@ from opencompass.openicl.icl_inferencer import GenInferencer
 from opencompass.openicl.icl_prompt_template import PromptTemplate
 from opencompass.openicl.icl_retriever import ZeroRetriever
 from opencompass.evaluator import GenericLLMEvaluator
+
 ZERO_SHOT_PROMPT = '{q4}'
 
 GRADER_TEMPLATE = """
