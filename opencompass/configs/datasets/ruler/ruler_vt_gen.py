bce27abc6929fd36d289748b6a935f8d8c856e8f
Chang Lan <c.lan@apple.com>
2025-05-20 17:56:17 -0700

    [RULER] Extend 256k and 512k data generators

--- opencompass/configs/datasets/ruler/ruler_vt_gen.py
+++ opencompass/configs/datasets/ruler/ruler_vt_gen.py
@@ -1,8 +1,7 @@
+from opencompass.datasets.ruler.ruler_vt import RulerVtDataset, RulerVtEvaluator
+from opencompass.openicl.icl_inferencer import GenInferencer
 from opencompass.openicl.icl_prompt_template import PromptTemplate
 from opencompass.openicl.icl_retriever import ZeroRetriever
-from opencompass.openicl.icl_inferencer import GenInferencer
-from opencompass.datasets.ruler.ruler_vt import RulerVtDataset
-from opencompass.datasets.ruler.ruler_vt import RulerVtEvaluator
 
 # VT Dataset
 vt_datasets = [
