56d49d034b2ca8394d1f3d529f762745c57206b7
Chang Lan <c.lan@apple.com>
2025-05-20 17:56:17 -0700

    [RULER] Extend 256k and 512k data generators

--- opencompass/configs/summarizers/groups/ruler.py
+++ opencompass/configs/summarizers/groups/ruler.py
@@ -1,19 +1,21 @@
+"""RULER summary groups"""
+
 default_ruler_tasks = [
-    'ruler_niah_single_1',
-    'ruler_niah_single_2',
-    'ruler_niah_single_3',
-    'ruler_niah_multikey_1',
-    'ruler_niah_multikey_2',
-    'ruler_niah_multikey_3',
-    'ruler_niah_multivalue',
-    'ruler_niah_multiquery',
-    'ruler_vt',
-    'ruler_fwe',
-    'ruler_cwe',
-    'ruler_qa_squad',
-    'ruler_qa_hotpotqa',
+    "ruler_niah_single_1",
+    "ruler_niah_single_2",
+    "ruler_niah_single_3",
+    "ruler_niah_multikey_1",
+    "ruler_niah_multikey_2",
+    "ruler_niah_multikey_3",
+    "ruler_niah_multivalue",
+    "ruler_niah_multiquery",
+    "ruler_vt",
+    "ruler_fwe",
+    "ruler_cwe",
+    "ruler_qa_squad",
+    "ruler_qa_hotpotqa",
 ]
-context_window_sizes = ['4k', '8k', '16k', '32k', '64k', '128k', '1m']
+context_window_sizes = ["4k", "8k", "16k", "32k", "64k", "128k", "256k", "512k", "1m"]
 
 ruler_summary_groups = []
 for context_window_size in context_window_sizes:
