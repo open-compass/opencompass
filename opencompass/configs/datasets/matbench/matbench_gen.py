3dc6a7f5b65edeb35c077225f69af92ff8d7e95b
Jucheng Hu <jucheng.hu.20@ucl.ac.uk>
2025-05-28 15:57:34 +0800

    fix file and import naming

--- opencompass/configs/datasets/matbench/matbench_gen.py
+++ opencompass/configs/datasets/matbench/matbench_gen.py
@@ -1,5 +1,5 @@
 from mmengine.config import read_base
 
 with read_base():
-    # from .matbench_gen_testing_updated import matbench_datasets  # noqa: F401, F403
+    # from .matbench_gen_regex_judge import matbench_datasets  # noqa: F401, F403
     from .matbench_gen_llm_judge import matbench_datasets  # noqa: F401, F403
