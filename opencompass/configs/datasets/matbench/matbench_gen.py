a120c3d2e1aead927daab0821d341909ea1c4654
JuchengHu <36852779+smgjch@users.noreply.github.com>
2025-06-04 15:17:26 +0800

    [Datasets] MatBench update (#2116)
    * add LLM as judge setting for matbench
    
    * fix reference negative sample gold value missing error
    
    * update import
    
    * comments trim
    
    * fix file and import naming
    
    * matbench fix
    
    * matbench fix
    
    * matbench fix
    
    ---------
    
    Co-authored-by: Jucheng Hu <jucheng.hu.20@ucl.ac.uk>
    Co-authored-by: Myhs-phz <demarcia2014@126.com>

--- opencompass/configs/datasets/matbench/matbench_gen.py
+++ opencompass/configs/datasets/matbench/matbench_gen.py
@@ -1,4 +1,5 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .matbench_gen_f71840 import matbench_datasets  # noqa: F401, F403
+    # from .matbench_gen_regex_judge import matbench_datasets  # noqa: F401, F403
+    from .matbench_llm_judge_gen_0e9276 import matbench_datasets  # noqa: F401, F403
