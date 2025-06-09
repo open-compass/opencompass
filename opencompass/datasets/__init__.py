c2bd67bb1ecbc9e00feae2337d5caeac15f166d5
chenzihong_gavin <522023320011@smail.nju.edu.cn>
2025-06-09 02:19:38 +0000

    fix: fix lint problem

--- opencompass/datasets/__init__.py
+++ opencompass/datasets/__init__.py
@@ -139,9 +139,9 @@ from .ruler import *  # noqa: F401, F403
 from .safety import *  # noqa: F401, F403
 from .scibench import ScibenchDataset, scibench_postprocess  # noqa: F401, F403
 from .scicode import *  # noqa: F401, F403
-from .SeedBench import *  # noqa: F401, F403
 from .SciEval import SciEvalDataset  # noqa: F401
 from .SciKnowEval import *  # noqa: F401, F403
+from .SeedBench import *  # noqa: F401, F403
 from .simpleqa import *  # noqa: F401, F403
 from .siqa import *  # noqa: F401, F403
 from .smolinstruct import *  # noqa: F401, F403
