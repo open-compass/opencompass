8ba2131d9edecd73da500a9a430692036b4a2a52
jnanliu <to.liujn@outlook.com>
2025-06-11 16:47:11 +0000

    update: support livemathbench v202505

--- opencompass/configs/datasets/livemathbench/livemathbench_greedy_gen_9befbf.py
+++ opencompass/configs/datasets/livemathbench/livemathbench_greedy_gen_9befbf.py
@@ -43,3 +43,43 @@ livemathbench_dataset = dict(
     )
 )
 livemathbench_datasets = [livemathbench_dataset]
+
+
+livemathbench_v202505_dataset = dict(
+    type=LiveMathBenchDataset,
+    path='',
+    k=1,
+    n=1,
+    dataset_splits=['all'],
+    dataset_languages=['en'],
+    cot=True,
+    version='202505',
+    abbr='LiveMathBench-v202505',
+    reader_cfg=dict(
+        input_columns=['prompt'], 
+        output_column='answer'
+    ),
+    infer_cfg=dict(
+        prompt_template=dict(
+            type=PromptTemplate,
+            template=dict(
+                round=[
+                    dict(role='HUMAN', prompt='{prompt}'),
+                ]
+            )
+        ),
+        retriever=dict(type=ZeroRetriever),
+        inferencer=dict(
+            type=GenInferencer, 
+            max_out_len=8192
+        ),
+    ),
+    eval_cfg=dict(
+        evaluator=dict(
+            type=LiveMathBenchEvaluator,
+            model_name='',
+            url=[]
+        )
+    )
+)
+livemathbench_v202505_datasets = [livemathbench_v202505_dataset]
