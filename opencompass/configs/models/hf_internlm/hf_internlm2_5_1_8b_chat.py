f611310014e86479313213e594d178ddbd9e4504
xgao922 <gaox23@m.fudan.edu.cn>
2025-07-24 10:55:29 +0000

    Merge feature updates from dev into main

--- opencompass/configs/models/hf_internlm/hf_internlm2_5_1_8b_chat.py
+++ opencompass/configs/models/hf_internlm/hf_internlm2_5_1_8b_chat.py
@@ -4,7 +4,7 @@ models = [
     dict(
         type=HuggingFacewithChatTemplate,
         abbr='internlm2_5-1_8b-chat-hf',
-        path='internlm/internlm2_5-1_8b-chat',
+        path='/cpfs01/shared/llm_ddd/gaoxuan/models/internlm2_5-1_8b-chat',
         max_out_len=2048,
         batch_size=8,
         run_cfg=dict(num_gpus=1),
--- opencompass/configs/models/qwen3/lmdeploy_qwen3_0_6b.py
+++ opencompass/configs/models/qwen3/lmdeploy_qwen3_0_6b.py
@@ -5,7 +5,7 @@ models = [
     dict(
         type=TurboMindModelwithChatTemplate,
         abbr='qwen_3_0.6b_thinking-turbomind',
-        path='Qwen/Qwen3-0.6B',
+        path='/cpfs01/shared/llm_ddd/opencompass/models/hf_hub/models--Qwen--Qwen3-0.6B/snapshots/ec0dcdfc641f7a94f8e969d459caa772b9c01706',
         engine_config=dict(session_len=32768, max_batch_size=16, tp=1),
         gen_config=dict(
             top_k=20, temperature=0.6, top_p=0.95, do_sample=True, enable_thinking=True
