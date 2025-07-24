89df95c4d640c38b2167b91560e64ccf989f81ec
xgao922 <gaox23@m.fudan.edu.cn>
2025-07-24 11:04:22 +0000

    Delete local model path

--- opencompass/configs/models/qwen2_5/hf_qwen_2_5_7b.py
+++ opencompass/configs/models/qwen2_5/hf_qwen_2_5_7b.py
@@ -4,7 +4,7 @@ models = [
     dict(
         type=HuggingFaceBaseModel,
         abbr='qwen2.5-7b-hf',
-        path='/cpfs01/shared/llm_ddd/opencompass/models/hf_hub/models--Qwen--Qwen2.5-7B/snapshots/09a0bac5707b43ec44508eab308b0846320c1ed4',
+        path='Qwen/Qwen2.5-7B',
         max_out_len=1024,
         batch_size=8,
         run_cfg=dict(num_gpus=1),
