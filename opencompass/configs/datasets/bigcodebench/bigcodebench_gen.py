be904ca1bd270aae4cfdc61c08d9355abdc1e46e
Myhs_phz <demarcia2014@126.com>
2025-08-21 14:51:00 +0800

    Update bigcodebench_gen.py

--- opencompass/configs/datasets/bigcodebench/bigcodebench_gen.py
+++ opencompass/configs/datasets/bigcodebench/bigcodebench_gen.py
@@ -4,4 +4,4 @@ with read_base():
     from .bigcodebench_hard_instruct_gen import bigcodebench_hard_instruct_datasets
     from .bigcodebench_hard_complete_gen import bigcodebench_hard_complete_datasets
 
-bigcodebench_hard_datasets = sum((v for k, v in locals().items() if k.endswith('_ds')), [])
+bigcodebench_hard_datasets = sum((v for k, v in locals().items() if k.endswith('_datasets')), [])
