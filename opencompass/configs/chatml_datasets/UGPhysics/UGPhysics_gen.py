c032a8860b60816e47e71c13453f14908f8fad4d
Myhs_phz <demarcia2014@126.com>
2025-09-30 10:32:12 +0000

    add datasets, example and doc

---
+++ opencompass/configs/chatml_datasets/UGPhysics/UGPhysics_gen.py
@@ -0,0 +1,42 @@
+
+subset_list = [
+    'AtomicPhysics',
+    'ClassicalElectromagnetism',
+    'ClassicalMechanics',
+    'Electrodynamics',
+    'GeometricalOptics',
+    'QuantumMechanics',
+    'Relativity',
+    'Solid-StatePhysics',
+    'StatisticalMechanics',
+    'SemiconductorPhysics',
+    'Thermodynamics',
+    'TheoreticalMechanics',
+    'WaveOptics',
+]
+
+language_list = [
+    'zh',
+    'en',
+]
+
+datasets = []
+
+for subset in subset_list:
+    for language in language_list:
+        datasets.append(
+            dict(
+                abbr=f'UGPhysics_{subset}_{language}',
+                path=f'./data/UGPhysics/{subset}/{language}.jsonl',
+                evaluator=dict(
+                    type='cascade_evaluator',
+                    rule_evaluator=dict(
+                        type='math_evaluator',
+                    ),
+                    llm_evaluator=dict(
+                        type='llm_evaluator',
+                        judge_cfg=dict(),
+                    )
+                ),
+            )
+        )
