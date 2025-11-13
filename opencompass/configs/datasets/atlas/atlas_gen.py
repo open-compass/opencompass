55deb68613001e21f0c16858be1bc96d19fc59f2
liushz <qq1791167085@163.com>
2025-11-13 11:21:17 +0800

    Fix atlas hash

--- opencompass/configs/datasets/atlas/atlas_gen.py
+++ opencompass/configs/datasets/atlas/atlas_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-  from .atlas_val_gen_b2d1b6 import atlas_datasets  # noqa: F401, F403
+  from .atlas_val_gen_b2d1b6 import atlas_datasets
