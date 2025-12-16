9eb6cceb0454b82cae5d8c1495e4998048ae7229
Zhudongsheng75 <2532956974@qq.com>
2025-12-16 14:07:20 +0800

    add lcb_pro

---
+++ opencompass/datasets/livecodebench_pro/__init__.py
@@ -0,0 +1,4 @@
+from .livecodebench_pro import LCBProDataset  # noqa: F401, F403
+from .livecodebench_pro_evaluator import LCBProEvaluator  # noqa: F401, F403
+
+__all__ = ['LCBProDataset', 'LCBProEvaluator']
---
+++ opencompass/datasets/livecodebench_pro/livecodebench_pro.py
@@ -0,0 +1,25 @@
+import json
+
+from datasets import Dataset
+
+from ..base import BaseDataset
+from opencompass.utils import get_data_path  # noqa: F401, F403
+
+
+class LCBProDataset(BaseDataset):
+
+    @staticmethod
+    def load(path, **kwargs):
+        path = get_data_path(path)
+        dataset_list = []
+        li = 0
+        with open(path, 'r', encoding='utf-8') as f:
+            for line in f:
+                line = line.strip()
+                data = json.loads(line)
+                dataset_list.append({
+                    'id_ddm': data['id_ddm'],
+                    'problem': data['dialogs'][0]['content']
+                })
+                li += 1
+        return Dataset.from_list(dataset_list)
