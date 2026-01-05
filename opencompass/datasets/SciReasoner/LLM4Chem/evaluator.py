eaf6ef25f1bf5a0f3ac96f77658132c4a84bec7d
Myhs_phz <demarcia2014@126.com>
2026-01-05 17:05:33 +0800

    [Fix] fix sample num and peer evaluator in SciReasoner (#2378)

--- opencompass/datasets/SciReasoner/LLM4Chem/evaluator.py
+++ opencompass/datasets/SciReasoner/LLM4Chem/evaluator.py
@@ -58,7 +58,7 @@ class LLM4ChemDataset(BaseDataset):
         if mini_set:
             import random
             random.seed(1024)
-            test_data = random.sample(test_data, 50)
+            test_data = random.sample(test_data, min(len(test_data), 150))
             random.seed()
 
         dataset = DatasetDict({
