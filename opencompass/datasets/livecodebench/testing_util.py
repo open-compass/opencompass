cc0cd7c8b5b55f0e20e73295cfe63d3a181e8040
Myhs_phz <demarcia2014@126.com>
2026-02-05 17:48:43 +0800

    fix lint

--- opencompass/datasets/livecodebench/testing_util.py
+++ opencompass/datasets/livecodebench/testing_util.py
@@ -657,7 +657,9 @@ def stripped_string_compare(s1, s2):
     s2 = s2.lstrip().rstrip()
     return s1 == s2
 
+
 class MockStdinWithBuffer:
+
     def __init__(self, inputs: str):
         self.inputs = inputs
         self._stringio = StringIO(inputs)
@@ -670,7 +672,7 @@ class MockStdinWithBuffer:
         return self._stringio.readline(*args)
 
     def readlines(self, *args):
-        return self.inputs.split("\n")
+        return self.inputs.split('\n')
 
     def __getattr__(self, name):
         # Delegate other attributes to StringIO
@@ -678,23 +680,24 @@ class MockStdinWithBuffer:
 
 
 class MockBuffer:
+
     def __init__(self, inputs: str):
-        self.inputs = inputs.encode("utf-8")  # Convert to bytes
+        self.inputs = inputs.encode('utf-8')  # Convert to bytes
 
     def read(self, *args):
         # Return as byte strings that can be split
         return self.inputs
 
     def readline(self, *args):
-        return self.inputs.split(b"\n")[0] + b"\n"
+        return self.inputs.split(b'\n')[0] + b'\n'
 
 
 def call_method(method, inputs):
 
     if isinstance(inputs, list):
-        inputs = "\n".join(inputs)
+        inputs = '\n'.join(inputs)
 
-    inputs_line_iterator = iter(inputs.split("\n"))
+    inputs_line_iterator = iter(inputs.split('\n'))
 
     # Create custom stdin mock with buffer support
     mock_stdin = MockStdinWithBuffer(inputs)
@@ -702,11 +705,11 @@ def call_method(method, inputs):
     # sys.setrecursionlimit(10000)
 
     # @patch('builtins.input', side_effect=inputs.split("\n"))
-    @patch("builtins.open", mock_open(read_data=inputs))
-    @patch("sys.stdin", mock_stdin)  # Use our custom mock instead of StringIO
-    @patch("sys.stdin.readline", lambda *args: next(inputs_line_iterator))
-    @patch("sys.stdin.readlines", lambda *args: inputs.split("\n"))
-    @patch("sys.stdin.read", lambda *args: inputs)
+    @patch('builtins.open', mock_open(read_data=inputs))
+    @patch('sys.stdin', mock_stdin)  # Use our custom mock instead of StringIO
+    @patch('sys.stdin.readline', lambda *args: next(inputs_line_iterator))
+    @patch('sys.stdin.readlines', lambda *args: inputs.split('\n'))
+    @patch('sys.stdin.read', lambda *args: inputs)
     # @patch('sys.stdout.write', print)
     def _inner_call_method(_method):
         try:
