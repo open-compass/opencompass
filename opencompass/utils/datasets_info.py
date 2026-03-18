90ba0ed8f67ed069783203dda1dd2f6d89252a6d
Myhs_phz <demarcia2014@126.com>
2026-03-18 18:05:55 +0800

    fix

--- opencompass/utils/datasets_info.py
+++ opencompass/utils/datasets_info.py
@@ -1,4 +1,9 @@
 DATASETS_MAPPING = {
+    "opencompass/IMO-Answer-Bench": {
+        "ms_id": None,
+        "hf_id": None,
+        "local": "./data/imo-bench-answerbench.jsonl",
+    },
     "opencompass/IFEval": {
         "ms_id": None,
         "hf_id": None,
