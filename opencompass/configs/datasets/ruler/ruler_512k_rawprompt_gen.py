6a7a6f77cc5a23d95c3d7599edc71f0d4a2b9058
Myhs_phz <demarcia2014@126.com>
2026-06-05 19:14:32 +0800

    add rawprompt for babilong, longbenchv2, needlebench and ruler

--- opencompass/configs/datasets/ruler/ruler_512k_rawprompt_gen.py
+++ opencompass/configs/datasets/ruler/ruler_512k_rawprompt_gen.py
@@ -3,11 +3,11 @@ import os
 from mmengine.config import read_base
 
 with read_base():
-    from .ruler_cwe_gen import cwe_datasets as cwe  # CWE
-    from .ruler_fwe_gen import fwe_datasets as fwe  # FWE
-    from .ruler_niah_gen import niah_datasets as niah  # Niah
-    from .ruler_qa_gen import qa_datasets as qa  # QA
-    from .ruler_vt_gen import vt_datasets as vt  # VT
+    from .ruler_cwe_rawprompt_gen import cwe_datasets as cwe  # CWE
+    from .ruler_fwe_rawprompt_gen import fwe_datasets as fwe  # FWE
+    from .ruler_niah_rawprompt_gen import niah_datasets as niah  # Niah
+    from .ruler_qa_rawprompt_gen import qa_datasets as qa  # QA
+    from .ruler_vt_rawprompt_gen import vt_datasets as vt  # VT
 
 
 import_ds = sum((cwe, fwe, niah, qa, vt), [])
@@ -16,8 +16,8 @@ import_ds = sum((cwe, fwe, niah, qa, vt), [])
 NUM_SAMPLES = 100  # Change to the number of samples you need
 tokenizer_model = os.environ.get('TOKENIZER_MODEL', 'gpt-4')
 # Change the context lengths to be tested
-max_seq_lens = [1024 * 64]
-abbr_suffixs = ['64k']
+max_seq_lens = [1024 * 512]
+abbr_suffixs = ['512k']
 
 ruler_datasets = []
 
---
+++ opencompass/configs/datasets/ruler/ruler_combined_rawprompt_gen.py
@@ -0,0 +1,14 @@
+from mmengine.config import read_base
+
+with read_base():
+    from .ruler_1m_rawprompt_gen import ruler_datasets as ruler_1m_ds
+    from .ruler_4k_rawprompt_gen import ruler_datasets as ruler_4k_ds
+    from .ruler_8k_rawprompt_gen import ruler_datasets as ruler_8k_ds
+    from .ruler_16k_rawprompt_gen import ruler_datasets as ruler_16k_ds
+    from .ruler_32k_rawprompt_gen import ruler_datasets as ruler_32k_ds
+    from .ruler_64k_rawprompt_gen import ruler_datasets as ruler_64k_ds
+    from .ruler_128k_rawprompt_gen import ruler_datasets as ruler_128k_ds
+    from .ruler_256k_rawprompt_gen import ruler_datasets as ruler_256k_ds
+    from .ruler_512k_rawprompt_gen import ruler_datasets as ruler_512k_ds
+
+ruler_combined_datasets = sum((v for k, v in locals().items() if k.endswith('_ds')), [])
