f6fd97ec99f9332937fc465ac77c5fa4173b2878
ssiq <liweiwuhome@hotmail.com>
2026-06-24 15:40:32 +0800

    [Update] Rename LongBench v2 config suffix

--- opencompass/configs/datasets/longbenchv2/longbenchv2_gen.py
+++ opencompass/configs/datasets/longbenchv2/longbenchv2_gen.py
@@ -1,4 +1,4 @@
 from mmengine.config import read_base
 
 with read_base():
-    from .longbenchv2_gen_75fbba import LongBenchv2_datasets
+    from .longbenchv2_gen_bd9437 import LongBenchv2_datasets
